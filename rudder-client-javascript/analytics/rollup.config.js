06a90309f3a3e73ca9b1f7b1f7bb6d9ac2982b3a
Sayan Mitra <sayan@rudderlabs.com>
2019-11-22 18:27:16 +0530

    add obfuscate plugin

--- rudder-client-javascript/analytics/rollup.config.js
+++ rudder-client-javascript/analytics/rollup.config.js
@@ -3,19 +3,22 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 import { terser } from "rollup-plugin-terser";
-import strip from "rollup-plugin-strip";
 import builtins from "rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
 import json from "rollup-plugin-json";
+import obfuscatorPlugin from "rollup-plugin-javascript-obfuscator";
 
 export default {
   input: "analytics.js",
   external: ["Xmlhttprequest", "universal-analytics"],
   output: [
     {
-      file: process.env.ENV == "prod" ? "dist/browser.min.js" : "dist/browser.js",
+      file:
+        process.env.ENV == "prod" ? "dist/browser.min.js" : "dist/browser.js",
       format: "iife",
-      name: "rudderanalytics"
+      name: "rudderanalytics",
+      sourceMap: true,
+      sourceMapMode: "inline"
     } /* ,
     {
       file: "dist/node.js",
@@ -50,7 +53,11 @@ export default {
     babel({
       exclude: "node_modules/**"
     }),
-    process.env.uglify === "true" && terser()
+    process.env.uglify === "true" && terser(),
+    process.env.ENV == "prod" &&
+      obfuscatorPlugin({
+        compact: true
+      })
     //process.env.uglify === "true" && uglify()
   ]
 };
