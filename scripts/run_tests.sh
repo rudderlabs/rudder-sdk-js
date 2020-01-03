2afca4e1204355c4906167dfb8ce5e37ec3b118e
Sampath <sampathvinayak1453@gmail.com>
2020-01-03 17:40:38 +0530

    serve file via server

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+nohup python3 -m http.server 8000 &
 npm run buildDevBrowser
 cp dist/browser.js tests/html/
-open tests/html/index.html
+open http://localhost:8000/tests/html/
