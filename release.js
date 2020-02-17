c49b78d708df2fd6c591b3ed5073595e113eb63e
Sayan Mitra <sayan@rudderlabs.com>
2020-02-17 19:29:55 +0530

    test gh-release

--- release.js
+++ release.js
@@ -1,19 +1,19 @@
 var ghRelease = require("gh-release");
 
+// options can also be just an empty object
+var options = {};
+
 // all options have defaults and can be omitted
 var options = {
   tag_name: "1.1.0-beta.0",
   target_commitish: "master",
   name: "Release 1.1.0-beta.0",
   draft: false,
-  prerelease: false,
+  prerelease: true,
   repo: "rudder-sdk-js",
-  owner: "sayan-rudder",
+  owner: "rudderlabs",
   endpoint: "https://api.github.com" // for GitHub enterprise, use http(s)://hostname/api/v3
 };
-
-// options can also be just an empty object
-var options = {};
 
 // or an API token
 options.auth = {
@@ -21,6 +21,9 @@ options.auth = {
 };
 
 ghRelease(options, function(err, result) {
-  if (err) throw err;
-  console.log(result); // create release response: https://developer.github.com/v3/repos/releases/#response-4
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log(JSON.stringify(result)); // create release response: https://developer.github.com/v3/repos/releases/#response-4
 });
