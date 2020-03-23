3bb0b9b3a172ec9eac66ee7e6f22a594179c9b07
Sayan Mitra <sayan@rudderlabs.com>
2020-03-23 23:39:53 +0530

    test commit

--- integrations/Lotame/browser.js
+++ integrations/Lotame/browser.js
@@ -1,5 +1,5 @@
 import logger from "../../utils/logUtil";
-import {LotameStorage} from "./LotameStorage";
+import { LotameStorage } from "./LotameStorage";
 class Lotame {
   constructor(config) {
     this.name = "LOTAME";
@@ -7,7 +7,7 @@ class Lotame {
     this.bcpUrlSettings = config.bcpUrlSettings;
     this.dspUrlSettings = config.dspUrlSettings;
     this.mappings = {};
-    config.mappings.forEach( mapping => {
+    config.mappings.forEach(mapping => {
       let key = mapping.key;
       let value = mapping.value;
       this.mappings[key] = value;
@@ -19,37 +19,42 @@ class Lotame {
     window.LOTAME_SYNCH_CALLBACK = () => {};
   }
 
-  addPixel(source, width, height){
-    let image = document.createElement('img'); 
-    image.src =  source;
+  addPixel(source, width, height) {
+    let image = document.createElement("img");
+    image.src = source;
     image.setAttribute("width", width);
     image.setAttribute("height", height);
-    document.getElementsByTagName('body')[0].appendChild(image);
+    document.getElementsByTagName("body")[0].appendChild(image);
   }
 
-  synchPixel(userId){
+  synchPixel(userId) {
     logger.debug("===== in synchPixel ======");
 
-    if(this.dspUrlSettings && this.dspUrlSettings.length > 0){
+    if (this.dspUrlSettings && this.dspUrlSettings.length > 0) {
       this.dspUrlSettings.forEach(urlSettings => {
-        let dspUrl = this.compileUrl({...this.mappings, userId:userId}, urlSettings.dspUrlTemplate);
+        let dspUrl = this.compileUrl(
+          { ...this.mappings, userId: userId },
+          urlSettings.dspUrlTemplate
+        );
         this.addPixel(dspUrl, "1", "1");
       });
     }
     this.storage.setLotameSynchTime(Date.now());
-    if(window.LOTAME_SYNCH_CALLBACK && typeof window.LOTAME_SYNCH_CALLBACK == "function"){
+    // this is custom to lotame, can be thought of as additional feature
+    if (
+      window.LOTAME_SYNCH_CALLBACK &&
+      typeof window.LOTAME_SYNCH_CALLBACK == "function"
+    ) {
       logger.debug("===== in synchPixel callback======");
       window.LOTAME_SYNCH_CALLBACK();
     }
-
   }
 
-  compileUrl(map, url){
-
+  compileUrl(map, url) {
     Object.keys(map).forEach(key => {
-      if(map.hasOwnProperty(key)){
-        let replaceKey = "{{"+key+"}}";
-        let regex = new RegExp(replaceKey, 'gi');
+      if (map.hasOwnProperty(key)) {
+        let replaceKey = "{{" + key + "}}";
+        let regex = new RegExp(replaceKey, "gi");
         url = url.replace(regex, map[key]);
       }
     });
@@ -59,7 +64,7 @@ class Lotame {
   identify(rudderElement) {
     logger.debug("in Lotame identify");
     let userId = rudderElement.message.userId;
-    this.synchPixel(userId); 
+    this.synchPixel(userId);
   }
 
   track(rudderElement) {
@@ -69,27 +74,31 @@ class Lotame {
   page(rudderElement) {
     logger.debug("in Lotame page");
 
-    if(this.bcpUrlSettings && this.bcpUrlSettings.length > 0){
+    if (this.bcpUrlSettings && this.bcpUrlSettings.length > 0) {
       this.bcpUrlSettings.forEach(urlSettings => {
-        let bcpUrl = this.compileUrl({...this.mappings}, urlSettings.bcpUrlTemplate);
+        let bcpUrl = this.compileUrl(
+          { ...this.mappings },
+          urlSettings.bcpUrlTemplate
+        );
         this.addPixel(bcpUrl, "1", "1");
       });
-      
     }
 
-    if(rudderElement.message.userId && this.isPixelToBeSynched()){
+    if (rudderElement.message.userId && this.isPixelToBeSynched()) {
       this.synchPixel(rudderElement.message.userId);
     }
   }
 
-  isPixelToBeSynched(){
+  isPixelToBeSynched() {
     let lastSynchedTime = this.storage.getLotameSynchTime();
     let currentTime = Date.now();
-    if(!lastSynchedTime){
+    if (!lastSynchedTime) {
       return true;
     }
 
-    let difference = Math.floor((currentTime - lastSynchedTime) / (1000 * 3600 * 24));
+    let difference = Math.floor(
+      (currentTime - lastSynchedTime) / (1000 * 3600 * 24)
+    );
     return difference >= 7;
   }
 
