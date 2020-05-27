bdeb1e455ec5281c536c9ef1d69600b7cd332f07
Sayan Mitra <sayan@rudderlabs.com>
2020-05-27 18:20:18 +0530

    add anonymousId header

--- utils/EventRepository.js
+++ utils/EventRepository.js
@@ -206,12 +206,14 @@ class EventRepository {
    * @memberof EventRepository
    */
   enqueue(rudderElement, type) {
+    var message = rudderElement.getElementContent();
+
     var headers = {
       "Content-Type": "application/json",
-      Authorization: "Basic " + btoa(this.writeKey + ":")
+      Authorization: "Basic " + btoa(this.writeKey + ":"),
+      AnonymousId: btoa(message.anonymousId)
     };
 
-    var message = rudderElement.getElementContent();
     message.originalTimestamp = getCurrentTimeFormatted();
     message.sentAt = getCurrentTimeFormatted(); // add this, will get modified when actually being sent
 
