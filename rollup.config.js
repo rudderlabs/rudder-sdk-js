775ab0cbb61696d83963f40e7deef663123a1164
Sayan Mitra <sayan@rudderlabs.com>
2020-05-28 13:08:35 +0530

    add sourcemap and cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -3,38 +3,64 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 import { terser } from "rollup-plugin-terser";
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import builtins from "rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
 import json from "rollup-plugin-json";
 import gzipPlugin from "rollup-plugin-gzip";
 import brotli from "rollup-plugin-brotli";
 import { version } from "./package.json";
+
+let distFileName = ''
+switch (process.env.ENV) {
+  case "prod":
+    switch (process.env.ENC) {
+      case "gzip":
+        if(process.env.PROD_DEBUG == "true") {
+          distFileName = "dist/rudder-analytics-map.min.gzip.js"
+        } else {
+          distFileName = "dist/rudder-analytics.min.gzip.js"
+        }
+        break
+      case "br":
+        if(process.env.PROD_DEBUG == "true") {
+          distFileName = "dist/rudder-analytics-map.min.br.js"
+        } else {
+          distFileName = "dist/rudder-analytics.min.br.js"
+        }
+        break
+      default:
+        if(process.env.PROD_DEBUG == "true") {
+          distFileName = "dist/rudder-analytics-map.min.js"
+        } else {
+          distFileName = "dist/rudder-analytics.min.js"
+        }
+        break
+    }
+    break
+  default:
+    distFileName =  "dist/browser.js"
+    break
+}
+
+let outputFiles = []
+if(process.env.NPM == "true") {
+  outputFiles.push({ file: "dist/bundle.js", format: "umd", name: "rudderanalytics" } )
+} else {
+  outputFiles.push({
+    file: distFileName,        
+    format: "iife",
+    name: "rudderanalytics",
+    sourcemap: process.env.PROD_DEBUG == 'true'? 'inline': false,
+  })
+}
+
 export default {
   input: "analytics.js",
   external: ["Xmlhttprequest", "universal-analytics"],
-  output: [
-    {
-      file:
-        process.env.ENV == "prod"
-          ? process.env.ENC == "gzip"
-            ? "dist/rudder-analytics.min.gzip.js"
-            : process.env.ENC == "br"
-            ? "dist/rudder-analytics.min.br.js"
-            : "dist/rudder-analytics.min.js"
-          : "dist/browser.js",
-      format: "iife",
-      name: "rudderanalytics",
-      sourceMap: true,
-      sourceMapMode: "inline"
-    },
-    { file: "dist/bundle.js", format: "umd", name: "rudderanalytics" }
-    /* ,
-    {
-      file: "dist/node.js",
-      format: "cjs"
-    } */
-  ],
+  output: outputFiles,
   plugins: [
+    sourcemaps(),
     replace({
       "process.browser": process.env.NODE_ENV == "true" ? false : true,
       "process.prod": process.env.ENV == "prod" ? true : false,
