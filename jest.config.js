b452b1171213a701e17cfefeca7d8355fe5fe70a
sayan <sayan.besu@gmail.com>
2020-06-30 21:20:13 +0530

    add ignore pattern

--- jest.config.js
+++ jest.config.js
@@ -143,9 +143,10 @@ module.exports = {
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/tests/"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
