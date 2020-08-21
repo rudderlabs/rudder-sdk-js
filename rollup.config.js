d2a505b2962d5ef0ae66924a92cff23a832066b7
sayan-mitra <sayan.besu@gmail.com>
2020-08-21 18:39:09 +0530

    add node modules transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -101,22 +101,7 @@ export default {
 
     babel({
       exclude: ["node_modules/@babel/**", "node_modules/core-js/**"],
-      presets: [
-        [
-          "@babel/env",
-          {
-            corejs: 3,
-            useBuiltIns: "usage",
-            targets: {
-              edge: "17",
-              firefox: "60",
-              ie: "9",
-              chrome: "67",
-              safari: "11.1",
-            },
-          },
-        ],
-      ],
+      presets: [["@babel/env"]],
       plugins: [
         [
           "@babel/plugin-proposal-class-properties",
