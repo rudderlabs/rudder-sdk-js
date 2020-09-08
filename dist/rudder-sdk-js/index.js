beed512aca70d8b7760f82d3a8a06fd685d7618b
sayan-mitra <sayan.besu@gmail.com>
2020-09-08 21:32:07 +0530

    add querystring parse to npm module

--- dist/rudder-sdk-js/index.js
+++ dist/rudder-sdk-js/index.js
@@ -339,6 +339,166 @@
 
   function noop() {}
 
+  var trim_1 = createCommonjsModule(function (module, exports) {
+    exports = module.exports = trim;
+
+    function trim(str) {
+      return str.replace(/^\s*|\s*$/g, '');
+    }
+
+    exports.left = function (str) {
+      return str.replace(/^\s*/, '');
+    };
+
+    exports.right = function (str) {
+      return str.replace(/\s*$/, '');
+    };
+  });
+  var trim_2 = trim_1.left;
+  var trim_3 = trim_1.right;
+
+  /**
+   * toString ref.
+   */
+  var toString = Object.prototype.toString;
+  /**
+   * Return the type of `val`.
+   *
+   * @param {Mixed} val
+   * @return {String}
+   * @api public
+   */
+
+  var componentType = function componentType(val) {
+    switch (toString.call(val)) {
+      case '[object Date]':
+        return 'date';
+
+      case '[object RegExp]':
+        return 'regexp';
+
+      case '[object Arguments]':
+        return 'arguments';
+
+      case '[object Array]':
+        return 'array';
+
+      case '[object Error]':
+        return 'error';
+    }
+
+    if (val === null) return 'null';
+    if (val === undefined) return 'undefined';
+    if (val !== val) return 'nan';
+    if (val && val.nodeType === 1) return 'element';
+    val = val.valueOf ? val.valueOf() : Object.prototype.valueOf.apply(val);
+    return _typeof(val);
+  };
+
+  /**
+   * Module dependencies.
+   */
+
+  var pattern = /(\w+)\[(\d+)\]/;
+  /**
+   * Safely encode the given string
+   * 
+   * @param {String} str
+   * @return {String}
+   * @api private
+   */
+
+  var encode = function encode(str) {
+    try {
+      return encodeURIComponent(str);
+    } catch (e) {
+      return str;
+    }
+  };
+  /**
+   * Safely decode the string
+   * 
+   * @param {String} str
+   * @return {String}
+   * @api private
+   */
+
+
+  var decode = function decode(str) {
+    try {
+      return decodeURIComponent(str.replace(/\+/g, ' '));
+    } catch (e) {
+      return str;
+    }
+  };
+  /**
+   * Parse the given query `str`.
+   *
+   * @param {String} str
+   * @return {Object}
+   * @api public
+   */
+
+
+  var parse = function parse(str) {
+    if ('string' != typeof str) return {};
+    str = trim_1(str);
+    if ('' == str) return {};
+    if ('?' == str.charAt(0)) str = str.slice(1);
+    var obj = {};
+    var pairs = str.split('&');
+
+    for (var i = 0; i < pairs.length; i++) {
+      var parts = pairs[i].split('=');
+      var key = decode(parts[0]);
+      var m;
+
+      if (m = pattern.exec(key)) {
+        obj[m[1]] = obj[m[1]] || [];
+        obj[m[1]][m[2]] = decode(parts[1]);
+        continue;
+      }
+
+      obj[parts[0]] = null == parts[1] ? '' : decode(parts[1]);
+    }
+
+    return obj;
+  };
+  /**
+   * Stringify the given `obj`.
+   *
+   * @param {Object} obj
+   * @return {String}
+   * @api public
+   */
+
+
+  var stringify = function stringify(obj) {
+    if (!obj) return '';
+    var pairs = [];
+
+    for (var key in obj) {
+      var value = obj[key];
+
+      if ('array' == componentType(value)) {
+        for (var i = 0; i < value.length; ++i) {
+          pairs.push(encode(key + '[' + i + ']') + '=' + encode(value[i]));
+        }
+
+        continue;
+      }
+
+      pairs.push(encode(key) + '=' + encode(obj[key]));
+    }
+
+    return pairs.join('&');
+  };
+
+  var componentQuerystring = {
+    parse: parse,
+    stringify: stringify
+  };
+
   var componentUrl = createCommonjsModule(function (module, exports) {
     /**
      * Parse the given `url`.
@@ -587,6 +747,7 @@
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   var CONFIG_URL = "https://api.rudderlabs.com/sourceConfig/?p=npm&v=1.0.7";
 =======
   var CONFIG_URL = "https://api.rudderlabs.com/sourceConfig/?p=npm&v=1.0.8";
@@ -594,6 +755,9 @@
 =======
   var CONFIG_URL = "https://api.rudderlabs.com/sourceConfig/?p=npm&v=1.0.9";
 >>>>>>> branch for npm and latest release
+=======
+  var CONFIG_URL = "https://api.rudderlabs.com/sourceConfig/?p=npm&v=1.0.10";
+>>>>>>> add querystring parse to npm module
   var MAX_WAIT_FOR_INTEGRATION_LOAD = 10000;
   var INTEGRATION_LOAD_CHECK_INTERVAL = 1000;
   /* module.exports = {
@@ -1957,7 +2121,7 @@
   /**
    * toString ref.
    */
-  var toString = Object.prototype.toString;
+  var toString$1 = Object.prototype.toString;
   /**
    * Return the type of `val`.
    *
@@ -1966,8 +2130,8 @@
    * @api public
    */
 
-  var componentType = function componentType(val) {
-    switch (toString.call(val)) {
+  var componentType$1 = function componentType(val) {
+    switch (toString$1.call(val)) {
       case '[object Function]':
         return 'function';
 
@@ -2235,9 +2399,9 @@
    */
 
   try {
-    var type$1 = componentType;
+    var type$1 = componentType$1;
   } catch (err) {
-    var type$1 = componentType;
+    var type$1 = componentType$1;
   }
   /**
    * HOP reference.
@@ -8575,7 +8739,7 @@
   /**
    * toString ref.
    */
-  var toString$1 = Object.prototype.toString;
+  var toString$2 = Object.prototype.toString;
   /**
    * Return the type of `val`.
    *
@@ -8584,8 +8748,8 @@
    * @api public
    */
 
-  var componentType$1 = function componentType(val) {
-    switch (toString$1.call(val)) {
+  var componentType$2 = function componentType(val) {
+    switch (toString$2.call(val)) {
       case '[object Date]':
         return 'date';
 
@@ -8629,7 +8793,7 @@
 
 
   var clone = function clone(obj) {
-    var t = componentType$1(obj);
+    var t = componentType$2(obj);
 
     if (t === 'object') {
       var copy = {};
@@ -8699,7 +8863,7 @@
 
   var ms = function ms(val, options) {
     options = options || {};
-    if ('string' == typeof val) return parse(val);
+    if ('string' == typeof val) return parse$1(val);
     return options["long"] ? _long(val) : _short(val);
   };
   /**
@@ -8711,7 +8875,7 @@
    */
 
 
-  function parse(str) {
+  function parse$1(str) {
     str = '' + str;
     if (str.length > 10000) return;
     var match = /^((?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|years?|yrs?|y)?$/i.exec(str);
@@ -9189,7 +9353,7 @@
 
   function set(name, value, options) {
     options = options || {};
-    var str = encode(name) + '=' + encode(value);
+    var str = encode$1(name) + '=' + encode$1(value);
     if (null == value) options.maxage = -1;
 
     if (options.maxage) {
@@ -9224,7 +9388,7 @@
       return {};
     }
 
-    return parse$1(str);
+    return parse$2(str);
   }
   /**
    * Get cookie `name`.
@@ -9247,7 +9411,7 @@
    */
 
 
-  function parse$1(str) {
+  function parse$2(str) {
     var obj = {};
     var pairs = str.split(/ *; */);
     var pair;
@@ -9255,7 +9419,7 @@
 
     for (var i = 0; i < pairs.length; ++i) {
       pair = pairs[i].split('=');
-      obj[decode(pair[0])] = decode(pair[1]);
+      obj[decode$1(pair[0])] = decode$1(pair[1]);
     }
 
     return obj;
@@ -9265,7 +9429,7 @@
    */
 
 
-  function encode(value) {
+  function encode$1(value) {
     try {
       return encodeURIComponent(value);
     } catch (e) {
@@ -9277,7 +9441,7 @@
    */
 
 
-  function decode(value) {
+  function decode$1(value) {
     try {
       return decodeURIComponent(value);
     } catch (e) {
@@ -11033,7 +11197,7 @@
 
   function set$1(name, value, options) {
     options = options || {};
-    var str = encode$1(name) + '=' + encode$1(value);
+    var str = encode$2(name) + '=' + encode$2(value);
     if (null == value) options.maxage = -1;
 
     if (options.maxage) {
@@ -11072,7 +11236,7 @@
 =======
 >>>>>>> branch for npm and latest release
 
-    return parse$2(str);
+    return parse$3(str);
   }
   /**
    * Get cookie `name`.
@@ -11097,8 +11261,12 @@
 <<<<<<< HEAD
 =======
 
+<<<<<<< HEAD
 >>>>>>> branch for npm and latest release
   function parse$2(str) {
+=======
+  function parse$3(str) {
+>>>>>>> add querystring parse to npm module
     var obj = {};
     var pairs = str.split(/ *; */);
     var pair;
@@ -11106,7 +11274,7 @@
 
     for (var i = 0; i < pairs.length; ++i) {
       pair = pairs[i].split('=');
-      obj[decode$1(pair[0])] = decode$1(pair[1]);
+      obj[decode$2(pair[0])] = decode$2(pair[1]);
     }
 
     return obj;
@@ -11116,7 +11284,7 @@
    */
 
 
-  function encode$1(value) {
+  function encode$2(value) {
     try {
       return encodeURIComponent(value);
     } catch (e) {
@@ -11128,7 +11296,7 @@
    */
 
 
-  function decode$1(value) {
+  function decode$2(value) {
     try {
       return decodeURIComponent(value);
     } catch (e) {
@@ -12691,7 +12859,7 @@
     this.build = "1.0.0";
     this.name = "RudderLabs JavaScript SDK";
     this.namespace = "com.rudderlabs.javascript";
-    this.version = "1.0.9";
+    this.version = "1.0.10";
   };
 >>>>>>> update npm module
 
@@ -12706,7 +12874,7 @@
       };
 =======
     this.name = "RudderLabs JavaScript SDK";
-    this.version = "1.0.9";
+    this.version = "1.0.10";
   }; // Operating System information class
 >>>>>>> update npm module
 
@@ -15063,6 +15231,11 @@
     return el;
   }
 
+  var queryDefaults = {
+    trait: "ajs_trait_",
+    prop: "ajs_prop_"
+  }; // https://unpkg.com/test-rudder-sdk@1.0.5/dist/browser.js
+
   /**
    * Add the rudderelement object to flush queue
    *
@@ -15930,10 +16103,75 @@
       value: function sendSampleRequest() {
         ScriptLoader("ad-block", "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js");
       }
+      /**
+       * parse the given query string into usable Rudder object
+       * @param {*} query
+       */
+
+    }, {
+      key: "parseQueryString",
+      value: function parseQueryString(query) {
+        function getTraitsFromQueryObject(qObj) {
+          var traits = {};
+          Object.keys(qObj).forEach(function (key) {
+            if (key.substr(0, queryDefaults.trait.length) == queryDefaults.trait) {
+              traits[key.substr(queryDefaults.trait.length)] = qObj[key];
+            }
+          });
+          return traits;
+        }
+
+        function getEventPropertiesFromQueryObject(qObj) {
+          var props = {};
+          Object.keys(qObj).forEach(function (key) {
+            if (key.substr(0, queryDefaults.prop.length) == queryDefaults.prop) {
+              props[key.substr(queryDefaults.prop.length)] = qObj[key];
+            }
+          });
+          return props;
+        }
+
+        var returnObj = {};
+        var queryObject = componentQuerystring.parse(query);
+        var userTraits = getTraitsFromQueryObject(queryObject);
+        var eventProps = getEventPropertiesFromQueryObject(queryObject);
+
+        if (queryObject.ajs_uid) {
+          returnObj.userId = queryObject.ajs_uid;
+          returnObj.traits = userTraits;
+        }
+
+        if (queryObject.ajs_aid) {
+          returnObj.anonymousId = queryObject.ajs_aid;
+        }
+
+        if (queryObject.ajs_event) {
+          returnObj.event = queryObject.ajs_event;
+          returnObj.properties = eventProps;
+        }
+
+        return returnObj;
+      }
     }]);
 
     return Analytics;
   }();
+
+  function pushDataToAnalyticsArray(argumentsArray, obj) {
+    if (obj.anonymousId) {
+      if (obj.userId) {
+        argumentsArray.unshift(["setAnonymousId", obj.anonymousId], ["identify", obj.userId, obj.traits]);
+      } else {
+        argumentsArray.unshift(["setAnonymousId", obj.anonymousId]);
+      }
+    } else if (obj.userId) {
+      argumentsArray.unshift(["identify", obj.userId, obj.traits]);
+    }
+
+    if (obj.event) {
+      argumentsArray.push(["track", obj.event, obj.properties]);
+    }
+  }
 
   var instance = new Analytics();
   componentEmitter(instance);
@@ -15960,7 +16198,11 @@
     logger.debug("=====from init, calling method:: ", method);
     instance[method].apply(instance, _toConsumableArray(argumentsArray[0]));
     argumentsArray.shift();
-  }
+  } // once loaded, parse querystring of the page url to send events
+
+
+  var parsedQueryObject = instance.parseQueryString(window.location.search);
+  pushDataToAnalyticsArray(argumentsArray, parsedQueryObject);
 
   if (eventsPushedAlready && argumentsArray && argumentsArray.length > 0) {
     for (var i$1 = 0; i$1 < argumentsArray.length; i$1++) {
