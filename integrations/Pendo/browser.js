de6d61299bf39943aeba488fb8dbc5739a59d92c
Shivam@RS <shivam441@gmail.com>
2020-11-24 17:09:31 +0530

    pendo-sdk/added identify call

--- integrations/Pendo/browser.js
+++ integrations/Pendo/browser.js
@@ -1,3 +1,5 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable lines-between-class-members */
 import logger from "../../utils/logUtil";
 import ScriptLoader from "../ScriptLoader";
 
@@ -7,47 +9,75 @@ class Pendo {
     this.apiKey = !config.apiKey ? "" : config.apiKey;
     this.isDebugMode = config.enableDebugMode;
     this.name = "PENDO";
-
+    this.setIntervalHandler = undefined;
     logger.debug("Config ", config);
   }
 
   init() {
+    window.pendo = {};
+    window.pendoCli = undefined;
+    window.pendo.apiKey = this.apiKey;
     ScriptLoader(
-      "pendo",
+      "pendo-fs",
       `https://cdn.pendo.io/agent/static/${this.apiKey}/pendo.js`
     );
-
     logger.debug("===in init Pendo===");
-    this.loadScript();
+
+    this.setIntervalHandler = setInterval(this.initPendo.bind(this), 1000);
   }
 
-
-
-  identify(rudderElement){
-
-
+  initPendo() {
+    if (this.isReady()) {
+      window.pendoCli = window.pendo;
+      clearInterval(this.setIntervalHandler);
+      if (this.isDebugMode) {
+        this.enableDebugging();
+      } else {
+        this.disableDebugging();
+      }
+    }
+  }
+  isLoaded() {
+    logger.debug("in PENDO isLoaded");
+    return !!window.pendoCli;
   }
 
+  isReady() {
+    return window.pendo && window.pendo.isReady && window.pendo.isReady();
+  }
+
+  identify(rudderElement) {
+    const obj = {};
+    let accountObj = {};
+    let visitorObj = {};
+    visitorObj.d =
+      rudderElement.message.userId || rudderElement.message.anonymousId;
+    if (rudderElement.message.context.traits) {
+      visitorObj = {
+        ...visitorObj,
+        ...rudderElement.message.context.traits,
+      };
+    }
+
+    obj.visitor = visitorObj;
+    window.pendoCli.identify(obj);
+  }
 
   track(rudderElement) {
-
+    window.pendoCli.track(rudderElement.message.event);
   }
 
-
   group(rudderElement) {
-
+    window.pendoCli.initialize();
   }
 
   enableDebugging() {
-
+    window.pendoCli.enableDebugging();
   }
 
   disableDebugging() {
-
+    window.pendoCli.disableDebugging();
   }
-
-
-
 }
 
-export {Pendo};
+export { Pendo };
