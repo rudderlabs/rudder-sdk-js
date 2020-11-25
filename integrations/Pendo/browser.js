1a8342e0d6e01a9b3f809555792040eaca9308fd
Shivam@RS <shivam441@gmail.com>
2020-11-25 13:42:31 +0530

    pendo-sdk/fixed review comments for track, flattening property and append anonymous id to _Pendo_ prefix

--- integrations/Pendo/browser.js
+++ integrations/Pendo/browser.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable lines-between-class-members */
@@ -47,35 +48,75 @@ class Pendo {
     return window.pendo && window.pendo.isReady && window.pendo.isReady();
   }
 
+  constructPendoAnonymousId(id) {
+    return `_PENDO_T_${id}`;
+  }
+
+  isUserAnonymous(message) {
+    return !message.userId;
+  }
+
   identify(rudderElement) {
     let visitorObj = {};
-    visitorObj.id =
-      rudderElement.message.userId || rudderElement.message.anonymousId;
+    const id = this.isUserAnonymous(rudderElement.message)
+      ? this.constructPendoAnonymousId(rudderElement.message.anonymousId)
+      : rudderElement.message.userId;
+    visitorObj.id = id;
     if (rudderElement.message.context.traits) {
       visitorObj = {
         ...visitorObj,
-        ...rudderElement.message.context.traits,
+        ...this.flattenAgentPayload(rudderElement.message.context.traits),
       };
     }
 
     this._identify({ visitor: visitorObj });
   }
 
+  recurse(cur, prop, result) {
+    if (Object(cur) !== cur) {
+      result[prop] = cur;
+    } else if (Array.isArray(cur)) {
+      const l = cur.length;
+      for (let i = 0; i < l; i += 1)
+        this.recurse(cur[i], prop ? `${prop}.${i}` : `${i}`);
+      if (l === 0) result[prop] = [];
+    } else {
+      let isEmpty = true;
+      Object.keys(cur).forEach((key) => {
+        isEmpty = false;
+        this.recurse(cur[key], prop ? `${prop}.${key}` : key, result);
+      });
+      if (isEmpty) result[prop] = {};
+    }
+    return result;
+  }
+
+  flattenAgentPayload(data) {
+    return this.recurse(data, "", {});
+  }
+
   track(rudderElement) {
-    window.pendoCli.track(rudderElement.message.event);
+    const props = rudderElement.message;
+    window.pendoCli.track(rudderElement.message.event, props);
   }
 
   group(rudderElement) {
+    const obj = {};
     let accountObj = {};
     accountObj.id =
       rudderElement.message.groupId || rudderElement.message.anonymousId;
     if (rudderElement.message.context.traits) {
       accountObj = {
         ...accountObj,
-        ...rudderElement.message.context.traits,
+        ...this.flattenAgentPayload(rudderElement.message.context.traits),
       };
+      obj.account = accountObj;
     }
-    this._identify({ account: accountObj });
+    if (rudderElement.message.userId) {
+      obj.visitor = { id: rudderElement.message.userId };
+    }
+
+    this._identify(obj);
   }
 
   _identify(obj) {
