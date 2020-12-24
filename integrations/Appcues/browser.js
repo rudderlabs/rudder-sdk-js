db210fb595feef388bddd5a17e0883207196398f
desusai <saivenkatdesu@gmail.com>
2020-12-24 11:11:44 +0530

    Made Minor Fixes and Added Appcues to integration_cname and client_server_name.js

--- integrations/Appcues/browser.js
+++ integrations/Appcues/browser.js
@@ -24,7 +24,8 @@ class Appcues {
 
   isReady() {
     logger.debug("in appcues isReady");
-    // if (this.sendToAllDestinations) {
+    // This block of code enables us to send Appcues Flow events to all the other destinations connected to the same source (we might use it in future)
+    // if (this.sendToAllDestinations && window.Appcues) {
     //   window.Appcues.on("all", function(eventName, event) {
     //     window.rudderanalytics.track(eventName, event, {
     //       integrations: {
@@ -46,6 +47,8 @@ class Appcues {
     } = rudderElement.message;
     if (userId) {
       window.Appcues.identify(userId, traits);
+    } else {
+      logger.error("user id is empty");
     }
   }
 
@@ -56,21 +59,23 @@ class Appcues {
     } = rudderElement.message;
     if (eventName) {
       window.Appcues.track(eventName, properties);
+    } else {
+      logger.error("event name is empty");
     }
   }
 
   page(rudderElement) {
     const {
       properties,
-      name,
-      category
+      name
     } = rudderElement.message;
     window.Appcues.page(name, properties);
   }
-
-  reset() {
-    window.Appcues.reset();
-  }
+  
+  // To be uncommented after adding Reset feature to our SDK
+  // reset() {
+  //   window.Appcues.reset();
+  // }
 
 }
 
