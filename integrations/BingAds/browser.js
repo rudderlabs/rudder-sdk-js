051d746b86b65d23c5f7dcbd957098ef47b98ad0
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2021-03-22 12:32:02 +0530

    Updated browser.js file
    Replaced var with let.

--- integrations/BingAds/browser.js
+++ integrations/BingAds/browser.js
@@ -7,14 +7,14 @@ class BingAds {
 
   loadBingadsScript = () => {
     ((w, d, t, r, u) => {
-      var f, n, i;
+      let f, n, i;
       w[u] = w[u] || [], f = () => {
-        var o = {
+        let o = {
           ti: this.apikey
         };
         o.q = w[u], w[u] = new UET(o);
       }, n = d.createElement(t), n.src = r, n.async = 1, n.onload = n.onreadystatechange = function() {
-        var s = this.readyState;
+        let s = this.readyState;
         s && s !== "loaded" && s !== "complete" || (f(), n.onload = n.onreadystatechange = null)
       }, i = d.getElementsByTagName(t)[0], i.parentNode.insertBefore(n, i)
     })(window, document, "script", "https://bat.bing.com/bat.js", "uetq");
