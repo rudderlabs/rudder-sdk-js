79b818a729c5ebf46c857167e6416bd5fa2f0a0d
shrouti <shrouti.gangopadhyay@gmail.com>
2021-08-05 19:33:32 +0530

    script loading edit

--- integrations/Qualtrics/browser.js
+++ integrations/Qualtrics/browser.js
@@ -22,35 +22,39 @@ class Qualtrics {
         return;
       }
     
-      let projectIdFormatted = this.projectId.replace(/_/g, "");
-      projectIdFormatted = projectIdFormatted.toLowerCase().trim();
-
+      const projectIdFormatted = this.projectId.replace(/_/g, "").toLowerCase().trim();;
+      const requestUrlFormatted = `https://${projectIdFormatted}-${this.brandId}.siteintercept.qualtrics.com/SIE/?Q_ZID=${this.projectId}`;
+      const requestIdFormatted = `QSI_S_${this.projectId}`;
+      
     (function () {
       var g = function (e, h, f, g) {
-        this.get = function (a) { for (var a = a + "=", c = document.cookie.split(";"), b = 0, e = c.length; b < e; b++) { for (var d = c[b]; " " == d.charAt(0);)d = d.substring(1, d.length); if (0 == d.indexOf(a)) return d.substring(a.length, d.length) } return null };
-        this.set = function (a, c) { var b = "", b = new Date; b.setTime(b.getTime() + 6048E5); b = "; expires=" + b.toGMTString(); document.cookie = a + "=" + c + b + "; path=/; " };
-        this.check = function () { var a = this.get(f); if (a) a = a.split(":"); else if (100 != e) "v" == h && (e = Math.random() >= e / 100 ? 0 : 100), a = [h, e, 0], this.set(f, a.join(":")); else return !0; var c = a[1]; if (100 == c) return !0; switch (a[0]) { case "v": return !1; case "r": return c = a[2] % Math.floor(100 / c), a[2]++, this.set(f, a.join(":")), !c }return !0 };
-        this.go = function () { if (this.check()) { var a = document.createElement("script"); a.type = "text/javascript"; a.src = g; document.body && document.body.appendChild(a) } };
-        this.start = function () { var t = this; "complete" !== document.readyState ? window.addEventListener ? window.addEventListener("load", function () { t.go() }, !1) : window.attachEvent && window.attachEvent("onload", function () { t.go() }) : t.go() };
-      };
-      try { (new g(100, "r", `QSI_S_${this.projectId}`, `https://${projectIdFormatted}-${this.brandId}.siteintercept.qualtrics.com/SIE/?Q_ZID=${this.projectId}`)).start() } catch (i) { }
+                this.get = function (a) { for (var a = a + "=", c = document.cookie.split(";"), b = 0, e = c.length; b < e; b++) { for (var d = c[b]; " " == d.charAt(0);)d = d.substring(1, d.length); if (0 == d.indexOf(a)) return d.substring(a.length, d.length) } return null };
+                this.set = function (a, c) { var b = "", b = new Date; b.setTime(b.getTime() + 6048E5); b = "; expires=" + b.toGMTString(); document.cookie = a + "=" + c + b + "; path=/; " };
+                this.check = function () { var a = this.get(f); if (a) a = a.split(":"); else if (100 != e) "v" == h && (e = Math.random() >= e / 100 ? 0 : 100), a = [h, e, 0], this.set(f, a.join(":")); else return !0; var c = a[1]; if (100 == c) return !0; switch (a[0]) { case "v": return !1; case "r": return c = a[2] % Math.floor(100 / c), a[2]++, this.set(f, a.join(":")), !c }return !0 };
+                this.go = function () { if (this.check()) { var a = document.createElement("script"); a.type = "text/javascript"; a.src = g; document.body && document.body.appendChild(a) } };
+                this.start = function () { var t = this; "complete" !== document.readyState ? window.addEventListener ? window.addEventListener("load", function () { t.go() }, !1) : window.attachEvent && window.attachEvent("onload", function () { t.go() }) : t.go() };
+            };
+      try { (new g(100, "r", requestIdFormatted, requestUrlFormatted)).start() } catch (i) { }
     })();
-   const div = document.createElement('div');
-    div.setAttribute("id", this.projectId);
-    window._qsie = window._qsie || [];
+
+     const div = document.createElement('div');
+     div.setAttribute("id", String(this.projectId));
+     window._qsie = window._qsie || [];
+     const e =  document.getElementsByTagName('head')[0].appendChild(div);
+    
 
   }
 
   // eslint-disable-next-line class-methods-use-this
   isLoaded() {
     logger.debug("===in Qualtrics isLoaded===");
-    return !!(window.window._qsie && window._qsie.push !== Array.prototype.push);
+    return !!(window._qsie && window.QSI && window.QSI.API );
   }
 
   // eslint-disable-next-line class-methods-use-this
   isReady() {
     logger.debug("===in Qualtrics isReady===");
-    return !!(window._qsie &&window._qsie.push !== Array.prototype.push);
+    return !!(window._qsie && window.QSI && window.QSI.API );
   }
 
   page (rudderElement) {
