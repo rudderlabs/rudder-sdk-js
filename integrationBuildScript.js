8ed93834e84729cd8d451d941e1384b1b52d6f0c
Moumita Mandal <moumita@rudderstack.com>
2021-08-27 12:22:57 +0530

    build script updated

--- integrationBuildScript.js
+++ integrationBuildScript.js
@@ -1,15 +1,21 @@
 import { exec } from "child_process";
+import logger from "./utils/logUtil";
 import { configToIntNames } from "./config_to_integration_names";
 
 Object.keys(configToIntNames).forEach((key) => {
-  console.log(configToIntNames[key]);
-
   exec(
     `npm run buildProdIntegrationCLI --intg=${configToIntNames[key]}`,
     (err, stdout) => {
-      if (err) console.log(err);
+      if (err)
+        logger.error(
+          `== Error occured while building ${configToIntNames[key]} ==`,
+          err
+        );
       else {
-        console.log(stdout);
+        logger.debug(
+          `== build completed for ${configToIntNames[key]} ==`,
+          stdout
+        );
       }
     }
   );
