9bb74b3c6e18d4dec4e5b2ca2e142675fd5d2072
saikumarrs <saibattinoju@rudderstack.com>
2021-08-27 15:38:55 +0530

    Made build package creation non-async

--- integrationBuildScript.js
+++ integrationBuildScript.js
@@ -1,22 +1,24 @@
-import { exec } from "child_process";
+import { execSync } from "child_process";
 import logger from "./utils/logUtil";
 import { configToIntNames } from "./config_to_integration_names";
 
-Object.keys(configToIntNames).forEach((key) => {
-  exec(
-    `npm run buildProdIntegrationCLI --intg=${configToIntNames[key]}`,
-    (err, stdout) => {
-      if (err)
-        logger.error(
-          `== Error occured while building ${configToIntNames[key]} ==`,
-          err
-        );
-      else {
-        logger.debug(
-          `== build completed for ${configToIntNames[key]} ==`,
-          stdout
-        );
-      }
-    }
-  );
+logger.setLogLevel("DEBUG");
+const intgNamesArr = Object.values(configToIntNames);
+let curInt = 1;
+intgNamesArr.forEach((intgName) => {
+  try {
+    logger.debug(
+      `\nBuilding integration module: ${intgName} (${curInt} of ${intgNamesArr.length})`
+    );
+    const cmdOutput = execSync(
+      `npm run buildProdIntegrationCLI --intg=${intgName}`,
+      { encoding: "utf-8" }
+    );
+    logger.debug("Done!");
+    logger.info("Command output: ", cmdOutput);
+  } catch (err) {
+    logger.error(`${intgName} build failed!!!`);
+    logger.error("ERROR: ", err);
+  }
+  curInt += 1;
 });
