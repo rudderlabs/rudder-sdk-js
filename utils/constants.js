fe9a1fb520000072869cc4cdef6153026ea5baf6
Moumita Mandal <moumita@rudderstack.com>
2021-09-16 16:49:40 +0530

    version changed

--- utils/constants.js
+++ utils/constants.js
@@ -68,7 +68,7 @@ const ECommerceEvents = {
 const CONFIG_URL =
   "https://api.rudderlabs.com/sourceConfig/?p=process.module_type&v=process.package_version";
 const CDN_INT_DIR = "js-integrations";
-const DEST_SDK_BASE_URL = `https://cdn.rudderlabs.com/v3/${CDN_INT_DIR}`;
+const DEST_SDK_BASE_URL = `https://cdn.rudderlabs.com/v1.1/${CDN_INT_DIR}`;
 
 const MAX_WAIT_FOR_INTEGRATION_LOAD = 10000;
 const INTEGRATION_LOAD_CHECK_INTERVAL = 1000;
