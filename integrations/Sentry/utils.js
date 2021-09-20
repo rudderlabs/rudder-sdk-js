549a7289477cab88dbef0985f41dcc5965b80971
shrouti <shrouti.gangopadhyay@gmail.com>
2021-09-20 14:43:42 +0530

    mandatory field checks reverted and scriploader edit

--- integrations/Sentry/utils.js
+++ integrations/Sentry/utils.js
@@ -17,11 +17,6 @@ const SentryScriptLoader = (id, src, integrity) => {
   logger.debug("==parent script==", e);
   logger.debug("==adding script==", js);
   e.parentNode.insertBefore(js, e);
-  SentryScriptLoader(
-    "Sentry",
-    `https://browser.sentry-cdn.com/6.12.0/rewriteframes.min.js`,
-    `sha384-WOm9k3kzVt1COFAB/zCXOFx4lDMtJh/2vmEizIwgog7OW0P/dPwl3s8f6MdwrD7q`
-  );
 };
 
 export { SentryScriptLoader, convertObjectToArray };
