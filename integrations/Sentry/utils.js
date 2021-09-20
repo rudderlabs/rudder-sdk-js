132b7b99983e460ee344da4f4d1b8a66d3fa664f
shrouti <shrouti.gangopadhyay@gmail.com>
2021-09-20 10:57:08 +0530

    logger addition, object to array conversion and formatting

--- integrations/Sentry/utils.js
+++ integrations/Sentry/utils.js
@@ -1,11 +1,21 @@
 import logger from "../../utils/logUtil";
 
+const convertObjectToArray = (objectInput) => {
+  const list = [];
+  objectInput.forEach(function (objectItem) {
+    const x = objectItem.eventCustomProperties;
+    list.push(x);
+  });
+  return list;
+};
+
 const SentryRewriteFramesLoader = (id, src) => {
-    logger.debug(`In rewriteFrame loader === ${id}`);
-    const js = document.createElement("script");
+  logger.debug(`In rewriteFrame loader === ${id}`);
+  const js = document.createElement("script");
   js.src = src;
-  js.integrity = "sha384-WOm9k3kzVt1COFAB/zCXOFx4lDMtJh/2vmEizIwgog7OW0P/dPwl3s8f6MdwrD7q";
-  js.crossorigin = "anonymous"
+  js.integrity =
+    "sha384-WOm9k3kzVt1COFAB/zCXOFx4lDMtJh/2vmEizIwgog7OW0P/dPwl3s8f6MdwrD7q";
+  js.crossorigin = "anonymous";
   js.async = true;
   js.type = "text/javascript";
   js.id = id;
@@ -13,15 +23,15 @@ const SentryRewriteFramesLoader = (id, src) => {
   logger.debug("==parent script==", e);
   logger.debug("==adding script==", js);
   e.parentNode.insertBefore(js, e);
-}
-
+};
 
 const SentryScriptLoader = (id, src) => {
   logger.debug(`in script loader=== ${id}`);
   const js = document.createElement("script");
   js.src = src;
-  js.integrity = "sha384-S3qfdh3AsT1UN84WIYNuOX9vVOoFg3nB17Jp5/pTFGDBGBt+dtz7MGAV845efkZr";
-  js.crossorigin = "anonymous"
+  js.integrity =
+    "sha384-S3qfdh3AsT1UN84WIYNuOX9vVOoFg3nB17Jp5/pTFGDBGBt+dtz7MGAV845efkZr";
+  js.crossorigin = "anonymous";
   js.async = true;
   js.type = "text/javascript";
   js.id = id;
@@ -30,28 +40,31 @@ const SentryScriptLoader = (id, src) => {
   logger.debug("==adding script==", js);
   e.parentNode.insertBefore(js, e);
   SentryRewriteFramesLoader(
-    "Sentry", 
+    "Sentry",
     `https://browser.sentry-cdn.com/6.12.0/rewriteframes.min.js`
-    );
-
+  );
 };
 
-
-const identifierPayloadBuilder = (userId, email, name, ip_address) => {
-    payload = {};
-    if (userId) {
-        userIdentifierPayload.id = userId;
-    }
-    if(email) {
-        userIdentifierPayload.email = email;
-    }
-    if(name) {
-        userIdentifierPayload.username = name;
-    }
-    if(ip_address) {
-        userIdentifierPayload.ip_address = ip_address;
-    }
-    return payload;
+const identifierPayloadBuilder = (userId, email, name, ipAddress) => {
+  const userIdentifierPayload = {};
+  if (userId) {
+    userIdentifierPayload.id = userId;
+  }
+  if (email) {
+    userIdentifierPayload.email = email;
+  }
+  if (name) {
+    userIdentifierPayload.username = name;
+  }
+  if (ipAddress) {
+    userIdentifierPayload.ip_address = ipAddress;
+  }
+  return userIdentifierPayload;
 };
 
-export {SentryScriptLoader, identifierPayloadBuilder, SentryRewriteFramesLoader}; 
+export {
+  SentryScriptLoader,
+  identifierPayloadBuilder,
+  SentryRewriteFramesLoader,
+  convertObjectToArray,
+};
