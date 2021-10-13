514479e631671187f3a3ad1dc7953f620c5d1d80
saikumarrs <saibattinoju@rudderstack.com>
2021-10-13 15:19:17 +0530

    Bumped version in RudderApp

--- utils/RudderApp.js
+++ utils/RudderApp.js
@@ -1,7 +1,7 @@
 // Application class
 class RudderApp {
   constructor() {
-    this.build = "1.0.0";
+    this.build = "1.2.2";
     this.name = "RudderLabs JavaScript SDK";
     this.namespace = "com.rudderlabs.javascript";
     this.version = "process.package_version";
