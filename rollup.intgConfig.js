7ae65ec39c84a7574ba622005042ba8cdee78cea
saikumarrs <saibattinoju@rudderstack.com>
2021-11-14 19:55:16 +0530

    Suppressed build warnings

--- rollup.intgConfig.js
+++ rollup.intgConfig.js
@@ -113,6 +113,18 @@ export default {
             loose: true,
           },
         ],
+        [
+          "@babel/plugin-proposal-private-property-in-object",
+          {
+            loose: true,
+          },
+        ],
+        [
+          "@babel/plugin-proposal-private-methods",
+          {
+            loose: true,
+          },
+        ],
         ["@babel/plugin-transform-arrow-functions"],
         ["@babel/plugin-transform-object-assign"],
       ],
