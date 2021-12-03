aa864b85a0df00a5d0afa68c8e55bf78cd41f106
Ankit Kumar <ankit8540988674@gmail.com>
2021-12-03 12:42:53 +0530

    Google optimize onboarding

---
+++ integrations/GoogleOptimize/browser.js
@@ -0,0 +1,37 @@
+import logger from "../../utils/logUtil";
+import ScriptLoader from "../ScriptLoader";
+
+class GoogleOptimize {
+  constructor(config) {
+    this.name = "GOOGLE_OPTIMIZE";
+    this.trackingId = config.trackingId;
+    this.containerId = config.containerId;
+    this.async = config.async;
+  }
+
+  init() {
+    logger.debug("===in init Google Optimize===");
+    if (!this.trackingId) {
+      return;
+    }
+    ScriptLoader(
+      "Google Tag Manager",
+      `https://www.googletagmanager.com/gtag/js?id=${this.trackingId}`
+    );
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", `${this.trackingId}`);
+
+    if (!this.containerId) {
+      return;
+    }
+    ScriptLoader(
+      "Google Optimize",
+      `https://www.googleoptimize.com/optimize.js?id=${this.containerId}`
+    );
+  }
+}
+export default GoogleOptimize;
