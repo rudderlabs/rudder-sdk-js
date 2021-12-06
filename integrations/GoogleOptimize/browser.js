9d3a557a334efc532ccf5ab23d3e2196ab3014c4
Ankit Kumar <ankit8540988674@gmail.com>
2021-12-06 11:38:40 +0530

    added anti flicker support and check for google analytics

--- integrations/GoogleOptimize/browser.js
+++ integrations/GoogleOptimize/browser.js
@@ -4,27 +4,30 @@ import ScriptLoader from "../ScriptLoader";
 class GoogleOptimize {
   constructor(config) {
     this.name = "GOOGLE_OPTIMIZE";
+    this.ga = config.ga;
     this.trackingId = config.trackingId;
     this.containerId = config.containerId;
     this.async = config.async;
+    this.aflicker = config.aflicker;
   }
 
   init() {
     logger.debug("===in init Google Optimize===");
-    if (!this.trackingId) {
-      return;
+    if (this.ga) {
+      if (!this.trackingId) {
+        return;
+      }
+      ScriptLoader(
+        "Google Tag Manager",
+        `https://www.googletagmanager.com/gtag/js?id=${this.trackingId}`
+      );
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", `${this.trackingId}`);
     }
-    ScriptLoader(
-      "Google Tag Manager",
-      `https://www.googletagmanager.com/gtag/js?id=${this.trackingId}`
-    );
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-    gtag("config", `${this.trackingId}`);
-
     if (!this.containerId) {
       return;
     }
@@ -40,6 +43,23 @@ class GoogleOptimize {
         `https://www.googleoptimize.com/optimize.js?id=${this.containerId}`,
         false
       );
+    }
+    if (this.aflicker) {
+      (function (a, s, y, n, c, h, i, d, e) {
+        s.className += " " + y;
+        h.start = 1 * new Date();
+        h.end = i = function () {
+          s.className = s.className.replace(RegExp(" ?" + y), "");
+        };
+        (a[n] = a[n] || []).hide = h;
+        setTimeout(function () {
+          i();
+          h.end = null;
+        }, c);
+        h.timeout = c;
+      })(window, document.documentElement, "async-hide", "dataLayer", 4000, {
+        this.containerId: true,
+      });
     }
   }
 }
