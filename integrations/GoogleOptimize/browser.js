5e800ec087a808b35535300af91af6670b24c5f1
Ankit Kumar <ankit8540988674@gmail.com>
2021-12-07 20:01:57 +0530

    removed flickerObj and added some comments

--- integrations/GoogleOptimize/browser.js
+++ integrations/GoogleOptimize/browser.js
@@ -39,15 +39,15 @@ class GoogleOptimize {
       gtag("config", `${this.trackingId}`);
     }
 
+    // anti flicker snippet contains insertBefore since it needs to be executed before any other script
+    // link -> https://support.google.com/optimize/answer/7100284?hl=en&ref_topic=6197443
     if (this.aflicker) {
-      const flickerObj = {};
-      flickerObj[`${this.containerId}`] = true;
       const flick = document.createElement("style");
       flick.innerHTML = ".async-hide { opacity: 0 !important}";
       const js = document.createElement("script");
       js.innerHTML = `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'${this.containerId}':true});`;
       const e = document.getElementsByTagName("script")[0];
-      e.parentNode.insertBefore(flick, e);
+      e.parentNode.insertBefore(flick, e); // style tag in anti flicker snippet should be before the a-flicker script as per docs
       e.parentNode.insertBefore(js, e);
     }
   }
