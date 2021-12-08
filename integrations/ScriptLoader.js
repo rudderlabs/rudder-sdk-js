f7584da7aadfa997a51cdde0884e5540f2d11d58
Ankit Kumar <ankit8540988674@gmail.com>
2021-12-08 12:09:53 +0530

    added defaultState variable and assigned true to parameter of scriptloader

--- integrations/ScriptLoader.js
+++ integrations/ScriptLoader.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-use-before-define */
 import logger from "../utils/logUtil";
+const defaultState = true;
 
-const ScriptLoader = (id, src, async) => {
+const ScriptLoader = (id, src, async = true) => {
   logger.debug(`in script loader=== ${id}`);
   const js = document.createElement("script");
   js.src = src;
-  js.async = async === undefined ? true : async;
+  js.async = async === undefined ? defaultState : async;
   js.type = "text/javascript";
   js.id = id;
   const e = document.getElementsByTagName("script")[0];
