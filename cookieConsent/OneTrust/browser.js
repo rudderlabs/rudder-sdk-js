efeb0c20a45a279fb66739f69f5a21227fcf27ba
Ruchira <ruchira@rudderlabs.com>
2021-12-22 12:40:28 +0530

    OneTrust_CookieConesnt_Changes: changed logic for accepting cookies refactored

--- cookieConsent/OneTrust/browser.js
+++ cookieConsent/OneTrust/browser.js
@@ -1,10 +1,13 @@
+/* eslint-disable class-methods-use-this */
 class OneTrust {
   constructor(sourceConfig) {
     this.sourceConfig = sourceConfig;
   }
 
   isEnabled(destConfig) {
-    console.log(this.sourceConfig);
+    if (!window.OneTrust && !window.OnetrustActiveGroups) {
+      return true;
+    }
     const { oneTrustConsentGroup } = destConfig; // mapping of the destination with the consent group name
     const userSetConsentGroupIds = window.OnetrustActiveGroups.split(","); // Content ids user has rejected
     const oneTrustAllGroupsInfo = window.OneTrust.GetDomainData().Groups; // info about all the groups created in the onetrust account
@@ -15,14 +18,18 @@ class OneTrust {
         userSetConsentGroupNames.push(GroupName.toUpperCase().trim());
       }
     });
-    const oneTrustConsentGroupArr = oneTrustConsentGroup.map(
-      (c) => c.oneTrustConsentGroup
-    );
-    const containsAllConsent = oneTrustConsentGroupArr
-      .filter((n) => n)
-      .every((element) =>
+    const oneTrustConsentGroupArr = oneTrustConsentGroup
+      .map((c) => c.oneTrustConsentGroup)
+      .filter((n) => n);
+    let containsAllConsent = true;
+    if (
+      Array.isArray(oneTrustConsentGroupArr) &&
+      oneTrustConsentGroupArr.length
+    ) {
+      containsAllConsent = oneTrustConsentGroupArr.every((element) =>
         userSetConsentGroupNames.includes(element.toUpperCase().trim())
       );
+    }
     return containsAllConsent;
   }
 }
