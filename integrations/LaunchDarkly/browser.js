1d68c59fdb2c71fd3577943e0efb5f754d1f44b7
ItsSudip <sudip.paul1997@gmail.com>
2022-01-06 20:19:29 +0530

    [LAUNCHDARKLY]:: Added createUser function.

--- integrations/LaunchDarkly/browser.js
+++ integrations/LaunchDarkly/browser.js
@@ -1,77 +1,65 @@
-import { isArray, isObject } from "../../utils/utils";
+/* eslint-disable class-methods-use-this */
 import logger from "../../utils/logUtil";
 import ScriptLoader from "../ScriptLoader";
-import isString from "./utils";
+import createUser from "./utils";
+
 class LaunchDarkly {
   constructor(config) {
     this.name = "LaunchDarkly";
     this.clientSideId = config.clientSideId;
   }
+
   init() {
     logger.debug("===in init LaunchDarkly===");
     if (!this.clientSideId) {
-      logger.error("clientSideId is missing");
+      logger.error(
+        `${this.name} :: Unable to initialize destination - clientSideId is missing in config`
+      );
       return;
     }
     ScriptLoader(null, "https://unpkg.com/launchdarkly-js-client-sdk@2");
   }
+
   isLoaded() {
     logger.debug("===In isLoaded LaunchDarkly===");
     return !!window.LDClient;
   }
+
   isReady() {
     logger.debug("===In isReady LaunchDarkly===");
     return this.isLoaded();
   }
+
   identify(rudderElement) {
-    window.user = {};
     const { message } = rudderElement;
-    user.key = message.userId || message.anonymousId;
-    const { traits } = message.context;
-    if (traits.anonymous !== undefined) user.anonymous = traits.anonymous;
-    if (traits.avatar !== undefined && isString(traits.avatar))
-      user.avatar = traits.avatar;
-    if (traits.country != undefined && isString(traits.country))
-      user.country = traits.country;
-    if (traits.custom != undefined && isObject(traits.custom))
-      user.custom = traits.custom;
-    if (traits.email != undefined && isString(traits.email))
-      user.email = traits.email;
-    if (traits.firstName != undefined && isString(traits.firstName))
-      user.firstName = traits.firstName;
-    if (traits.ip != undefined && isString(traits.ip)) user.ip = traits.ip;
-    if (traits.lastName != undefined && isString(traits.lastName))
-      user.lastName = traits.lastName;
-    if (traits.name != undefined && isString(traits.name))
-      user.name = traits.name;
-    if (
-      traits.privateAttributeNames != undefined &&
-      isArray(traits.privateAttributeNames)
-    )
-      user.privateAttributeNames = traits.privateAttributeNames;
-    if (traits.secondary != undefined && isString(traits.secondary))
-      user.secondary = traits.secondary;
+    window.user = createUser(message);
+
     if (window.ldclient) {
-      ldclient.identify(user);
+      window.ldclient.identify(window.user);
     } else {
-      window.ldclient = LDClient.initialize(this.clientSideId, user);
+      window.ldclient = window.LDClient.initialize(
+        this.clientSideId,
+        window.user
+      );
     }
   }
+
   track(rudderElement) {
     const { event, properties } = rudderElement.message;
     if (window.ldclient) {
-      ldclient.track(event, properties);
+      window.ldclient.track(event, properties);
     } else
       logger.error(
         "=== In LaunchDarkly, track is not supported before identify ==="
       );
   }
+
   alias(rudderElement) {
     const { message } = rudderElement;
-    var newUser = { key: message.userId };
+    const newUser = { key: message.userId };
 
     if (window.ldclient) {
-      ldclient.alias(newUser, user);
+      window.ldclient.alias(newUser, window.user);
     } else
       logger.error(
         "=== In LaunchDarkly, alias is not supported before identify ==="
--- integrations/LaunchDarkly/utils.js
+++ integrations/LaunchDarkly/utils.js
@@ -1,5 +1,38 @@
+import { isObject } from "../../utils/utils";
+
 const isString = function isString(val) {
   return Object.prototype.toString.call(val) === "[object String]";
 };
 
-export default isString;
+const createUser = (message) => {
+  const user = {};
+  user.key = message.userId || message.anonymousId;
+  const { traits } = message.context;
+  if (traits.anonymous !== undefined) {
+    user.anonymous = traits.anonymous;
+  }
+  if (traits.avatar !== undefined && isString(traits.avatar))
+    user.avatar = traits.avatar;
+  if (traits.country !== undefined && isString(traits.country))
+    user.country = traits.country;
+  if (traits.custom !== undefined && isObject(traits.custom))
+    user.custom = traits.custom;
+  if (traits.email !== undefined && isString(traits.email))
+    user.email = traits.email;
+  if (traits.firstName !== undefined && isString(traits.firstName))
+    user.firstName = traits.firstName;
+  if (traits.ip !== undefined && isString(traits.ip)) user.ip = traits.ip;
+  if (traits.lastName !== undefined && isString(traits.lastName))
+    user.lastName = traits.lastName;
+  if (traits.name !== undefined && isString(traits.name))
+    user.name = traits.name;
+  if (
+    traits.privateAttributeNames !== undefined &&
+    Array.isArray(traits.privateAttributeNames)
+  )
+    user.privateAttributeNames = traits.privateAttributeNames;
+  if (traits.secondary !== undefined && isString(traits.secondary))
+    user.secondary = traits.secondary;
+  return user;
+};
+export default createUser;
