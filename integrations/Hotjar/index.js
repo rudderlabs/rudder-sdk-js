4ff4415b098f03e1576964461beb82febd25987d
Moumita Mandal <moumita@rudderstack.com>
2022-01-07 10:17:10 +0530

    fixed export statement issue

--- integrations/Hotjar/index.js
+++ integrations/Hotjar/index.js
@@ -1,4 +1,6 @@
-import { HotjarNode } from "./node";
+// import { HotjarNode } from "./node";
 import { Hotjar } from "./browser";
 
-export let hotjar = process.browser ? { Hotjar } : { HotjarNode };
+// export let hotjar = process.browser ? { Hotjar } : { HotjarNode };
+
+export { Hotjar };
