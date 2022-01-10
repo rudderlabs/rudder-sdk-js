ac87f2400c8142e72b512da60ee79b2c219943e3
ItsSudip <sudip.paul1997@gmail.com>
2022-01-10 18:23:57 +0530

    [LAUNCHDARKLY]:: Refactored code. Removed the redundent tests in createUser function.

--- integrations/LaunchDarkly/utils.js
+++ integrations/LaunchDarkly/utils.js
@@ -11,28 +11,26 @@ const createUser = (message) => {
   if (traits.anonymous !== undefined) {
     user.anonymous = traits.anonymous;
   }
-  if (traits.avatar !== undefined && isString(traits.avatar))
-    user.avatar = traits.avatar;
-  if (traits.country !== undefined && isString(traits.country))
-    user.country = traits.country;
-  if (traits.custom !== undefined && isObject(traits.custom))
-    user.custom = traits.custom;
-  if (traits.email !== undefined && isString(traits.email))
-    user.email = traits.email;
-  if (traits.firstName !== undefined && isString(traits.firstName))
-    user.firstName = traits.firstName;
-  if (traits.ip !== undefined && isString(traits.ip)) user.ip = traits.ip;
-  if (traits.lastName !== undefined && isString(traits.lastName))
-    user.lastName = traits.lastName;
-  if (traits.name !== undefined && isString(traits.name))
-    user.name = traits.name;
-  if (
-    traits.privateAttributeNames !== undefined &&
-    Array.isArray(traits.privateAttributeNames)
-  )
-    user.privateAttributeNames = traits.privateAttributeNames;
-  if (traits.secondary !== undefined && isString(traits.secondary))
-    user.secondary = traits.secondary;
+  if (traits.avatar !== undefined)
+    if (isString(traits.avatar)) user.avatar = traits.avatar;
+  if (traits.country !== undefined)
+    if (isString(traits.country)) user.country = traits.country;
+  if (traits.custom !== undefined)
+    if (isObject(traits.custom)) user.custom = traits.custom;
+  if (traits.email !== undefined)
+    if (isString(traits.email)) user.email = traits.email;
+  if (traits.firstName !== undefined)
+    if (isString(traits.firstName)) user.firstName = traits.firstName;
+  if (traits.ip !== undefined) if (isString(traits.ip)) user.ip = traits.ip;
+  if (traits.lastName !== undefined)
+    if (isString(traits.lastName)) user.lastName = traits.lastName;
+  if (traits.name !== undefined)
+    if (isString(traits.name)) user.name = traits.name;
+  if (traits.privateAttributeNames !== undefined)
+    if (Array.isArray(traits.privateAttributeNames))
+      user.privateAttributeNames = traits.privateAttributeNames;
+  if (traits.secondary !== undefined)
+    if (isString(traits.secondary)) user.secondary = traits.secondary;
   return user;
 };
 export default createUser;
