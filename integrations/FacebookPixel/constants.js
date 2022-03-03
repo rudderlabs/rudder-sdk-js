379b53d363a8456030b5ca9eb743342aaa7e01e6
saikumarrs <saibattinoju@rudderstack.com>
2022-03-03 17:58:19 +0530

    Identify traits update for Facebook Pixel

--- integrations/FacebookPixel/constants.js
+++ integrations/FacebookPixel/constants.js
@@ -6,4 +6,64 @@ const CNameMapping = {
   FB_PIXEL: NAME,
 };
 
-export { NAME, CNameMapping };
+const traitsMapper = [
+  {
+    destKey: "external_id",
+    sourceKeys: [
+      "userId",
+      "context.traits.userId",
+      "context.traits.id",
+      "anonymousId",
+    ],
+  },
+  {
+    destKey: "em",
+    sourceKeys: "context.traits.email",
+  },
+  {
+    destKey: "ph",
+    sourceKeys: "context.traits.phone",
+  },
+  {
+    destKey: "ge",
+    sourceKeys: "context.traits.gender",
+  },
+  {
+    destKey: "db",
+    sourceKeys: "context.traits.birthday",
+  },
+  {
+    destKey: "ln",
+    sourceKeys: [
+      "context.traits.lastname",
+      "context.traits.lastName",
+      "context.traits.last_name",
+    ],
+  },
+  {
+    destKey: "fn",
+    sourceKeys: [
+      "context.traits.firstname",
+      "context.traits.firstName",
+      "context.traits.first_name",
+    ],
+  },
+  {
+    destKey: "ct",
+    sourceKeys: ["context.traits.address.city", "context.traits.city"],
+  },
+  {
+    destKey: "st",
+    sourceKeys: ["context.traits.address.state", "context.traits.state"],
+  },
+  {
+    destKey: "zp",
+    sourceKeys: ["context.traits.address.zip", "context.traits.zip"],
+  },
+  {
+    destKey: "country",
+    sourceKeys: ["context.traits.address.country", "context.traits.country"],
+  },
+];
+
+export { traitsMapper, CNameMapping, NAME };
