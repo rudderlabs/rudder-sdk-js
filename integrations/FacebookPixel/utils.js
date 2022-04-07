538e0bffdaef039d8aa207b0cca7e705be479050
Moumita <36885121+MoumitaM@users.noreply.github.com>
2022-04-07 20:11:10 +0530

    FB util file renamed and fixed `originalTimestamp` override (#488)
    * fixed util file name mismatch
    
    * prevent originalTimestamp override
    
    * Update utils/EventRepository.js
    
    Co-authored-by: Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
    
    * Minor formatting issue fixed
    
    Co-authored-by: Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
    Co-authored-by: saikumarrs <saibattinoju@rudderstack.com>

---
+++ integrations/FacebookPixel/utils.js
@@ -0,0 +1,9 @@
+function getEventId(message) {
+  return (
+    message.traits.event_id ||
+    message.context.traits.event_id ||
+    message.properties.event_id ||
+    message.messageId
+  );
+}
+export default getEventId;
