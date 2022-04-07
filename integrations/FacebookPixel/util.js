fbce8be771b75d2cf5cca211a68ab23100987322
Moumita <mandal.moumita2013@gmail.com>
2022-04-07 13:22:31 +0530

    fixed util file name mismatch

--- integrations/FacebookPixel/util.js
+++
@@ -1,4 +0,0 @@
-function getEventId(message){
-    return message.traits.event_id || message.context.traits.event_id || message.properties.event_id || message.messageId;
-};
-export default getEventId;
