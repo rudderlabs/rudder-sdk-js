cacfcc63741871af5f4fe5e2edbbfa2d8577f2e2
Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
2022-04-08 18:54:33 +0530

    FB Pixel utils: Added validation for event_id values (#492)
    * FB Pixel utils: Added validation for event_id values
    
    * Refactored getEventId method to use 'get' method

--- integrations/FacebookPixel/utils.js
+++ integrations/FacebookPixel/utils.js
@@ -1,8 +1,11 @@
+import get from "get-value";
+
 function getEventId(message) {
-  if (message.traits) return message.traits.event_id;
-  if (message.context && message.context.traits)
-    return message.context.traits.event_id;
-  if (message.properties) return message.properties.event_id;
-  return message.messageId;
+  return (
+    get(message, "traits.event_id") ||
+    get(message, "context.traits.event_id") ||
+    get(message, "properties.event_id") ||
+    message.messageId
+  );
 }
 export default getEventId;
