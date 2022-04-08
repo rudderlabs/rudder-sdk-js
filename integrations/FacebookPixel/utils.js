7e0494cd9888b91ab7e2d399a6457d1c743aa0c7
saikumarrs <saibattinoju@rudderstack.com>
2022-04-08 18:50:26 +0530

    Refactored getEventId method to use 'get' method

--- integrations/FacebookPixel/utils.js
+++ integrations/FacebookPixel/utils.js
@@ -1,16 +1,11 @@
+import get from "get-value";
+
 function getEventId(message) {
-  if (message.traits && message.traits.event_id) return message.traits.event_id;
-
-  if (
-    message.context &&
-    message.context.traits &&
-    message.context.traits.event_id
-  )
-    return message.context.traits.event_id;
-
-  if (message.properties && message.properties.event_id)
-    return message.properties.event_id;
-
-  return message.messageId;
+  return (
+    get(message, "traits.event_id") ||
+    get(message, "context.traits.event_id") ||
+    get(message, "properties.event_id") ||
+    message.messageId
+  );
 }
 export default getEventId;
