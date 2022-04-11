e937bf68aa51428102cb78848afd10d97a600de6
saikumarrs <saibattinoju@rudderstack.com>
2022-04-11 17:00:30 +0530

    Updated the minimum number of processes to 1

--- integrationBuildScript.js
+++ integrationBuildScript.js
@@ -12,7 +12,7 @@ let index = 0;
 
 // At any given time, create only processes for
 // max CPUs available
-const maxAtOnce = numCPUs - 1;
+const maxAtOnce = numCPUs > 1 ? numCPUs - 1 : 1;
 
 console.log(`Maximum number of concurrent processes: ${maxAtOnce}`);
 
