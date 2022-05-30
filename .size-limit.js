619ba6a4b4bcecd12939ce3004decaad763204df
saikumarrs <saibattinoju@rudderstack.com>
2022-05-30 22:10:45 +0530

    Increased size limits for all SDKs

--- .size-limit.js
+++ .size-limit.js
@@ -7,18 +7,18 @@ module.exports = [
     name: "Core - CDN",
     path: "dist/rudder-analytics.min.js",
     gzip: true,
-    limit: "37 kB",
+    limit: "38 kB",
   },
   {
     name: "All Integrations - CDN",
     path: "dist/integrations/*.min.js",
     gzip: true,
-    limit: "600 kB",
+    limit: "610 kB",
   },
   {
     name: "Core - NPM",
     path: "dist/rudder-sdk-js/index.js",
     gzip: true,
-    limit: "37 kB",
+    limit: "38 kB",
   },
 ];
