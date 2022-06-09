40952302ddceb57c052cea69f77cabefb8be8fee
saikumarrs <saibattinoju@rudderstack.com>
2022-06-09 18:39:01 +0530

    chore(cleanup): removed rollup-plugin-sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
-import sourcemaps from "rollup-plugin-sourcemaps";
 import builtins from "rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
 import json from "@rollup/plugin-json";
@@ -75,7 +74,6 @@ export default {
   external: [],
   output: outputFiles,
   plugins: [
-    sourcemaps(),
     replace({
       preventAssignment: true,
       "process.browser": process.env.NODE_ENV !== "true",
@@ -104,6 +102,7 @@ export default {
     builtins(),
 
     babel({
+      inputSourceMap: true,
       babelHelpers: "bundled",
       exclude: ["node_modules/@babel/**", "node_modules/core-js/**"],
       presets: [
