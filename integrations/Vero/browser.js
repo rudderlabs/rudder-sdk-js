a114c0ed658c8a33d0e586130ae5643cfdd4d554
sauravlal-rudderstack <sauravlal@rudderstack.com>
2022-06-29 23:04:14 +0530

    feature[Vero-web-device-mode]: build identify, track, alias call

--- integrations/Vero/browser.js
+++ integrations/Vero/browser.js
@@ -1,27 +1,72 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+import get from "get-value";
+import { NAME } from "./constants";
+import logger from "../../utils/logUtil";
+
 class Vero {
-    constructor(config) {
+  constructor(config) {
+    this.apiKey = config.apiKey;
+    this.name = NAME;
+  }
 
-    }
-    init() {
-        logger.debug("===in init Vero===");
-    }
-    isLoaded() {
-        logger.debug("in Vero isLoaded");
-    }
-    isReady() {
-        logger.debug("in Vero isReady");
-    }
-    identify(rudderElement) {
+  init() {
+    logger.debug("===In init Vero===");
+    var _veroq = _veroq || [];
 
+    (function () {
+      var ve = document.createElement("script");
+      ve.type = "text/javascript";
+      ve.async = true;
+      ve.src = "https://d3qxef4rp70elm.cloudfront.net/m.js";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(ve, s);
+    })();
+    _veroq.push(["init", { api_key: this.apiKey }]);
+  }
+
+  isLoaded() {
+    logger.debug("in Vero isLoaded");
+    return !!window._veroq;
+  }
+
+  isReady() {
+    logger.debug("in Vero isReady");
+  }
+
+  identify(rudderElement) {
+    const message = { rudderElement };
+    const userId = message.userId || message.anonymousId;
+    const { traits } = message.context || message;
+    // userId OR email address are required by Vero's API. When userId isn't present,
+    // email will be used as the userId.
+    const email =
+      get(message, "context.traits.email") || get(message, "traits.email");
+    if (!userId && !email) {
+      logger.error("[Vero]: User parameter userId or email is required.");
+      return;
     }
+    window._veroq.push([userId, { traits }]);
+  }
 
-    track(rudderElement) {
+  track(rudderElement) {
+    logger.debug("===In Vero track===");
 
+    const { message } = rudderElement;
+    const { event, properties } = message;
+    if (!event) {
+      logger.error("[Vero]: Event name from track call is missing!!===");
+      return;
     }
+    window._veroq.push(["userId", { properties }]);
+  }
 
-    alias() {
-
-    }
+  alias(rudderElement) {
+    const { message } = rudderElement;
+    const { event } = message;
+    const userId = message.userId;
+    const previousId = message.previousId;
+  }
 }
 
 export default Vero;
