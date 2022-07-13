46a2b0869ad8b09f4324a76d1cbfbd8979f9ba4c
yashasvibajpai <yashasvi.bajpai1999@gmail.com>
2022-07-13 17:39:24 +0530

    add debug mode support, refactor gtag parameters to an object

--- integrations/GA4/browser.js
+++ integrations/GA4/browser.js
@@ -21,6 +21,7 @@ export default class GA4 {
     this.blockPageView = config.blockPageViewEvent || false;
     this.extendPageViewParams = config.extendPageViewParams || false;
     this.extendGroupPayload = config.extendGroupPayload || false;
+    this.debugMode = config.debugMode || false;
     this.name = NAME;
   }
 
@@ -33,26 +34,22 @@ export default class GA4 {
         window.dataLayer.push(arguments);
       };
     window.gtag("js", new Date());
-
+    const gtagParameterObject = {};
     // This condition is not working, even after disabling page view
     // page_view is even getting called on page load
     if (this.blockPageView) {
-      if (this.sendUserId) {
-        window.gtag("config", measurementId, {
-          user_id: userId,
-          send_page_view: false,
-        });
-      } else {
-        window.gtag("config", measurementId, {
-          send_page_view: false,
-        });
-      }
-    } else if (this.sendUserId) {
-      window.gtag("config", measurementId, {
-        user_id: userId,
-      });
-    } else {
+      gtagParameterObject.send_page_view = false;
+    }
+    if (this.sendUserId) {
+      gtagParameterObject.user_id = userId;
+    }
+    if (this.debugMode) {
+      gtagParameterObject.debug_mode = true;
+    }
+    if (Object.keys(gtagParameterObject).length === 0) {
       window.gtag("config", measurementId);
+    } else {
+      window.gtag("config", measurementId, gtagParameterObject);
     }
 
     ScriptLoader(
