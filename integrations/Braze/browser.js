25181e7a4cc386eb6dd790b33efa829b6886b68e
aashishmalik <imashishmalik.45@gmail.com>
2022-08-24 15:14:45 +0530

    comments addressed

--- integrations/Braze/browser.js
+++ integrations/Braze/browser.js
@@ -1,8 +1,8 @@
 /* eslint-disable class-methods-use-this */
+import { del } from "obj-case";
 import logger from "../../utils/logUtil";
-import { LOAD_ORIGIN } from "../ScriptLoader";
-import { NAME } from "./constants";
-import {del} from "obj-case";
+import { LOAD_ORIGIN } from '../ScriptLoader';
+import { BrazeOperationString, NAME } from "./constants";
 
 /*
 E-commerce support required for logPurchase support & other e-commerce events as track with productId changed
@@ -13,7 +13,7 @@ class Braze {
     this.appKey = config.appKey;
     this.enableHtmlInAppMessages = config.enableHtmlInAppMessages || false;
     this.allowUserSuppliedJavascript =
-    config.allowUserSuppliedJavascript || false;
+      config.allowUserSuppliedJavascript || false;
     if (!config.appKey) this.appKey = "";
     this.endPoint = "";
     if (config.dataCenter) {
@@ -51,24 +51,55 @@ class Braze {
   }
 
   init() {
-    logger.debug("===init Braze===");
+    logger.debug("===in init Braze===");
 
     // load braze
-    +function(a,p,P,b,y){a.braze={};a.brazeQueue=[];for(var s="BrazeSdkMetadata DeviceProperties Card Card.prototype.dismissCard Card.prototype.removeAllSubscriptions Card.prototype.removeSubscription Card.prototype.subscribeToClickedEvent Card.prototype.subscribeToDismissedEvent Card.fromContentCardsJson Banner CaptionedImage ClassicCard ControlCard ContentCards ContentCards.prototype.getUnviewedCardCount Feed Feed.prototype.getUnreadCardCount ControlMessage InAppMessage InAppMessage.SlideFrom InAppMessage.ClickAction InAppMessage.DismissType InAppMessage.OpenTarget InAppMessage.ImageStyle InAppMessage.Orientation InAppMessage.TextAlignment InAppMessage.CropType InAppMessage.prototype.closeMessage InAppMessage.prototype.removeAllSubscriptions InAppMessage.prototype.removeSubscription InAppMessage.prototype.subscribeToClickedEvent InAppMessage.prototype.subscribeToDismissedEvent InAppMessage.fromJson FullScreenMessage ModalMessage HtmlMessage SlideUpMessage User User.Genders User.NotificationSubscriptionTypes User.prototype.addAlias User.prototype.addToCustomAttributeArray User.prototype.addToSubscriptionGroup User.prototype.getUserId User.prototype.incrementCustomUserAttribute User.prototype.removeFromCustomAttributeArray User.prototype.removeFromSubscriptionGroup User.prototype.setCountry User.prototype.setCustomLocationAttribute User.prototype.setCustomUserAttribute User.prototype.setDateOfBirth User.prototype.setEmail User.prototype.setEmailNotificationSubscriptionType User.prototype.setFirstName User.prototype.setGender User.prototype.setHomeCity User.prototype.setLanguage User.prototype.setLastKnownLocation User.prototype.setLastName User.prototype.setPhoneNumber User.prototype.setPushNotificationSubscriptionType InAppMessageButton InAppMessageButton.prototype.removeAllSubscriptions InAppMessageButton.prototype.removeSubscription InAppMessageButton.prototype.subscribeToClickedEvent automaticallyShowInAppMessages destroyFeed hideContentCards showContentCards showFeed showInAppMessage toggleContentCards toggleFeed changeUser destroy getDeviceId initialize isPushBlocked isPushPermissionGranted isPushSupported logCardClick logCardDismissal logCardImpressions logContentCardImpressions logContentCardsDisplayed logCustomEvent logFeedDisplayed logInAppMessageButtonClick logInAppMessageClick logInAppMessageHtmlClick logInAppMessageImpression logPurchase openSession requestPushPermission removeAllSubscriptions removeSubscription requestContentCardsRefresh requestFeedRefresh requestImmediateDataFlush enableSDK isDisabled setLogger setSdkAuthenticationSignature addSdkMetadata disableSDK subscribeToContentCardsUpdates subscribeToFeedUpdates subscribeToInAppMessage subscribeToSdkAuthenticationFailures toggleLogging unregisterPush wipeData handleBrazeAction".split(" "),i=0;i<s.length;i++){for(var m=s[i],k=a.braze,l=m.split("."),j=0;j<l.length-1;j++)k=k[l[j]];k[l[j]]=(new Function("return function "+m.replace(/\./g,"_")+"(){window.brazeQueue.push(arguments); return true}"))()}window.braze.getCachedContentCards=function(){return new window.braze.ContentCards};window.braze.getCachedFeed=function(){return new window.braze.Feed};window.braze.getUser=function(){return new window.braze.User};(y=p.createElement(P)).type='text/javascript';
-      y.src='https://js.appboycdn.com/web-sdk/4.2/braze.min.js';
-      y.async=1;(b=p.getElementsByTagName(P)[0]).parentNode.insertBefore(y,b)
-    }(window,document,'script');
+    // eslint-disable-next-line func-names
+    +(function (a, p, P, b, y) {
+      a.braze = {};
+      a.brazeQueue = [];
+      for (let s = BrazeOperationString.split(" "), i = 0; i < s.length; i++) {
+        for (
+          var m = s[i], k = a.braze, l = m.split("."), j = 0;
+          j < l.length - 1;
+          j++
+        )
+          k = k[l[j]];
+        k[l[j]] = new Function(
+          `return function ${m.replace(
+            /\./g,
+            "_"
+          )}(){window.brazeQueue.push(arguments); return true}`
+        )();
+      }
+      window.braze.getCachedContentCards = function () {
+        return new window.braze.ContentCards();
+      };
+      window.braze.getCachedFeed = function () {
+        return new window.braze.Feed();
+      };
+      window.braze.getUser = function () {
+        return new window.braze.User();
+      };
+      (y = p.createElement(P)).type = "text/javascript";
+      y.src = "https://js.appboycdn.com/web-sdk/4.2/braze.min.js";
+      y.async = 1;
+      (b = p.getElementsByTagName(P)[0]).parentNode.insertBefore(y, b);
+    })(window, document, "script");
 
     window.braze.initialize(this.appKey, {
       enableLogging: true,
       baseUrl: this.endPoint,
+      enableHtmlInAppMessages: this.enableHtmlInAppMessages,
+      allowUserSuppliedJavascript: this.allowUserSuppliedJavascript,
     });
     window.braze.automaticallyShowInAppMessages();
 
     const { userId } = this.analytics;
     // send userId if you have it https://js.appboycdn.com/web-sdk/latest/doc/module-appboy.html#.changeUser
-    if (userId)
-      braze.changeUser(userId);
+    if (userId) {
+      window.braze.changeUser(userId);
+    }
     window.braze.openSession();
   }
 
@@ -85,6 +116,7 @@ class Braze {
     ];
 
     reserved.forEach((element) => {
+      // eslint-disable-next-line no-param-reassign
       delete props[element];
     });
     return props;
@@ -102,7 +134,7 @@ class Braze {
 
     // This is a hack to make a deep copy that is not recommended because it will often fail:
     const traits = JSON.parse(
-        JSON.stringify(rudderElement.message.context.traits)
+      JSON.stringify(rudderElement.message.context.traits)
     );
 
     window.braze.changeUser(userId);
@@ -118,12 +150,13 @@ class Braze {
       window.braze.getUser().setHomeCity(address.city);
     }
     if (birthday) {
-      window.braze.getUser()
-          .setDateOfBirth(
-              birthday.getUTCFullYear(),
-              birthday.getUTCMonth() + 1,
-              birthday.getUTCDate()
-          );
+      window.braze
+        .getUser()
+        .setDateOfBirth(
+          birthday.getUTCFullYear(),
+          birthday.getUTCMonth() + 1,
+          birthday.getUTCDate()
+        );
     }
 
     // remove reserved keys https://www.appboy.com/documentation/Platform_Wide/#reserved-keys
@@ -178,11 +211,11 @@ class Braze {
       const { quantity } = product;
       if (quantity && price && productId)
         window.braze.logPurchase(
-            productId,
-            price,
-            currencyCode,
-            quantity,
-            properties
+          productId,
+          price,
+          currencyCode,
+          quantity,
+          properties
         );
     });
   }
@@ -193,8 +226,7 @@ class Braze {
     let { properties } = rudderElement.message;
 
     window.braze.changeUser(userId);
-
-    if (eventName.toLowerCase() === "order completed") {
+    if (eventName && eventName.toLowerCase() === "order completed") {
       this.handlePurchase(properties, userId);
     } else {
       properties = this.handleReservedProperties(properties);
