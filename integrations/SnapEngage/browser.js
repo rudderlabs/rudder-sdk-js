f7eaaf1e8ddc1c372ca17579a1a28e747ce3f76d
RudderStack <rohithkaza@gmail.com>
2022-08-30 17:35:12 +0530

    bugfix[snapengage]: adding name field in constructor

--- integrations/SnapEngage/browser.js
+++ integrations/SnapEngage/browser.js
@@ -14,6 +14,7 @@ class SnapEngage {
     this.recordLiveChatEvents = config.recordLiveChatEvents;
     this.eventsToStandard = config.eventsToStandard;
     this.updateEventNames = config.updateEventNames;
+    this.name = NAME;
   }
 
   loadScript() {
