f2f750c94b977b9bdaf5c6e623cb3854254f7541
aashishmalik <imashishmalik.45@gmail.com>
2022-09-07 12:47:08 +0530

    feature[Shynet]:increased size

--- .size-limit.js
+++ .size-limit.js
@@ -13,7 +13,7 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/integrations/*.min.js',
     gzip: true,
-    limit: '850 kB',
+    limit: '900 kB',
   },
   {
     name: 'Core - NPM',
