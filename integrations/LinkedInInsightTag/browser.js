a0f812fcccb7a668154d26b5de7f777bacefa1c1
Saurav Lal <sauravlal@rudderstack.com>
2022-09-12 14:52:03 +0530

    feature/(LinkedIn_insight_tag): add support for event-specific converison

--- integrations/LinkedInInsightTag/browser.js
+++ integrations/LinkedInInsightTag/browser.js
@@ -1,12 +1,14 @@
 /* eslint-disable class-methods-use-this */
 import logger from "../../utils/logUtil";
 import ScriptLoader from "../ScriptLoader";
+import { getHashFromArray } from "../utils/commonUtils";
 import { NAME } from "./constants";
 
 class LinkedInInsightTag {
   constructor(config) {
     this.name = NAME;
     this.partnerId = config.partnerId;
+    this.eventConversionId = config.eventConversionId;
   }
 
   init() {
@@ -17,6 +19,9 @@ class LinkedInInsightTag {
     );
     if (!this.partnerId) {
       return;
+    }
+    if (this.eventConversionId === undefined) {
+      this.eventConversionId = [];
     }
     window._linkedin_data_partner_id = this.partnerId;
   }
@@ -30,5 +35,32 @@ class LinkedInInsightTag {
     logger.debug("=== in isReady LinkedIn Insight Tag===");
     return !!window._linkedin_data_partner_id;
   }
+
+  track(rudderElement) {
+    logger.debug("===In LinkedIn Insight Tag Track===");
+    const { message } = rudderElement;
+    const { event } = message;
+    if (!event) {
+      logger.error(
+        "[LinkedIn Insight Tag]: Event name is missing for track call!!==="
+      );
+      return;
+    }
+    const trimmedEvent = event.trim();
+    const eventMapping = getHashFromArray(
+      this.eventConversionId,
+      "from",
+      "to",
+      false
+    );
+    if (!eventMapping[trimmedEvent]) {
+      logger.error(
+        `Event ${trimmedEvent} doesn't matches with LinkedIn Insight Tag!!===`
+      );
+      return;
+    }
+    window.lintrk("track", { conversion_id: eventMapping[trimmedEvent] });
+  }
 }
+
 export default LinkedInInsightTag;
