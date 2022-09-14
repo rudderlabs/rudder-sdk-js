96b8feeee723e2cb128495fc452177df50f6f79d
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2022-09-14 14:13:56 +0530

    feat(integration): add common util function for mapping event names in ads destinations (#643)
    * add common util function for mapping, onboard googleads changes
    
    * add snap pixel support for ui mapping
    
    * add reddit  pixel support for ui mapping
    
    * utilise the added common util in the integrations
    
    * add eventsToEvents mapping configuration to facebook pixel
    
    * support one to many mapping for events, revert fb pixel changes
    
    * address review comments
    
    * remove unused declarations, refactor
    
    * Better nomenclature for function

--- integrations/GoogleAds/browser.js
+++ integrations/GoogleAds/browser.js
@@ -1,7 +1,11 @@
 /* eslint-disable class-methods-use-this */
 import logger from "../../utils/logUtil";
 import { LOAD_ORIGIN } from "../ScriptLoader";
-import { removeUndefinedAndNullValues } from "../utils/commonUtils";
+import {
+  getHashFromArrayWithDuplicate,
+  removeUndefinedAndNullValues,
+  getEventMappingFromConfig,
+} from "../utils/commonUtils";
 import { NAME } from "./constants";
 
 class GoogleAds {
@@ -16,6 +20,7 @@ class GoogleAds {
     this.conversionLinker = config.conversionLinker || true;
     this.disableAdPersonalization = config.disableAdPersonalization || false;
     this.name = NAME;
+    this.eventMappingFromConfig = config.eventMappingFromConfig;
   }
 
   init() {
@@ -88,16 +93,29 @@ class GoogleAds {
         logger.error("Event name not present");
         return;
       }
-
+      // modify the event name to mapped event name from the config
+      const eventsHashmap = getHashFromArrayWithDuplicate(
+        this.eventMappingFromConfig,
+        "from",
+        "to",
+        false
+      );
       let payload = {};
       const sendToValue = this.conversionId;
 
       if (rudderElement.message.properties) {
         payload = rudderElement.message.properties;
       }
-
       payload.send_to = sendToValue;
-      window.gtag("event", event, payload);
+
+      const events = getEventMappingFromConfig(event, eventsHashmap);
+      if (events) {
+        events.forEach((ev) => {
+          window.gtag("event", ev, payload);
+        });
+      } else {
+        window.gtag("event", event, payload);
+      }
     }
   }
 
