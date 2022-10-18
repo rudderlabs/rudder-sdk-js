22ad1ee483afed1c680ba07dae0fba0d311776a3
Saurav Lal <sauravlal@rudderstack.com>
2022-10-18 19:00:53 +0530

    group call support

--- integrations/Posthog/browser.js
+++ integrations/Posthog/browser.js
@@ -59,7 +59,7 @@ class Posthog {
           }
           ((p = t.createElement("script")).type = "text/javascript"),
             (p.async = !0),
-            p.setAttribute("data-loader", LOAD_ORIGIN),
+            (p.setAttribute("data-loader", LOAD_ORIGIN)),
             (p.src = s.api_host + "/static/array.js"),
             (r = t.getElementsByTagName("script")[0]).parentNode.insertBefore(
               p,
