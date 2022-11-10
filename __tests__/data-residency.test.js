7591e7dd62aa4ef2621123b2ff6dc2e0bf806cd8
Moumita <mandal.moumita2013@gmail.com>
2022-11-10 14:25:23 +0530

    refactor: test cases

--- __tests__/data-residency.test.js
+++ __tests__/data-residency.test.js
@@ -3,6 +3,7 @@ import { DEFAULT_DATAPLANE_URL } from '../utils/constants';
 
 const usDataplaneUrl = 'https://sample.rudderlabs.com/us';
 const euDataplaneUrl = 'https://sample.rudderlabs.com/eu';
+const serverUrl = 'https://sample.rudderlabs.com';
 
 const sourceConfigWithBothDataPlane = {
   source: {
@@ -48,85 +49,149 @@ const sourceConfigWithEUDataPlane = {
     },
   },
 };
+const sourceConfigWithEmptyDataPlanes = { source: {} };
 
-const serverUrl = 'https://sample.rudderlabs.com';
+const invalidResidencyServer1 = { residencyServer: 1234 };
+const invalidResidencyServer2 = { residencyServer: 'test' };
+const validResidencyServerUS = { residencyServer: 'US' };
+const validResidencyServerEU = { residencyServer: 'EU' };
 
-test('When no source config dataplane url and no load call url default will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} });
-  expect(url).toEqual(DEFAULT_DATAPLANE_URL);
-});
+const testCaseData = [
+  {
+    description:
+      'When dataplane is not provided in source config or in load API, default dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+    },
+    output: DEFAULT_DATAPLANE_URL,
+  },
+  {
+    description:
+      'When dataplane is not provided in source config but provided in load API, dataplane Url from load option will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      serverUrl: serverUrl,
+    },
+    output: serverUrl,
+  },
+  {
+    description: 'When dataplane is provided in source config that dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithBothDataPlane,
+      serverUrl: serverUrl,
+    },
+    output: usDataplaneUrl,
+  },
+  {
+    description:
+      'In case of invalid residencyServer option: dataplane is not provided in source config or in load API, default dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      options: invalidResidencyServer1,
+    },
+    output: DEFAULT_DATAPLANE_URL,
+  },
+  {
+    description:
+      'In case of invalid residencyServer option: dataplane is not provided in source config but provided in load API, dataplane Url from load option will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      serverUrl: serverUrl,
+      options: invalidResidencyServer2,
+    },
+    output: serverUrl,
+  },
+  {
+    description:
+      'In case of invalid residencyServer option: dataplane is provided in source config that dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithBothDataPlane,
+      serverUrl: serverUrl,
+      options: invalidResidencyServer1,
+    },
+    output: usDataplaneUrl,
+  },
+  {
+    description:
+      'In case of valid residencyServer(US) option: dataplane is not provided in source config or in load API, default dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      options: validResidencyServerUS,
+    },
+    output: DEFAULT_DATAPLANE_URL,
+  },
+  {
+    description:
+      'In case of valid residencyServer(US) option: dataplane is not provided in source config but provided in load API, dataplane Url from load option will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      serverUrl: serverUrl,
+      options: validResidencyServerUS,
+    },
+    output: serverUrl,
+  },
+  {
+    description:
+      'In case of valid residencyServer(US) option: dataplane is provided in source config that dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithBothDataPlane,
+      serverUrl: serverUrl,
+      options: validResidencyServerUS,
+    },
+    output: usDataplaneUrl,
+  },
+  {
+    description:
+      'In case of valid residencyServer(US) option: dataplane is provided but US region not available in source config then load API dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithEUDataPlane,
+      serverUrl: serverUrl,
+      options: validResidencyServerUS,
+    },
+    output: serverUrl,
+  },
+  {
+    description:
+      'In case of valid residencyServer(EU) option: dataplane is not provided in source config or in load API, default dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      options: validResidencyServerEU,
+    },
+    output: DEFAULT_DATAPLANE_URL,
+  },
+  {
+    description:
+      'In case of valid residencyServer(EU) option: dataplane is not provided in source config but provided in load API, dataplane Url from load option will be selected',
+    input: {
+      response: sourceConfigWithEmptyDataPlanes,
+      serverUrl: serverUrl,
+      options: validResidencyServerEU,
+    },
+    output: serverUrl,
+  },
+  {
+    description:
+      'In case of valid residencyServer(EU) option: dataplane is provided in source config that dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithBothDataPlane,
+      serverUrl: serverUrl,
+      options: validResidencyServerEU,
+    },
+    output: euDataplaneUrl,
+  },
+  {
+    description:
+      'In case of valid residencyServer(EU) option: dataplane is provided only for US region not available in source config then US region dataplane Url will be selected',
+    input: {
+      response: sourceConfigWithUSDataPlane,
+      serverUrl: serverUrl,
+      options: validResidencyServerEU,
+    },
+    output: usDataplaneUrl,
+  },
+];
 
-test('When no source config dataplane url and url provided in load call url that url will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, serverUrl);
-  expect(url).toEqual(serverUrl);
-});
-
-test('When source config dataplane url is provided that will be choosen irrespective of load call url', () => {
-  const url = resolveDataPlaneUrl(sourceConfigWithBothDataPlane, serverUrl);
-  expect(url).toEqual(usDataplaneUrl);
-});
-
-test('When option provides invalid residency server input + no source config dataplane url + no load call url: default will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, undefined, { residencyServer: 1234 });
-  expect(url).toEqual(DEFAULT_DATAPLANE_URL);
-});
-
-test('When option provides invalid residency server input + no source config dataplane url + load call url provided : load call url will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, serverUrl, { residencyServer: 'test' });
-  expect(url).toEqual(serverUrl);
-});
-
-test('When option provides invalid residency server input + source config dataplane url + load call url provided : source config dataplane url will be choosen', () => {
-  const url = resolveDataPlaneUrl(sourceConfigWithBothDataPlane, serverUrl, {
-    residencyServer: 'test',
-  });
-  expect(url).toEqual(usDataplaneUrl);
-});
-
-test('When option provides valid residency server input as US + no source config dataplane url + no load call url: default will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, undefined, { residencyServer: 'US' });
-  expect(url).toEqual(DEFAULT_DATAPLANE_URL);
-});
-
-test('When option provides valid residency server input as US + no source config dataplane url + load call url provided : load call url will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, serverUrl, { residencyServer: 'US' });
-  expect(url).toEqual(serverUrl);
-});
-
-test('When option provides valid residency server input as US + source config dataplane url with both US and EU + load call url provided : source config dataplane url for US region will be choosen', () => {
-  const url = resolveDataPlaneUrl(sourceConfigWithBothDataPlane, serverUrl, {
-    residencyServer: 'US',
-  });
-  expect(url).toEqual(usDataplaneUrl);
-});
-
-test('When option provides valid residency server input as US + source config dataplane url only EU + load call url provided : load call url will be choosen', () => {
-  const url = resolveDataPlaneUrl(sourceConfigWithEUDataPlane, serverUrl, {
-    residencyServer: 'US',
-  });
-  expect(url).toEqual(serverUrl);
-});
-
-test('When option provides valid residency server input as EU + no source config dataplane url + no load call url: default will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, undefined, { residencyServer: 'EU' });
-  expect(url).toEqual(DEFAULT_DATAPLANE_URL);
-});
-
-test('When option provides valid residency server input as EU + no source config dataplane url + load call url provided : load call url will be choosen', () => {
-  const url = resolveDataPlaneUrl({ source: {} }, serverUrl, { residencyServer: 'EU' });
-  expect(url).toEqual(serverUrl);
-});
-
-test('When option provides valid residency server input as EU + source config dataplane url with both US and EU + load call url provided : source config dataplane url for US region will be choosen', () => {
-  const url = resolveDataPlaneUrl(sourceConfigWithBothDataPlane, serverUrl, {
-    residencyServer: 'EU',
-  });
-  expect(url).toEqual(euDataplaneUrl);
-});
-
-test('When option provides valid residency server input as EU + source config dataplane url with only US + load call url provided : US region url will be choosen', () => {
-  const url = resolveDataPlaneUrl(sourceConfigWithUSDataPlane, serverUrl, {
-    residencyServer: 'EU',
-  });
-  expect(url).toEqual(usDataplaneUrl);
+test.each(testCaseData)('$description', ({ input, output }) => {
+  const url = resolveDataPlaneUrl(input.response, input.serverUrl, input.options);
+  expect(url).toEqual(output);
 });
