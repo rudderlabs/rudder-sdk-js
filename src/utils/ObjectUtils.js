4271f18215db3ae04e7ef73b7776d3a21c52e6d9
George Bardis <gbardis@rudderstack.com>
2022-12-01 10:09:46 +0200

    fix: ensure mergeDeepRight behaves as lodash merge

--- src/utils/ObjectUtils.js
+++ src/utils/ObjectUtils.js
@@ -1,22 +1,20 @@
-import * as R from "ramda";
+import * as R from 'ramda';
 
 const mergeDeepRightObjectArrays = (leftValue, rightValue) => {
-  if(!Array.isArray(leftValue) || !Array.isArray(rightValue)) {
+  if (!Array.isArray(leftValue) || !Array.isArray(rightValue)) {
     return R.clone(rightValue);
   }
 
   const mergedArray = R.clone(leftValue);
-  rightValue.forEach((value , index) => {
-    mergedArray[index] = R.clone(value);
+  rightValue.forEach((value, index) => {
+    // eslint-disable-next-line no-use-before-define
+    mergedArray[index] = mergeDeepRight(mergedArray[index], value);
   });
 
   return mergedArray;
-}
+};
 
 const mergeDeepRight = (leftObject, rightObject) =>
   R.mergeDeepWith(mergeDeepRightObjectArrays, leftObject, rightObject);
 
-export {
-  mergeDeepRightObjectArrays,
-  mergeDeepRight
-}
+export { mergeDeepRightObjectArrays, mergeDeepRight };
