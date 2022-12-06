34f1e1f6970b0f979946fc3d675d2314967d0dec
saikumarrs <saibattinoju@rudderstack.com>
2022-12-06 09:06:39 +0530

    refactor: remove redundant usage of common names

--- analytics.js
+++ analytics.js
@@ -28,7 +28,6 @@ import {
   checkReservedKeywords,
   getReferrer,
   getReferringDomain,
-  commonNames,
   get,
   getStringId,
 } from "./utils/utils";
@@ -734,10 +733,7 @@ class Analytics {
     if (!eventName || !(typeof eventName === "string")) {
       return false;
     }
-    const sdkIntgName = commonNames[intgName];
-    const intg = this.clientIntegrations.find(
-      (intg) => intg.name === sdkIntgName
-    );
+    const intg = this.clientIntegrations.find((intg) => intg.name === intgName);
 
     const { blacklistedEvents, whitelistedEvents, eventFilteringOption } =
       intg.config;
