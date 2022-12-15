016ea05f0c8d3c42bbe594e33adc1422a4db4ce5
Gauravudia <gauravudia@rudderstack.com>
2022-12-15 13:46:15 +0530

    chore: max size increased

--- .size-limit.js
+++ .size-limit.js
@@ -19,13 +19,13 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '610 kB',
+    limit: '630 kB',
   },
   {
     name: 'All Integrations - CDN (modern only)',
     path: 'dist/modern/js-integrations/*.min.js',
     gzip: true,
-    limit: '490 kB',
+    limit: '505 kB',
   },
   {
     name: 'Core - NPM',
