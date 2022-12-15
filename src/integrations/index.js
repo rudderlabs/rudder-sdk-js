73140949b6ef7a96bd4bd5fbd3ba7f401348ae44
Gauravudia <60897972+Gauravudia@users.noreply.github.com>
2022-12-15 11:22:38 +0530

    feat(new integration): onboarding sendinblue web device mode destination (#785)
    * feat(new integration): onboarding sendinblue web device mode destination
    
    * feat: update canonical maping
    
    * feat: addressing review comments
    
    * feat: addressing review comments
    
    * feat: removing email from track call
    
    * feat: removing name check in page call

--- src/integrations/index.js
+++ src/integrations/index.js
@@ -67,6 +67,7 @@ import * as Qualaroo from "./Qualaroo";
 import * as Satismeter from "./Satismeter";
 import * as MicrosoftClarity from "./MicrosoftClarity";
 import * as Axeptio from "./Axeptio";
+import * as Sendinblue from "./Sendinblue";
 
 // the key names should match the destination.name value to keep partity everywhere
 // (config-plan name, native destination.name , exported integration name(this one below))
@@ -141,6 +142,7 @@ const integrations = {
   SATISMETER: Satismeter.default,
   MICROSOFT_CLARITY: MicrosoftClarity.default,
   AXEPTIO: Axeptio.default,
+  SENDINBLUE: Sendinblue.default
 };
 
 export { integrations };
