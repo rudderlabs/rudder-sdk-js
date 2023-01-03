3c6a5cb181f84715f6bfef20018c790027637f68
Gauravudia <60897972+Gauravudia@users.noreply.github.com>
2023-01-03 20:12:18 +0530

    feat(pinterest): add ldp support (#812)
    * feat(pinterest): add ldp support
    
    * chore: max size increased

--- .size-limit.js
+++ .size-limit.js
@@ -19,13 +19,13 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '616 kB',
+    limit: '621 kB',
   },
   {
     name: 'All Integrations - CDN (modern only)',
     path: 'dist/modern/js-integrations/*.min.js',
     gzip: true,
-    limit: '490 kB',
+    limit: '492 kB',
   },
   {
     name: 'Core - NPM',
