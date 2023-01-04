1d06e279dc95efec10b95c4614536256b6aa5fbd
Mihir Bhalala <77438541+mihir-4116@users.noreply.github.com>
2023-01-04 14:03:17 +0530

    feat: onboard olark web device mode (#763)
    * feat: onboard olark web device mode
    
    * feat: size limit changes
    
    * feat: code review changes
    
    * fix: size-limit issue

--- .size-limit.js
+++ .size-limit.js
@@ -19,13 +19,13 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '621 kB',
+    limit: '647 kB',
   },
   {
     name: 'All Integrations - CDN (modern only)',
     path: 'dist/modern/js-integrations/*.min.js',
     gzip: true,
-    limit: '492 kB',
+    limit: '512 kB',
   },
   {
     name: 'Core - NPM',
