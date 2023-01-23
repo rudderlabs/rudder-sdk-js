6696c3be096c92c719d12c2285cf3d05ffdd09c0
yashasvibajpai <yashasvi.bajpai1999@gmail.com>
2023-01-23 10:55:54 +0530

    feat(integration): rockerbox - add support for custom properties from UI, minor refactors

--- src/integrations/Rockerbox/browser.js
+++ src/integrations/Rockerbox/browser.js
@@ -1,9 +1,12 @@
+/* eslint-disable prefer-rest-params */
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable class-methods-use-this */
-import logger from '../../utils/logUtil';
-import { NAME } from './constants';
-import { LOAD_ORIGIN } from '../../utils/ScriptLoader';
-import { getHashFromArray } from '../../utils/commonUtils';
+import logger from "../../utils/logUtil";
+import { NAME } from "./constants";
+import { LOAD_ORIGIN } from "../../utils/ScriptLoader";
+import { getHashFromArray, isDefinedAndNotNullAndNotEmpty } from "../../utils/commonUtils";
 
 class Rockerbox {
   constructor(config) {
@@ -12,6 +15,7 @@ class Rockerbox {
     this.customDomain = config.customDomain;
     this.enableCookieSync = config.enableCookieSync;
     this.eventsMap = config.eventsMap || [];
+    this.customPropsMapping = config.customPropsMapping || [];
     this.useNativeSDKToSend = config.useNativeSDKToSend;
   }
 
@@ -58,16 +62,16 @@ class Rockerbox {
   identify(rudderElement) {
     logger.debug('===In Rockerbox Identify===');
     const { message } = rudderElement;
-    const { userId, anonymousId } = message;
+    const { userId, anonymousId, traits, context } = message;
     if (!userId) {
       logger.debug('userId is needed. A primary identifier is expected by RockerBox');
     }
-    const email = message.traits?.email || message.context?.traits?.email;
-    window.RB.track('identify', {
+    const email = traits?.email || context?.traits?.email;
+    window.RB.track("identify", {
       external_id: userId,
       anonymousId,
       email,
-      phone_number: message.traits?.phone || message.context?.traits?.phone,
+      phone_number: traits?.phone || context?.traits?.phone,
     });
   }
 
@@ -81,25 +85,38 @@ class Rockerbox {
     logger.debug('===In Rockerbox track===');
 
     const { message } = rudderElement;
-    const { event, anonymousId } = message;
+    const { event, anonymousId, properties } = message;
+    let finalProperties = properties;
     if (!event) {
       logger.error('Event name not present');
       return;
     }
     const eventsHashmap = getHashFromArray(this.eventsMap);
+    const customPropsHashMap = getHashFromArray(this.customPropsMapping);
+
     const rbEvent = eventsHashmap[event.toLowerCase()];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in customPropsHashMap) {
+      if (isDefinedAndNotNullAndNotEmpty(properties[key])){
+          finalProperties[customPropsHashMap[key]] = properties[key]
+          delete finalProperties.key;
+      }
+    }
+
     if (rbEvent) {
-      window.RB.track(rbEvent, { ...message.properties, anonymousId });
+      window.RB.track(rbEvent, { ...finalProperties, anonymousId });
     } else {
-      logger.error(`The event ${message.event} is not mapped to any Rockerbox Event. Aborting!`);
+      logger.error(
+        `The event ${event} is not mapped to any Rockerbox Event. Aborting!`
+      );
     }
   }
 
   page(rudderElement) {
     logger.debug('=== In Rockerbox Page ===');
     const { message } = rudderElement;
-    const { anonymousId } = message;
-    window.RB.track('view', { ...message.properties, anonymousId });
+    const { anonymousId, properties } = message;
+    window.RB.track("view", { ...properties, anonymousId });
   }
 }
 
