441a9bd42352d7f2f3f9841612c1a6d611a1b43e
mihir-4116 <bhalalamark@gmail.com>
2023-01-31 14:20:34 +0530

    feat: added support for both click event and dynamic remarketing events

--- src/integrations/GoogleAds/browser.js
+++ src/integrations/GoogleAds/browser.js
@@ -6,11 +6,15 @@ import {
   removeUndefinedAndNullValues,
   getEventMappingFromConfig,
 } from "../utils/commonUtils";
+import {
+  shouldSendConversionEvent,
+  shouldSendDynamicRemarketingEvent,
+  getConversionData,
+} from "./utils";
 import { NAME } from "./constants";
 
 class GoogleAds {
   constructor(config) {
-    // this.accountId = config.accountId;//AW-696901813
     this.conversionId = config.conversionID;
     this.pageLoadConversions = config.pageLoadConversions;
     this.clickEventConversions = config.clickEventConversions;
@@ -20,160 +24,54 @@ class GoogleAds {
     this.conversionLinker = config.conversionLinker || true;
     this.disableAdPersonalization = config.disableAdPersonalization || false;
     this.name = NAME;
+    this.trackConversions = config.trackConversions || false;
+    this.trackDynamicRemarketing = config.trackDynamicRemarketing || false;
+    this.enableConversionEventsFiltering = config.enableConversionEventsFiltering || false;
+    this.enableDynamicRemarketingEventsFiltering =
+      config.enableDynamicRemarketingEventsFiltering || false;
+    this.eventsToTrackConversions = config.eventsToTrackConversions || [];
+    this.eventsToTrackDynamicRemarketing = config.eventsToTrackDynamicRemarketing || [];
     this.eventMappingFromConfig = config.eventMappingFromConfig;
+    // Depreciating: Added to make changes backward compatible
+    this.dynamicRemarketing = config.dynamicRemarketing;
   }
 
   init() {
     const sourceUrl = `https://www.googletagmanager.com/gtag/js?id=${this.conversionId}`;
     (function (id, src, document) {
       logger.debug(`in script loader=== ${id}`);
-      const js = document.createElement("script");
+      const js = document.createElement('script');
       js.src = src;
       js.async = 1;
-      js.setAttribute("data-loader", LOAD_ORIGIN);
-      js.type = "text/javascript";
+      js.setAttribute('data-loader', LOAD_ORIGIN);
+      js.type = 'text/javascript';
       js.id = id;
-      const e = document.getElementsByTagName("head")[0];
-      logger.debug("==script==", e);
+      const e = document.getElementsByTagName('head')[0];
+      logger.debug('==script==', e);
       e.appendChild(js);
-    })("googleAds-integration", sourceUrl, document);
+    })('googleAds-integration', sourceUrl, document);
 
     window.dataLayer = window.dataLayer || [];
     window.gtag = function () {
+      // eslint-disable-next-line prefer-rest-params
       window.dataLayer.push(arguments);
     };
-    window.gtag("js", new Date());
+    window.gtag('js', new Date());
 
     // Additional Settings
 
-    const config = {};
-    config.send_page_view = this.sendPageView;
-    config.conversion_linker = this.conversionLinker;
+    const config = {
+      send_page_view: this.sendPageView,
+      conversion_linker: this.conversionLinker,
+    };
 
     if (this.disableAdPersonalization) {
-      window.gtag("set", "allow_ad_personalization_signals", false);
+      window.gtag('set', 'allow_ad_personalization_signals', false);
     }
 
-    window.gtag("config", this.conversionId, config);
+    window.gtag('config', this.conversionId, config);
 
-    logger.debug("===in init Google Ads===");
-  }
-
-  identify() {
-    logger.debug("[GoogleAds] identify:: method not supported");
-  }
-
-  // https://developers.google.com/gtagjs/reference/event
-  track(rudderElement) {
-    logger.debug("in GoogleAdsAnalyticsManager track");
-
-    // Dynamic remarketing disabled
-    if (!this.dynamicRemarketing) {
-      const conversionData = this.getConversionData(
-        this.clickEventConversions,
-        rudderElement.message.event
-      );
-      if (conversionData.conversionLabel) {
-        const { conversionLabel } = conversionData;
-        const { eventName } = conversionData;
-        const sendToValue = `${this.conversionId}/${conversionLabel}`;
-        let properties = {};
-        if (rudderElement.message.properties) {
-          properties.value = rudderElement.message.properties.revenue;
-          properties.currency = rudderElement.message.properties.currency;
-          properties.transaction_id = rudderElement.message.properties.order_id;
-        }
-        properties.send_to = sendToValue;
-        properties = removeUndefinedAndNullValues(properties);
-        window.gtag("event", eventName, properties);
-      }
-    } else {
-      const { event } = rudderElement.message;
-      if (!event) {
-        logger.error("Event name not present");
-        return;
-      }
-      // modify the event name to mapped event name from the config
-      const eventsHashmap = getHashFromArrayWithDuplicate(
-        this.eventMappingFromConfig,
-        "from",
-        "to",
-        false
-      );
-      let payload = {};
-      const sendToValue = this.conversionId;
-
-      if (rudderElement.message.properties) {
-        payload = rudderElement.message.properties;
-      }
-      payload.send_to = sendToValue;
-
-      const events = getEventMappingFromConfig(event, eventsHashmap);
-      if (events) {
-        events.forEach((ev) => {
-          window.gtag("event", ev, payload);
-        });
-      } else {
-        window.gtag("event", event, payload);
-      }
-    }
-  }
-
-  page(rudderElement) {
-    logger.debug("in GoogleAdsAnalyticsManager page");
-
-    // Dynamic re-marketing is disabled
-    if (!this.dynamicRemarketing) {
-      const conversionData = this.getConversionData(
-        this.pageLoadConversions,
-        rudderElement.message.name
-      );
-      if (conversionData.conversionLabel) {
-        const { conversionLabel } = conversionData;
-        const { eventName } = conversionData;
-        window.gtag("event", eventName, {
-          send_to: `${this.conversionId}/${conversionLabel}`,
-        });
-      }
-    } else {
-      const event = rudderElement.message.name;
-      if (!event) {
-        logger.error("Event name not present");
-        return;
-      }
-
-      let payload = {};
-      const sendToValue = this.conversionId;
-
-      if (rudderElement.message.properties) {
-        payload = rudderElement.message.properties;
-      }
-
-      payload.send_to = sendToValue;
-      window.gtag("event", event, payload);
-    }
-  }
-
-  getConversionData(eventTypeConversions, eventName) {
-    const conversionData = {};
-    if (eventTypeConversions) {
-      if (eventName) {
-        eventTypeConversions.forEach((eventTypeConversion) => {
-          if (
-            eventTypeConversion.name.toLowerCase() === eventName.toLowerCase()
-          ) {
-            // rudderElement["message"]["name"]
-            conversionData.conversionLabel =
-              eventTypeConversion.conversionLabel;
-            conversionData.eventName = eventTypeConversion.name;
-          }
-        });
-      } else if (this.defaultPageConversion) {
-        conversionData.conversionLabel = this.defaultPageConversion;
-        conversionData.eventName = "Viewed a Page";
-      }
-    }
-    return conversionData;
+    logger.debug('===in init Google Ads===');
   }
 
   isLoaded() {
@@ -181,7 +79,133 @@ class GoogleAds {
   }
 
   isReady() {
-    return window.dataLayer.push !== Array.prototype.push;
+    return this.isLoaded();
+  }
+
+  identify() {
+    logger.debug('[GoogleAds] identify:: method not supported');
+  }
+
+  // https://developers.google.com/gtagjs/reference/event
+  track(rudderElement) {
+   logger.debug('in GoogleAdsAnalyticsManager track');
+
+   const { event } = rudderElement.message;
+   const conversionData = getConversionData(
+     this.clickEventConversions,
+     event,
+     this.defaultPageConversion,
+   );
+   if (
+     conversionData.conversionLabel &&
+     shouldSendConversionEvent(
+       event,
+       this.trackConversions,
+       this.enableConversionEventsFiltering,
+       this.eventsToTrackConversions,
+       this.dynamicRemarketing,
+     )
+   ) {
+     const { conversionLabel } = conversionData;
+     const { eventName } = conversionData;
+     const sendToValue = `${this.conversionId}/${conversionLabel}`;
+
+     let properties = {
+       value: rudderElement.message?.properties?.revenue,
+       currency: rudderElement.message?.properties?.currency,
+       transaction_id: rudderElement.message?.properties?.order_id,
+       send_to: sendToValue,
+     };
+     properties = removeUndefinedAndNullValues(properties);
+
+     window.gtag('event', eventName, properties);
+   }
+
+   if (!event) {
+     logger.error('Event name not present');
+     return;
+   }
+
+   if (
+     shouldSendDynamicRemarketingEvent(
+       event,
+       this.trackDynamicRemarketing,
+       this.enableDynamicRemarketingEventsFiltering,
+       this.eventsToTrackDynamicRemarketing,
+       this.dynamicRemarketing,
+     )
+   ) {
+     // modify the event name to mapped event name from the config
+     const eventsHashmap = getHashFromArrayWithDuplicate(
+       this.eventMappingFromConfig,
+       'from',
+       'to',
+       false,
+     );
+
+     const { properties } = rudderElement.message;
+     const payload = properties || {};
+     const sendToValue = this.conversionId;
+
+     payload.send_to = sendToValue;
+     const events = getEventMappingFromConfig(event, eventsHashmap);
+     if (events) {
+       events.forEach((ev) => {
+         window.gtag('event', ev, payload);
+       });
+     } else {
+       window.gtag('event', event, payload);
+     }
+   }
+  }
+
+  page(rudderElement) {
+     logger.debug('in GoogleAdsAnalyticsManager page');
+
+    const { name } = rudderElement.message;
+    const conversionData = getConversionData(
+      this.clickEventConversions,
+      name,
+      this.defaultPageConversion,
+    );
+    if (
+      conversionData.conversionLabel &&
+      shouldSendConversionEvent(
+        name,
+        this.trackConversions,
+        this.enableConversionEventsFiltering,
+        this.eventsToTrackConversions,
+        this.dynamicRemarketing,
+      )
+    ) {
+      const { conversionLabel } = conversionData;
+      const { eventName } = conversionData;
+      window.gtag('event', eventName, {
+        send_to: `${this.conversionId}/${conversionLabel}`,
+      });
+    }
+
+    if (!name) {
+      logger.error('Event name not present');
+      return;
+    }
+
+    if (
+      shouldSendDynamicRemarketingEvent(
+        name,
+        this.trackDynamicRemarketing,
+        this.enableDynamicRemarketingEventsFiltering,
+        this.eventsToTrackDynamicRemarketing,
+        this.dynamicRemarketing,
+      )
+    ) {
+      const event = name;
+      const { properties } = rudderElement.message;
+      const sendToValue = this.conversionId;
+      const payload = properties || {};
+      payload.send_to = sendToValue;
+      window.gtag('event', event, payload);
+    }
   }
 }
 
