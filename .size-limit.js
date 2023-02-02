55e5297aca36625cb3790ca0ac80fab323dad978
Mihir Bhalala <77438541+mihir-4116@users.noreply.github.com>
2023-02-02 15:29:16 +0530

    feat: added support for both click event and dynamic remarketing events (#854)
    * feat: added support for both click event and dynamic remarketing events
    
    * feat: code review changes
    
    * feat: getConversionData function moved to utilities file
    
    * feat: size limit reduced by 0.5 kb
    
    * feat: reorganize googleads init and isloaded functions
    
    * feat: unit tests added for getConversionData function
    
    * feat: google ads track call test cases added
    
    * feat: code review changes
    
    * feat: default values added
    
    * feat: size-limit fix
    
    * feat: test file names refactored
    
    * feat: backward compatible changes added
    
    * feat: code review changes
    
    * feat: test cases added to handle edge scenarios where both old and new config exists together
    
    * feat: reverted testbook.js changes
    
    * feat: reverted testbook.js changes
    
    * reverted TestBook.js changes
    
    * fix: size limit

--- .size-limit.js
+++ .size-limit.js
@@ -13,7 +13,7 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '685.5 kB',
+    limit: '703.15 kB',
   },
   {
     name: 'Core - NPM',
