4da78920ec602f10980bd7e9e4c9a37e75070c0d
Anant <anant@rudderstack.com>
2023-02-03 10:25:29 +0530

    fix: All Integration CDN Size increased

--- .size-limit.js
+++ .size-limit.js
@@ -13,7 +13,7 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '703.15 kB',
+    limit: '710.15 kB',
   },
   {
     name: 'Core - NPM',
