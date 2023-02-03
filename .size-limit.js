2d18bc05dd915113338b9c6907d745cde9bdc803
shrouti1507 <60211312+shrouti1507@users.noreply.github.com>
2023-02-03 18:11:01 +0530

    feat(test-suite): adding test cases for hugely used util functions for device mode integrations (#860)
    * feat(test-suit): adding test cases for hugely used util functions for device mode integrations
    
    * feat(test-suit): adding test cases for flattenJsonPayload
    
    * feat(test-suit): editing format
    
    * feat(test-suite): review comments addressed
    
    * fix(size-limit): size limit increase for All Integrations - CDN
    
    ---------
    
    Co-authored-by: George Bardis <109069547+bardisg@users.noreply.github.com>

--- .size-limit.js
+++ .size-limit.js
@@ -13,7 +13,7 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '710.15 kB',
+    limit: '710.5 kB',
   },
   {
     name: 'Core - NPM',
