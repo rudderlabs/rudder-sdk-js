4747468def95145fec9a7966704d273ccc5f0da2
mihir-4116 <bhalalamark@gmail.com>
2023-02-28 18:37:39 +0530

    fix: traits are not being sent to ga4 group call

--- src/integrations/GA4/browser.js
+++ src/integrations/GA4/browser.js
@@ -24,7 +24,6 @@ export default class GA4 {
     this.sendUserId = config.sendUserId || false;
     this.blockPageView = config.blockPageViewEvent || false;
     this.extendPageViewParams = config.extendPageViewParams || false;
-    this.extendGroupPayload = config.extendGroupPayload || false;
     this.debugMode = config.debugMode || false;
     this.isHybridModeEnabled = config.useNativeSDKToSend === false || false;
     this.name = NAME;
@@ -270,7 +269,7 @@ export default class GA4 {
     getDestinationEventName(rudderElement.message.type).forEach((events) => {
       this.sendGAEvent(events.dest, {
         group_id: groupId,
-        ...(this.extendGroupPayload ? traits : {}),
+        ...(traits || {}),
       });
     });
   }
