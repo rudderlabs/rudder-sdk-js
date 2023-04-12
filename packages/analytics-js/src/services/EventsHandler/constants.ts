c8af4b7fde71060408299084ef1f0126198cb19a
saikumarrs <saibattinoju@rudderstack.com>
2023-04-12 13:04:53 +0530

    feat: add necessary constants

---
+++ packages/analytics-js/src/services/EventsHandler/constants.ts
@@ -0,0 +1,7 @@
+export const TRACK = "track";
+export const PAGE = "page";
+export const IDENTIFY = "identify";
+export const GROUP = "group";
+export const ALIAS = "alias";
+
+export const CHANNEL = "web";
