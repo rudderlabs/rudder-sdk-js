54cce8c1e06e5c3daea2220ab914d07bc933d41b
saikumarrs <saibattinoju@rudderstack.com>
2023-04-13 15:47:37 +0530

    fix: add missing file

---
+++ packages/analytics-js/src/components/eventManager/constants.ts
@@ -0,0 +1,17 @@
+export const CHANNEL = 'web';
+
+export const TOP_LEVEL_ELEMENTS = ['integrations', 'anonymousId', 'originalTimestamp'];
+
+// TODO: Check if more elements like 'app' can be added
+export const SYSTEM_KEYWORDS = ['library', 'consentManagement'];
+
+export const RESERVED_ELEMENTS = [
+  'anonymous_id',
+  'id', // TODO: Is this valid?
+  'sent_at',
+  'received_at',
+  'timestamp',
+  'original_timestamp',
+  'event_text',
+  'event',
+];
