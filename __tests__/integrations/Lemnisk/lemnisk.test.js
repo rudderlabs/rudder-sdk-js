ed9e579efd95180365d577a741589a75b2c4659f
Anant Jain <anantjain45823@gmail.com>
2023-04-18 12:52:37 +0530

    chore: added bing ads test cases

--- __tests__/integrations/Lemnisk/lemnisk.test.js
+++ __tests__/integrations/Lemnisk/lemnisk.test.js
@@ -6,7 +6,7 @@ afterAll(() => {
 describe('lemnisk init tests', () => {
     let lemnisk;
 
-    test('Testing init call of Google Ads with ConversionId', () => {
+    test('Testing init call of Lemnisk', () => {
         lemnisk = new Lemnisk({ accountId: "12567839", writeKey: "04789yt8rfhbkwjenkl" }, { loglevel: "debug" });
         lemnisk.init();
         expect(typeof window.lmSMTObj).toBe('object');
@@ -35,7 +35,6 @@ describe("lemnisk page", () => {
                 },
             },
         });
-        console.log(JSON.stringify(window.lmSMTObj.page.mock.calls)); // this has set with empty {} object when resetCustomDimensions
         expect(window.lmSMTObj.page.mock.calls[0][0]).toEqual({
             "category": "test cat",
             "path": "/test",
@@ -55,7 +54,6 @@ describe("Lemnisk Track event", () => {
         window.lmSMTObj.track = jest.fn();
     });
     test("Testing Track Custom Events", () => {
-
         lemnisk.track({
             message: {
                 context: {},
@@ -90,7 +88,6 @@ describe("Lemnisk Track event", () => {
                 },
             }
         });
-        console.log(JSON.stringify(window.lmSMTObj.track.mock.calls));
         expect(window.lmSMTObj.track.mock.calls[0][0]).toEqual("Custom");
         expect(window.lmSMTObj.track.mock.calls[0][1]).toEqual({
             "customProp": "testProp",
@@ -120,7 +117,6 @@ describe("Lemnisk Track event", () => {
                 }
             ]
         });
-
     });
 });
 describe("Lemnisk Identify event", () => {
