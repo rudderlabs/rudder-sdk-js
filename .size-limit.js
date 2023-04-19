207b31b3b3df348d30f4ced1c3e0d8ae90c698b4
Moumita <mandal.moumita2013@gmail.com>
2023-04-19 12:44:44 +0530

    chore: size limit updated

--- .size-limit.js
+++ .size-limit.js
@@ -7,7 +7,7 @@ module.exports = [
     name: 'Core - CDN',
     path: 'dist/legacy/rudder-analytics.min.js',
     gzip: true,
-    limit: '39.5 kB',
+    limit: '39.6 kB',
   },
   {
     name: 'All Integrations - CDN',
@@ -19,7 +19,7 @@ module.exports = [
     name: 'Core - NPM',
     path: 'dist/npm-lib/index.js',
     gzip: true,
-    limit: '39.5 kB',
+    limit: '39.6 kB',
   },
   {
     name: 'Service Worker - NPM',
