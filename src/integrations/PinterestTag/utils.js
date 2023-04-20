99fa23bf1144939b962782c5212747ee6e3d0b79
Gauravudia <gauravudia@rudderstack.com>
2023-04-20 17:52:05 +0530

    chore: add test case

---
+++ src/integrations/PinterestTag/utils.js
@@ -0,0 +1,52 @@
+import logger from '../../utils/logUtil';
+import { getHashFromArrayWithDuplicate } from '../../utils/commonUtils';
+import { isDefinedAndNotNull } from '../../utils/utils';
+import { eventMapping } from './propertyMappingConfig';
+
+function getDestinationEventName(event, userDefinedEventsMapping, sendAsCustomEvent) {
+  let eventNames;
+  /*
+    Step 1: At first we will look for
+            the event mapping in the UI. In case it is similar, will map to that.
+     */
+  if (userDefinedEventsMapping.length > 0) {
+    const keyMap = getHashFromArrayWithDuplicate(userDefinedEventsMapping, 'from', 'to', false);
+    eventNames = keyMap[event];
+  }
+  if (eventNames) {
+    return eventNames;
+  }
+  /*
+    Step 2: To find if the particular event is amongst the list of standard
+            Rudderstack ecommerce events, used specifically for Pinterest Conversion API
+            mappings.
+    */
+  const eventMapInfo = eventMapping.find((eventMap) => {
+    if (eventMap.src.includes(event.toLowerCase())) {
+      return eventMap;
+    }
+    return false;
+  });
+  if (isDefinedAndNotNull(eventMapInfo)) {
+    return [eventMapInfo.dest];
+  }
+
+  /*
+    Step 3: In case both of the above stated cases fail, will check if sendAsCustomEvent toggle is enabled in UI. 
+            If yes, then we will send it as custom event
+    */
+  if (sendAsCustomEvent) {
+    return ['Custom'];
+  }
+
+  /*
+    Step 4: In case all of the above stated cases fail, will send the event name as it is.
+          This is going to be reflected as "unknown" event in pinterest tag dashboard.
+    */
+  logger.warn(
+    `'${event}' is not mapped in UI. Make sure to mapped the event in UI or enable the 'send as custom event' setting`,
+  );
+  return [event];
+}
+
+export { getDestinationEventName };
