f840a18f55c5e1a392a3cf993d625384de6766c5
saikumarrs <saibattinoju@rudderstack.com>
2023-04-20 20:57:16 +0530

    refactor: revert the type change

--- packages/analytics-js/src/components/userSessionManager/types.ts
+++ packages/analytics-js/src/components/userSessionManager/types.ts
@@ -1,5 +1,5 @@
 import { IStore } from '@rudderstack/analytics-js/services/StoreManager/types';
-import { AnonymousIdOptions, ApiObject, SessionInfo } from '@rudderstack/analytics-js/state/types';
+import { AnonymousIdOptions, ApiObject, ApiOptions, SessionInfo } from '@rudderstack/analytics-js/state/types';
 import { Nullable } from '@rudderstack/analytics-js/types';
 
 export interface IUserSessionManager {
@@ -12,7 +12,7 @@ export interface IUserSessionManager {
   getUserId(): Nullable<string>;
   setUserId(userId?: null | string): void;
   setUserTraits(traits?: Nullable<ApiObject>): void;
-  getGroupTraits(): Nullable<ApiObject>;
+  getGroupTraits(): Nullable<ApiOptions>;
   setGroupId(groupId?: Nullable<string>): void;
   setGroupTraits(traits?: Nullable<ApiObject>): void;
   reset(resetAnonymousId?: boolean, noNewSessionStart?: boolean): void;
