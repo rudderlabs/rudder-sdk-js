f41adaabcb8e57f8ead02e1e84b13f01ce9c46f8
mihir-4116 <bhalalamark@gmail.com>
2023-05-18 11:19:35 +0530

    fix(ga4): condition based cookie override

--- src/integrations/GA4/browser.js
+++ src/integrations/GA4/browser.js
@@ -52,13 +52,13 @@ export default class GA4 {
     if (sendUserIdToGA4(this.analytics.loadOnlyIntegrations) && this.analytics.userId) {
       gtagParameterObject.user_id = this.analytics.userId;
     }
-    gtagParameterObject.cookie_prefix = 'rs';
+
     if (this.isHybridModeEnabled && this.overrideClientAndSessionId) {
+      gtagParameterObject.cookie_prefix = 'rs';
       gtagParameterObject.client_id = this.analytics.anonymousId;
-    }
-    if (this.isHybridModeEnabled && this.overrideClientAndSessionId) {
       gtagParameterObject.session_id = this.analytics.uSession.sessionInfo.id;
     }
+
     gtagParameterObject.debug_mode = true;
 
     if (Object.keys(gtagParameterObject).length === 0) {
