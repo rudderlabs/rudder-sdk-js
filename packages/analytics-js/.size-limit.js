ad2e580b6eebca8006bda4bc4879a18c51efe6c1
saikumarrs <saibattinoju@rudderstack.com>
2023-05-19 11:30:00 +0530

    chore(ci): update size limits

--- packages/analytics-js/.size-limit.js
+++ packages/analytics-js/.size-limit.js
@@ -7,13 +7,13 @@ module.exports = [
     name: 'Core Legacy - CDN',
     path: 'dist/legacy/iife/index.js',
     gzip: true,
-    limit: '35 kB',
+    limit: '37 kB',
   },
   {
     name: 'Core - NPM',
     path: 'dist/legacy/esm/index.js',
     gzip: true,
-    limit: '35 kB',
+    limit: '37 kB',
   },
   {
     name: 'Core - CDN',
