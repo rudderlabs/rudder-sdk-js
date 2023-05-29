8987d4cf54bc84e378f33f87130c013ce61feda4
Priyam <priyam@rudderstack.com>
2023-05-30 01:49:21 +0530

    added TC for Amplitude and Braze Destination

--- sanity-suite/src/testBookSuites/eventFiltering.js
+++ sanity-suite/src/testBookSuites/eventFiltering.js
@@ -1,4 +1,5 @@
 import eventFiltering1ExpectedData from '../../__mocks__/eventFiltering1.json';
+import eventFiltering1ExpectedData2 from '../../__mocks__/eventFiltering2.json';
 
 const eventFilteringSuite = {
   id: 'eventFilteringFeature',
@@ -8,7 +9,7 @@ const eventFilteringSuite = {
   testCases: [
     {
       id: 'eventFiltering',
-      description: 'Call with all arguments and all primitives in properties and apiOptions',
+      description: 'GA Destination : Call with all arguments and all primitives in properties and apiOptions',
       inputData: [
         'OrderCompleted_AutomationQA_1',
         {
@@ -81,6 +82,45 @@ const eventFilteringSuite = {
         },
       ],
       expectedResult: eventFiltering1ExpectedData,
+      triggerHandler: 'track',
+    },
+    {
+      id: 'eventFiltering1',
+      description: 'Braze, Amplitude Destination: Call with all arguments and all primitives in properties and apiOptions',
+      inputData: [
+        'OrderCompleted_AutomationQA_3',
+        {
+          "respond_with":
+          {
+              "user_id": "customUserID",
+              "config":
+              {
+                  "config_time": 1676538416
+              }
+          },
+          "events":
+          [
+              {
+                  "name": "ce",
+                  "time": 1685206153.972,
+                  "data":
+                  {
+                      "n": "OrderCompleted_AutomationQA_1",
+                      "p":
+                      {}
+                  },
+                  "session_id": "27f97032-ef00-707c-e96f-164ecf6fa992",
+                  "user_id": "customUserID"
+              }
+          ],
+          "api_key": "6121fa4a-9fe9-4341-a4f5-641e3ba888e6",
+          "time": 1685206163,
+          "sdk_version": "4.2.1",
+          "device_id": "ca39f465-c889-f719-5cf1-d4bcd7246c86",
+          "user_id": "customUserID"
+      }
+      ],
+      expectedResult: eventFiltering1ExpectedData2,
       triggerHandler: 'track',
     }
   ],
