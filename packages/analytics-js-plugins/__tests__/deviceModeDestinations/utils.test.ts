101ec54e43f938ce7d150a5c46d5c1cfd17748bb
saikumarrs <saibattinoju@rudderstack.com>
2023-06-08 11:24:27 +0530

    test: add unit tests for createDestinationInstance

--- packages/analytics-js-plugins/__tests__/deviceModeDestinations/utils.test.ts
+++ packages/analytics-js-plugins/__tests__/deviceModeDestinations/utils.test.ts
@@ -3,9 +3,11 @@ import {
   normalizeIntegrationOptions,
   wait,
   isDestinationReady,
+  createDestinationInstance
 } from '@rudderstack/analytics-js-plugins/deviceModeDestinations/utils';
 import * as dmdConstants from '@rudderstack/analytics-js-plugins/deviceModeDestinations/constants';
-import { Destination } from '@rudderstack/analytics-js-plugins/types/common';
+import { Destination, IRudderAnalytics } from '@rudderstack/analytics-js-plugins/types/common';
+import { signal } from '@preact/signals-core';
 
 describe('deviceModeDestinations utils', () => {
   describe('filterDestinations', () => {
@@ -297,6 +299,95 @@ describe('deviceModeDestinations utils', () => {
       await expect(isReadyPromise).rejects.toThrow(
         new Error(`Destination "${destination.userFriendlyId}" ready check timed out`),
       );
+    });
+  });
+
+  describe('createDestinationInstance', () => {
+    class mockAnalytics {
+      page = () => {};
+      track = () => {};
+      identify = () => {};
+      group = () => {};
+      alias = () => {};
+    }
+
+    const mockAnalyticsInstance_writeKey1 = new mockAnalytics();
+    const mockAnalyticsInstance_writeKey2 = new mockAnalytics();
+
+
+    class mockRudderAnalytics {
+      getAnalyticsInstance = (writeKey) => {
+        const instancesMap = {
+          '1234567890': mockAnalyticsInstance_writeKey1,
+          '12345678910': mockAnalyticsInstance_writeKey2,
+        };
+        return instancesMap[writeKey];
+        }
+    }
+
+    const mockRudderAnalyticsInstance = new mockRudderAnalytics();
+
+    (window as any).rudderanalytics = mockRudderAnalyticsInstance;
+
+    const destSDKIdentifier = 'GA4_RS';
+    const sdkTypeName = 'GA4';
+
+    (window as any)[destSDKIdentifier] = {
+      [sdkTypeName]: class {
+        config: any;
+        analytics: any;
+        constructor(config, analytics) {
+          this.config = config;
+          this.analytics = analytics;
+        }
+      }
+    };
+
+    it('should return an instance of the destination', () => {
+      const state = {
+        nativeDestinations: {
+          loadIntegration: signal(true),
+          loadOnlyIntegrations: signal({ All: true }),
+        },
+        lifecycle: {
+          logLevel: signal('DEBUG'),
+          writeKey: signal('12345678910'), // mockAnalyticsInstance_writeKey2
+        },
+      };
+
+      const destination = {
+        config: {
+          apiKey: '1234',
+        },
+        areTransformationsConnected: false,
+        id: 'GA4___5678',
+      }
+
+      const destinationInstance = createDestinationInstance(destSDKIdentifier, sdkTypeName, destination, state);
+
+      expect(destinationInstance).toBeInstanceOf((window as any)[destSDKIdentifier][sdkTypeName]);
+      expect(destinationInstance.config).toEqual(destination.config);
+      expect(destinationInstance.analytics).toEqual({
+        loadIntegration: true,
+        loadOnlyIntegrations: { All: true },
+        logLevel: 'DEBUG',
+        alias: expect.any(Function),
+        group: expect.any(Function),
+        identify: expect.any(Function),
+        page: expect.any(Function),
+        track: expect.any(Function),
+        getAnonymousId: expect.any(Function),
+        getGroupId: expect.any(Function),
+        getUserId: expect.any(Function),
+        getUserTraits: expect.any(Function),
+        getGroupTraits: expect.any(Function),
+        getSessionId: expect.any(Function),
+      });
+
+      // Making sure that the call gets forwarded to the correct instance
+      jest.spyOn(mockAnalyticsInstance_writeKey2, 'page');
+      destinationInstance.analytics.page();
+      expect(mockAnalyticsInstance_writeKey2.page).toHaveBeenCalled();
     });
   });
 });
