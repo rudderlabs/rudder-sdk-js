fe586e92dd8ef1ba121ea7ac844f1b8ff8a61b8a
Utsab Chowdhury <utsab.c97@gmail.com>
2023-06-16 17:37:02 +0530

    feat: add test case and clean-up

--- src/integrations/Braze/browser.js
+++ src/integrations/Braze/browser.js
@@ -1,11 +1,11 @@
 import isEqual from 'lodash.isequal';
 import * as R from 'ramda';
 import Logger from '../../utils/logger';
-import { LOAD_ORIGIN } from '../../utils/ScriptLoader';
-import { BrazeOperationString, NAME } from './constants';
+
+import { NAME } from './constants';
 import Storage from '../../utils/storage/index';
 import { isObject } from '../../utils/utils';
-import { handlePurchase, formatGender, handleReservedProperties } from './utils';
+import { handlePurchase, formatGender, handleReservedProperties, loadBraze } from './utils';
 
 const logger = new Logger(NAME);
 
@@ -45,38 +45,7 @@ class Braze {
 
   init() {
     logger.debug('===in init Braze===');
-
-    // load braze
-    /* eslint-disable */
-    +(function (a, p, P, b, y) {
-      a.braze = {};
-      a.brazeQueue = [];
-      for (let s = BrazeOperationString.split(' '), i = 0; i < s.length; i++) {
-        for (var m = s[i], k = a.braze, l = m.split('.'), j = 0; j < l.length - 1; j++) k = k[l[j]];
-        k[l[j]] = new Function(
-          `return function ${m.replace(
-            /\./g,
-            '_',
-          )}(){window.brazeQueue.push(arguments); return true}`,
-        )();
-      }
-      window.braze.getCachedContentCards = function () {
-        return new window.braze.ContentCards();
-      };
-      window.braze.getCachedFeed = function () {
-        return new window.braze.Feed();
-      };
-      window.braze.getUser = function () {
-        return new window.braze.User();
-      };
-      (y = p.createElement(P)).type = 'text/javascript';
-      y.src = 'https://js.appboycdn.com/web-sdk/4.2/braze.min.js';
-      y.async = 1;
-      y.setAttribute('data-loader', LOAD_ORIGIN);
-      (b = p.getElementsByTagName(P)[0]).parentNode.insertBefore(y, b);
-    })(window, document, 'script');
-    /* eslint-enable */
-
+    loadBraze();
     window.braze.initialize(this.appKey, {
       enableLogging: this.enableBrazeLogging,
       baseUrl: this.endPoint,
