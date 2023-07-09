b6a59e95da45c81302c2d02fe5e23f483f6e29e3
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-07-09 23:16:23 +0530

    chore(Fullstory): move sdk loader to different file

--- src/integrations/Fullstory/browser.js
+++ src/integrations/Fullstory/browser.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-undef */
 import camelcase from '../../utils/camelcase';
 import logger from '../../utils/logUtil';
-import { LOAD_ORIGIN } from '../../utils/ScriptLoader';
 import { NAME } from './constants';
+import { loader } from './loader';
 
 class Fullstory {
   constructor(config, analytics, destinationInfo) {
@@ -61,64 +61,8 @@ class Fullstory {
     window._fs_script = 'edge.fullstory.com/s/fs.js';
     window._fs_org = this.fs_org;
     window._fs_namespace = 'FS';
-    (function (m, n, e, t, l, o, g, y) {
-      if (e in m) {
-        if (m.console && m.console.log) {
-          m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');
-        }
-        return;
-      }
-      g = m[e] = function (a, b, s) {
-        g.q ? g.q.push([a, b, s]) : g._api(a, b, s);
-      };
-      g.q = [];
-      o = n.createElement(t);
-      o.async = 1;
-      o.crossOrigin = 'anonymous';
-      o.src = `https://${_fs_script}`;
-      o.setAttribute('data-loader', LOAD_ORIGIN);
-      y = n.getElementsByTagName(t)[0];
-      y.parentNode.insertBefore(o, y);
-      g.identify = function (i, v, s) {
-        g(l, { uid: i }, s);
-        if (v) g(l, v, s);
-      };
-      g.setUserVars = function (v, s) {
-        g(l, v, s);
-      };
-      g.event = function (i, v, s) {
-        g('event', { n: i, p: v }, s);
-      };
-      g.shutdown = function () {
-        g('rec', !1);
-      };
-      g.restart = function () {
-        g('rec', !0);
-      };
-      g.log = function (a, b) {
-        g('log', [a, b]);
-      };
-      g.consent = function (a) {
-        g('consent', !arguments.length || a);
-      };
-      g.identifyAccount = function (i, v) {
-        o = 'account';
-        v = v || {};
-        v.acctId = i;
-        g(o, v);
-      };
-      g.clearUserCookie = function () {};
-      g._w = {};
-      y = 'XMLHttpRequest';
-      g._w[y] = m[y];
-      y = 'fetch';
-      g._w[y] = m[y];
-      if (m[y])
-        m[y] = function () {
-          return g._w[y].apply(this, arguments);
-        };
-    })(window, document, window._fs_namespace, 'script', 'user');
 
+    loader(window, document, window._fs_namespace, 'script', 'user');
     const { FULLSTORY } = this.analytics.loadOnlyIntegrations;
     // Checking if crossDomainSupport is their or not.
     if (FULLSTORY?.crossDomainSupport === true) {
