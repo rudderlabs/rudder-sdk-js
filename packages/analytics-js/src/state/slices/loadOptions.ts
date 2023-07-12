0341fc8a35433209a402f497cd92865bcec9f20f
Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
2023-07-12 16:45:26 +0530

    fix: native destinations queue options (#1209)
    * fix: use correct queue options for native destinations queue
    
    * refactor: remove unwanted constants

--- packages/analytics-js/src/state/slices/loadOptions.ts
+++ packages/analytics-js/src/state/slices/loadOptions.ts
@@ -1,10 +1,6 @@
 import { Signal, signal } from '@preact/signals-core';
 import { clone } from 'ramda';
-import {
-  DEFAULT_BEACON_QUEUE_FLUSH_INTERVAL,
-  DEFAULT_SESSION_TIMEOUT,
-} from '@rudderstack/analytics-js/constants/timeouts';
-import { DEFAULT_BEACON_QUEUE_MAX_SIZE } from '@rudderstack/analytics-js/constants/sizes';
+import { DEFAULT_SESSION_TIMEOUT } from '@rudderstack/analytics-js/constants/timeouts';
 import {
   CookieSameSite,
   LoadOptions,
@@ -23,10 +19,6 @@ const defaultLoadOptions: LoadOptions = {
   sessions: {
     autoTrack: true,
     timeout: DEFAULT_SESSION_TIMEOUT,
-  },
-  beaconQueueOptions: {
-    maxItems: DEFAULT_BEACON_QUEUE_MAX_SIZE,
-    flushQueueInterval: DEFAULT_BEACON_QUEUE_FLUSH_INTERVAL,
   },
   sameSiteCookie: CookieSameSite.Lax,
   polyfillIfRequired: true,
