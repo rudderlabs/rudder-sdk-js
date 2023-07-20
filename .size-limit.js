89d6224e24a5944fd944fc675b8b3d9da814faab
ItsSudip <sudip.paul1997@gmail.com>
2023-07-20 13:58:07 +0530

    feat(VWO): update size-limit

--- .size-limit.js
+++ .size-limit.js
@@ -7,7 +7,7 @@ module.exports = [
     name: 'Core - CDN',
     path: 'dist/legacy/rudder-analytics.min.js',
     gzip: true,
-    limit: '41 kB',
+    limit: '37.1 kB',
   },
   {
     name: 'All Integrations - CDN',
