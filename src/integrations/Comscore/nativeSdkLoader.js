e10781db223847e49704a16bd821e4fdda9c5296
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-07-24 11:21:55 +0530

    chore: refactor code

--- src/integrations/Comscore/nativeSdkLoader.js
+++ src/integrations/Comscore/nativeSdkLoader.js
@@ -1,11 +1,10 @@
 import { LOAD_ORIGIN } from '../../utils/ScriptLoader';
 
 function loadNativeSdk() {
-  if (document.location.protocol == 'https:') {
-    ScriptLoader('comscore', 'https://sb.scorecardresearch.com/beacon.js');
-  } else {
-    ScriptLoader('comscore', 'http://b.scorecardresearch.com/beacon.js');
-  }
+  const src = `${
+    document.location.protocol == 'https:' ? 'https://sb' : 'http://b'
+  }.scorecardresearch.com/beacon.js`;
+  ScriptLoader('comscore', src);
 }
 
 export { loadNativeSdk };
