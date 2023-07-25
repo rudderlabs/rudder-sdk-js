18f67e6b01691f4453ea7ef3154955d2a9c37573
Gauravudia <gauravudia@rudderstack.com>
2023-07-25 12:55:11 +0530

    fix: revert mixpanel alias id ordering

--- src/integrations/Mixpanel/browser.js
+++ src/integrations/Mixpanel/browser.js
@@ -371,25 +371,27 @@ class Mixpanel {
   }
 
   /**
+   * https://github.com/mixpanel/mixpanel-js/blob/master/doc/readme.io/javascript-full-api-reference.md#mixpanelalias
    * @param {*} rudderElement
    */
   alias(rudderElement) {
     logger.debug('in Mixpanel alias');
     const { previousId, userId } = rudderElement.message;
+    const newId = userId;
     if (!previousId) {
       logger.debug('===Mixpanel: previousId is required for alias call===');
       return;
     }
-    if (!userId) {
+    if (!newId) {
       logger.debug('===Mixpanel: userId is required for alias call===');
       return;
     }
 
-    if (window.mixpanel.get_distinct_id && window.mixpanel.get_distinct_id() === userId) {
+    if (window.mixpanel.get_distinct_id && window.mixpanel.get_distinct_id() === newId) {
       logger.debug('===Mixpanel: userId is same as previousId. Skipping alias ===');
       return;
     }
-    window.mixpanel.alias(previousId, userId);
+    window.mixpanel.alias(newId, previousId);
   }
 }
 export default Mixpanel;
