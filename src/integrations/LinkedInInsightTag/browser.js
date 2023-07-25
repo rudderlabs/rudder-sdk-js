a8f992525320ce24fffda498986d96fa3cdfe50a
mihir-4116 <bhalalamark@gmail.com>
2023-07-25 18:42:57 +0530

    fix(launchdarkly, linkedininsighttag, livechat, lotame, lytics): sonar code smells

--- src/integrations/LinkedInInsightTag/browser.js
+++ src/integrations/LinkedInInsightTag/browser.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable class-methods-use-this */
 import logger from '../../utils/logUtil';
 import ScriptLoader from '../../utils/ScriptLoader';
--- src/integrations/Lytics/browser.js
+++ src/integrations/Lytics/browser.js
@@ -1,23 +1,4 @@
-/* eslint-disable no-param-reassign */
 /* eslint-disable class-methods-use-this */
-// disabled these for Lytics js tag
-/* eslint-disable no-plusplus */
-/* eslint-disable block-scoped-var */
-/* eslint-disable no-sequences */
-/* eslint-disable yoda */
-/* eslint-disable prefer-spread */
-// disabling these eslint which are caused by the Lytics js tag
-
-/* eslint-disable prefer-rest-params */
-/* eslint-disable no-var */
-/* eslint-disable vars-on-top */
-/* eslint-disable no-return-assign */
-/* eslint-disable no-shadow */
-/* eslint-disable no-void */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable one-var */
-/* eslint-disable lines-around-directive */
-/* eslint-disable strict */
 import logger from '../../utils/logUtil';
 import { NAME } from './constants';
 import { loadNativeSdk } from './nativeSdkLoader';
@@ -62,43 +43,44 @@ class Lytics {
   identify(rudderElement) {
     logger.debug('in Lytics identify');
     // eslint-disable-next-line camelcase
-    const user_id = rudderElement.message.userId || rudderElement.message.anonymousId;
+    const userId = rudderElement.message.userId || rudderElement.message.anonymousId;
     const { traits } = rudderElement.message.context;
-    const payload = { user_id, ...traits };
-    this.handleName(payload);
+    let payload = { user_id: userId, ...traits };
+    payload = this.handleName(payload);
     window.jstag.send(this.stream, payload);
   }
 
   page(rudderElement) {
     logger.debug('in Lytics page');
     const { properties } = rudderElement.message;
-    const payload = { event: rudderElement.message.name, ...properties };
-    this.handleName(payload);
+    let payload = { event: rudderElement.message.name, ...properties };
+    payload = this.handleName(payload);
     window.jstag.pageView(this.stream, payload);
   }
 
   track(rudderElement) {
     logger.debug('in Lytics track');
     const { properties } = rudderElement.message;
-    const payload = { _e: rudderElement.message.event, ...properties };
-    this.handleName(payload);
+    let payload = { _e: rudderElement.message.event, ...properties };
+    payload = this.handleName(payload);
     window.jstag.send(this.stream, payload);
   }
 
   handleName(payload) {
+    const params = payload;
     this.forFirstName.forEach((key) => {
-      if (payload[key]) {
-        payload.first_name = payload[key];
-        delete payload[key];
+      if (params[key]) {
+        params.first_name = payload[key];
+        delete params[key];
       }
     });
     this.forLastName.forEach((key) => {
-      if (payload[key]) {
-        payload.last_name = payload[key];
-        delete payload[key];
+      if (params[key]) {
+        params.last_name = payload[key];
+        delete params[key];
       }
     });
-    return payload;
+    return params;
   }
 }
 export default Lytics;
