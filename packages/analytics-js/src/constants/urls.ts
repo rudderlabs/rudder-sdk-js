8d4bbe4bbf8be60dbdf0d07244a11da8f1948e5f
George Bardis <gbardis@rudderstack.com>
2023-08-10 17:20:03 +0300

    fix(analytics-js): change default cdn paths to the beta ones

--- packages/analytics-js/src/constants/urls.ts
+++ packages/analytics-js/src/constants/urls.ts
@@ -5,8 +5,11 @@ const SDK_CDN_BASE_URL = 'https://cdn.rudderlabs.com';
 const CDN_ARCH_VERSION_DIR = 'v3';
 const CDN_INT_DIR = 'js-integrations';
 const CDN_PLUGINS_DIR = 'plugins';
-const DEST_SDK_BASE_URL = `${SDK_CDN_BASE_URL}/${CDN_ARCH_VERSION_DIR}/latest/${BUILD_TYPE}/${CDN_INT_DIR}`;
-const PLUGINS_BASE_URL = `${SDK_CDN_BASE_URL}/${CDN_ARCH_VERSION_DIR}/latest/${BUILD_TYPE}/${CDN_PLUGINS_DIR}`;
+const DEST_SDK_BASE_URL = `${SDK_CDN_BASE_URL}/beta/3.0.0-beta/${BUILD_TYPE}/${CDN_INT_DIR}`;
+const PLUGINS_BASE_URL = `${SDK_CDN_BASE_URL}/beta/3.0.0-beta/${BUILD_TYPE}/${CDN_PLUGINS_DIR}`;
+// TODO: change the above to production URLs when beta phase is done
+// const DEST_SDK_BASE_URL = `${SDK_CDN_BASE_URL}/latest/${CDN_ARCH_VERSION_DIR}/${BUILD_TYPE}/${CDN_INT_DIR}`;
+// const PLUGINS_BASE_URL = `${SDK_CDN_BASE_URL}/latest/${CDN_ARCH_VERSION_DIR}/${BUILD_TYPE}/${CDN_PLUGINS_DIR}`;
 const DEFAULT_CONFIG_BE_URL = 'https://api.rudderstack.com';
 
 export {
