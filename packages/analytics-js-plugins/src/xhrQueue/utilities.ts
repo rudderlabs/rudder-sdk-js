2df93cecd8c53ed5df8ec226558a45b471303a96
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2023-08-11 10:51:43 +0530

    feat: add getBatchDeliveryUrl utility method

--- packages/analytics-js-plugins/src/xhrQueue/utilities.ts
+++ packages/analytics-js-plugins/src/xhrQueue/utilities.ts
@@ -4,7 +4,6 @@ import { ResponseDetails } from '@rudderstack/analytics-js-common/types/HttpClie
 import { ILogger } from '@rudderstack/analytics-js-common/types/Logger';
 import { isErrRetryable } from '@rudderstack/analytics-js-common/utilities/http';
 import { removeDuplicateSlashes } from '@rudderstack/analytics-js-common/utilities/url';
-import { RudderEventType } from '../types/plugins';
 import { DATA_PLANE_API_VERSION, DEFAULT_RETRY_QUEUE_OPTIONS, XHR_QUEUE_PLUGIN } from './constants';
 import { XHRQueueItem } from './types';
 import { EVENT_DELIVERY_FAILURE_ERROR_PREFIX } from '../utilities/logMessages';
@@ -12,13 +11,17 @@ import { EVENT_DELIVERY_FAILURE_ERROR_PREFIX } from '../utilities/logMessages';
 const getNormalizedQueueOptions = (queueOpts: QueueOpts): QueueOpts =>
   mergeDeepRight(DEFAULT_RETRY_QUEUE_OPTIONS, queueOpts);
 
-const getDeliveryUrl = (dataplaneUrl: string, eventType: RudderEventType): string => {
+const getDeliveryUrl = (dataplaneUrl: string, endpointType: string): string => {
   const dpUrl = new URL(dataplaneUrl);
   return new URL(
-    removeDuplicateSlashes([dpUrl.pathname, '/', DATA_PLANE_API_VERSION, '/', eventType].join('')),
+    removeDuplicateSlashes(
+      [dpUrl.pathname, '/', DATA_PLANE_API_VERSION, '/', endpointType].join(''),
+    ),
     dpUrl,
   ).href;
 };
+
+const getBatchDeliveryUrl = (dataplaneUrl: string): string => getDeliveryUrl(dataplaneUrl, 'batch');
 
 const logErrorOnFailure = (
   details: ResponseDetails | undefined,
@@ -50,4 +53,4 @@ const logErrorOnFailure = (
   logger?.error(errMsg);
 };
 
-export { getNormalizedQueueOptions, getDeliveryUrl, logErrorOnFailure };
+export { getNormalizedQueueOptions, getDeliveryUrl, logErrorOnFailure, getBatchDeliveryUrl };
