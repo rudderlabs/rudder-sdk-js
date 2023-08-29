a02b8db6b77f5fee84f28d26da679d1b837aa909
Moumita Mandal <moumita@Moumitas-MacBook-Pro-2.local>
2023-08-29 15:24:24 +0530

    chore: removed unnecessary code

--- packages/analytics-js/src/state/slices/storage.ts
+++ packages/analytics-js/src/state/slices/storage.ts
@@ -1,45 +1,5 @@
 import { signal } from '@preact/signals-core';
-import {
-  // StorageEntries,
-  StorageState,
-} from '@rudderstack/analytics-js-common/types/ApplicationState';
-// import { DEFAULT_STORAGE_TYPE } from '@rudderstack/analytics-js-common/types/Storage';
-// import { userSessionStorageKeys } from '@rudderstack/analytics-js/components/userSessionManager/userSessionStorageKeys';
-
-// const defaultEntries: StorageEntries = {
-//   userId: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.userId,
-//   },
-//   userTraits: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.userTraits,
-//   },
-//   anonymousId: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.anonymousId,
-//   },
-//   groupId: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.groupId,
-//   },
-//   groupTraits: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.groupTraits,
-//   },
-//   initialReferrer: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.initialReferrer,
-//   },
-//   initialReferringDomain: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.initialReferringDomain,
-//   },
-//   sessionInfo: {
-//     storage: DEFAULT_STORAGE_TYPE,
-//     key: userSessionStorageKeys.sessionInfo,
-//   },
-// };
+import { StorageState } from '@rudderstack/analytics-js-common/types/ApplicationState';
 
 const storageState: StorageState = {
   encryptionPluginName: signal(undefined),
