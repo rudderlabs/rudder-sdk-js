43118750b1000a7d87e9cbbc40071fe480197275
mihir-4116 <mihirb@rudderstack.com>
2023-09-08 10:06:31 +0530

    fix(GA4): object.formEntries is not a function

--- .size-limit.js
+++ .size-limit.js
@@ -13,7 +13,7 @@ module.exports = [
     name: 'All Integrations - CDN',
     path: 'dist/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '446.5 kB',
+    limit: '447.0 kB',
   },
   {
     name: 'Core - NPM',
