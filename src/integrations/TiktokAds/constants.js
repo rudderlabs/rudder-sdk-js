e28225ccc49dc2ad6c524aaf017d8da62041ba6d
Mihir Bhalala <77438541+mihir-4116@users.noreply.github.com>
2023-09-08 13:24:55 +0530

    feat(INT-580): messageId to event_id mapping support (#1366)
    feat(tiktok_ads): messageId to event_id mapping support

--- src/integrations/TiktokAds/constants.js
+++ src/integrations/TiktokAds/constants.js
@@ -3,67 +3,67 @@ const CNameMapping = {
   [NAME]: NAME,
   TiktokAds: NAME,
   TIKTOK_ADS: NAME,
-  "Tiktok ads": NAME,
-  "Tiktok Ads": NAME,
-  "Tik Tok Ads": NAME,
-  "tik tok ads": NAME,
-  tiktokads: NAME
+  'Tiktok ads': NAME,
+  'Tiktok Ads': NAME,
+  'Tik Tok Ads': NAME,
+  'tik tok ads': NAME,
+  tiktokads: NAME,
 };
 const PARTNER_NAME = 'RudderStack';
 
 const trackMapping = [
   {
-    "destKey": "event_id",
-    "sourceKeys": "properties.eventId"
+    destKey: 'event_id',
+    sourceKeys: ['properties.eventId', 'properties.event_id', 'messageId'],
   },
   {
-    "destKey": "test_event_code",
-    "sourceKeys": "testEventCode"
+    destKey: 'test_event_code',
+    sourceKeys: 'testEventCode',
   },
   {
-    "destKey": "contents",
-    "sourceKeys": "properties.contents"
+    destKey: 'contents',
+    sourceKeys: 'properties.contents',
   },
   {
-    "destKey": "content_category",
-    "sourceKeys": "properties.category"
+    destKey: 'content_category',
+    sourceKeys: 'properties.category',
   },
   {
-    "destKey": "status",
-    "sourceKeys": "properties.status"
+    destKey: 'status',
+    sourceKeys: 'properties.status',
   },
   {
-    "destKey": "content_name",
-    "sourceKeys": "properties.name"
+    destKey: 'content_name',
+    sourceKeys: 'properties.name',
   },
   {
-    "destKey": "content_id",
-    "sourceKeys": ["properties.product_id", "properties.productId"]
+    destKey: 'content_id',
+    sourceKeys: ['properties.product_id', 'properties.productId'],
   },
   {
-    "destKey": "content_type",
-    "sourceKeys": ["properties.contentType", "properties.content_type"]
+    destKey: 'content_type',
+    sourceKeys: ['properties.contentType', 'properties.content_type'],
   },
   {
-    "destKey": "currency",
-    "sourceKeys": "properties.currency"
+    destKey: 'currency',
+    sourceKeys: 'properties.currency',
   },
   {
-    "destKey": "value",
-    "sourceKeys": "properties.value"
+    destKey: 'value',
+    sourceKeys: 'properties.value',
   },
   {
-    "destKey": "description",
-    "sourceKeys": "properties.description"
+    destKey: 'description',
+    sourceKeys: 'properties.description',
   },
   {
-    "destKey": "email",
-    "sourceKeys": ["context.traits.email", "traits.email", "properties.email"]
+    destKey: 'email',
+    sourceKeys: ['context.traits.email', 'traits.email', 'properties.email'],
   },
   {
-    "destKey": "phone",
-    "sourceKeys": ["context.traits.phone", "traits.phone", "properties.phone"]
-  }
+    destKey: 'phone',
+    sourceKeys: ['context.traits.phone', 'traits.phone', 'properties.phone'],
+  },
 ];
 const eventNameMapping = {
   'product added to wishlist': 'AddToWishlist',
@@ -81,6 +81,5 @@ const eventNameMapping = {
   completeregistration: 'CompleteRegistration',
   subscribe: 'Subscribe',
 };
-
 
 export { NAME, CNameMapping, PARTNER_NAME, trackMapping, eventNameMapping };
