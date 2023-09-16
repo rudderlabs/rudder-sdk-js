bd62c31495debde328da382ac55c38eadc90a243
George Bardis <gbardis@rudderstack.com>
2023-09-17 02:17:37 +0300

    chore(monorepo): update check size limits

--- packages/analytics-js-plugins/.size-limit.js
+++ packages/analytics-js-plugins/.size-limit.js
@@ -13,6 +13,6 @@ module.exports = [
     name: 'Remote Module Federated Plugins - CDN',
     path: 'dist/cdn/modern/plugins/rsa-plugins-*.js',
     gzip: true,
-    limit: '15 KiB',
+    limit: '9 KiB',
   },
 ];
