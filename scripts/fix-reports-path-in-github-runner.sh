8ed1fbbe10151e7edf81eea461021c7de8b57f1a
George Bardis <gbardis@rudderstack.com>
2023-10-10 18:05:49 +0300

    chore: fix github runner paths

--- scripts/fix-reports-path-in-github-runner.sh
+++ scripts/fix-reports-path-in-github-runner.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
-# List of package folders
+# Path variables
 defaultPrefixToReplace="/github/workspace"
 defaultAbsolutePathPrefix="home/runner/work/rudder-sdk-js/rudder-sdk-js"
 selfHostedAbsolutePathPrefix="runner/_work/rudder-sdk-js/rudder-sdk-js"
+absolutePathPrefix="$defaultAbsolutePathPrefix"
+# List of package folders
+projectFolderNames=("analytics-js" "analytics-js-common" "analytics-js-integrations" "analytics-js-plugins" "analytics-js-service-worker" "analytics-v1.1")
 
 # List of files to alter
-sed -i "s+$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js/reports/coverage/lcov.info
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js/reports/eslint.json
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js/reports/sonar/results-report.xml
-
-sed -i "s+$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-common/reports/coverage/lcov.info
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-common/reports/eslint.json
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-common/reports/sonar/results-report.xml
-
-sed -i "s+$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-integrations/reports/coverage/lcov.info
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-integrations/reports/eslint.json
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-integrations/reports/sonar/results-report.xml
-
-sed -i "s+$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-plugins/reports/coverage/lcov.info
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-plugins/reports/eslint.json
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-plugins/reports/sonar/results-report.xml
-
-sed -i "s+$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-service-worker/reports/coverage/lcov.info
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-service-worker/reports/eslint.json
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-js-service-worker/reports/sonar/results-report.xml
-
-sed -i "s+$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-v1.1/reports/coverage/lcov.info
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-v1.1/reports/eslint.json
-sed -i "s+/$selfHostedAbsolutePathPrefix+$defaultPrefixToReplace+g" packages/analytics-v1.1/reports/sonar/results-report.xml
+for projectFolder in "${projectFolderNames[@]}"; do
+  sed -i "s+$absolutePathPrefix+$defaultPrefixToReplace+g" "packages/$projectFolder/reports/coverage/lcov.info"
+  sed -i "s+/$absolutePathPrefix+$defaultPrefixToReplace+g" "packages/$projectFolder/reports/eslint.json"
+  sed -i "s+/$absolutePathPrefix+$defaultPrefixToReplace+g" "packages/$projectFolder/reports/sonar/results-report.xml"
+done
