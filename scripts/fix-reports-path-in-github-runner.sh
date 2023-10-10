f4851d22a51ffa95b4ec312b1f9933a51b7c0372
George Bardis <gbardis@rudderstack.com>
2023-10-10 18:19:39 +0300

    ci(monorepo): fix sonar action checks

---
+++ scripts/fix-reports-path-in-github-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Path variables
+defaultPrefixToReplace="/github/workspace"
+defaultAbsolutePathPrefix="home/runner/work/rudder-sdk-js/rudder-sdk-js"
+selfHostedAbsolutePathPrefix="runner/_work/rudder-sdk-js/rudder-sdk-js"
+absolutePathPrefix="$defaultAbsolutePathPrefix"
+# List of package folders
+projectFolderNames=("analytics-js" "analytics-js-common" "analytics-js-integrations" "analytics-js-plugins" "analytics-js-service-worker" "analytics-v1.1")
+
+# List of files to alter
+for projectFolder in "${projectFolderNames[@]}"; do
+  sed -i "s+$absolutePathPrefix+$defaultPrefixToReplace+g" "packages/$projectFolder/reports/coverage/lcov.info"
+  sed -i "s+/$absolutePathPrefix+$defaultPrefixToReplace+g" "packages/$projectFolder/reports/eslint.json"
+  sed -i "s+/$absolutePathPrefix+$defaultPrefixToReplace+g" "packages/$projectFolder/reports/sonar/results-report.xml"
+done
