b83ac6c414fe19aa0367bc06f168d3dcba7e4d85
George Bardis <gbardis@rudderstack.com>
2023-10-17 18:14:31 +0300

    fix(analytics-js): correct declared global extended type

--- packages/analytics-js-common/types/rudderanalytics.d.ts
+++ packages/analytics-js-common/types/rudderanalytics.d.ts
@@ -2,11 +2,12 @@ import type {
   IRudderStackGlobals,
   RudderAnalytics,
   PreloadedEventCall,
+  RudderAnalyticsPreloader,
 } from '@rudderstack/analytics-js';
 
 declare global {
   interface Window {
-    rudderanalytics: RudderAnalytics | PreloadedEventCall[];
+    rudderanalytics: RudderAnalytics | PreloadedEventCall[] | RudderAnalyticsPreloader;
     RudderStackGlobals: IRudderStackGlobals;
     rudderAnalyticsMount: () => void;
     rudderAnalyticsBuildType: 'legacy' | 'modern';
