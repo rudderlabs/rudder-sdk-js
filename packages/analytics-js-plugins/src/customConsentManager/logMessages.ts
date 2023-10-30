2484fd5bcdd656e97ff5afea0207cda149a4e60d
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2023-10-30 12:20:33 +0530

    feat: update custom consent manager plugin to use gcm config

---
+++ packages/analytics-js-plugins/src/customConsentManager/logMessages.ts
@@ -0,0 +1,3 @@
+const DESTINATION_CONSENT_STATUS_ERROR = `Failed to determine the consent status for the destination. Please check the destination configuration and try again.`;
+
+export { DESTINATION_CONSENT_STATUS_ERROR };
