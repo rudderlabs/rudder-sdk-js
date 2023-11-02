ebbaafa682c1ef5664f413d626110cf743b575d2
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2023-11-02 17:02:10 +0530

    fix: storage migration to allow empty string values

--- packages/analytics-js/src/components/userSessionManager/UserSessionManager.ts
+++ packages/analytics-js/src/components/userSessionManager/UserSessionManager.ts
@@ -7,6 +7,7 @@ import {
 } from '@rudderstack/analytics-js-common/utilities/object';
 import {
   isDefinedNotNullAndNotEmptyString,
+  isNullOrUndefined,
   isString,
 } from '@rudderstack/analytics-js-common/utilities/checks';
 import type { IPluginsManager } from '@rudderstack/analytics-js-common/types/PluginsManager';
@@ -189,7 +190,11 @@ class UserSessionManager implements IUserSessionManager {
           this.errorHandler,
           this.logger,
         );
-        if (migratedVal) {
+
+        // Skip setting the value if it is null or undefined
+        // as those values indicate there is no need for migration or
+        // migration failed
+        if (!isNullOrUndefined(migratedVal)) {
           store.set(storageEntry, migratedVal);
         }
       });
