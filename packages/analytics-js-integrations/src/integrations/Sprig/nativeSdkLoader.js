54c0488665c9815293534f5313fdcb9dd4e581d1
mihir-4116 <mihirb@rudderstack.com>
2023-11-07 12:15:58 +0530

    feat: onboard sprig destination

---
+++ packages/analytics-js-integrations/src/integrations/Sprig/nativeSdkLoader.js
@@ -0,0 +1,19 @@
+import { LOAD_ORIGIN } from '@rudderstack/analytics-js-common/v1.1/utils/constants';
+
+function loadNativeSdk(apiKey) {
+    (function (l, e, a, p) {
+        if (window.Sprig) return;
+        window.Sprig = function () { S._queue.push(arguments) }
+        var S = window.Sprig; 
+        S.appId = a; 
+        S._queue = []; 
+        window.UserLeap = S;
+        a = l.createElement('script');
+        a.async = 1; a.src = e + '?id=' + S.appId;
+        a.setAttribute('data-loader', LOAD_ORIGIN);
+        p = l.getElementsByTagName('script')[0];
+        p.parentNode.insertBefore(a, p);
+    })(document, 'https://cdn.sprig.com/shim.js', apiKey);
+}
+
+export { loadNativeSdk };
