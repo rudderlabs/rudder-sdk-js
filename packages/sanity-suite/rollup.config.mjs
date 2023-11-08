6e9f7fcad5a904347aa558f9a64e8c713e795ca5
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2023-11-08 20:18:07 +0530

    chore: add test suites for gcm feature

--- packages/sanity-suite/rollup.config.mjs
+++ packages/sanity-suite/rollup.config.mjs
@@ -20,7 +20,7 @@ const prodCDNURL = 'https://cdn.rudderlabs.com';
 const defaultVersion = 'v1.1';
 const isV3 = process.env.CDN_VERSION_PATH === 'v3';
 // TODO: get this list from public folder subfolders
-const featuresList = ['eventFiltering', 'preloadBuffer', 'dataResidency'];
+const featuresList = ['eventFiltering', 'preloadBuffer', 'dataResidency', 'genericConsentManagement'];
 
 const getDistPath = () => {
   let distPath = process.env.TEST_PACKAGE ? `/${process.env.TEST_PACKAGE}` : '/npm';
@@ -193,6 +193,7 @@ const getBuildConfig = featureName => ({
         __FEATURE_PRELOAD_BUFFER_WRITE_KEY__: process.env.FEATURE_PRELOAD_BUFFER_WRITE_KEY,
         __FEATURE_EVENT_FILTERING_WRITE_KEY__: process.env.FEATURE_EVENT_FILTERING_WRITE_KEY,
         __FEATURE_DATA_RESIDENCY_WRITE_KEY__: process.env.FEATURE_DATA_RESIDENCY_WRITE_KEY,
+        __FEATURE_GCM_WRITE_KEY__: process.env.FEATURE_GCM_WRITE_KEY,
         __DATAPLANE_URL__: process.env.DATAPLANE_URL,
         __CONFIG_SERVER_HOST__: process.env.CONFIG_SERVER_HOST || '',
         __DEST_SDK_BASE_URL__: getDestinationsURL(),
