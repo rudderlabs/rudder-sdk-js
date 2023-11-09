b5b8e7f7fff24ca2e6e097a476f744d427017ddd
mihir-4116 <mihirb@rudderstack.com>
2023-11-09 16:24:28 +0530

    chore: added sprig tests

---
+++ packages/analytics-js-integrations/__tests__/integrations/Sprig/browser.test.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-underscore-dangle */
+import Sprig from '../../../src/integrations/Sprig/browser';
+
+const destinationInfo = {
+  areTransformationsConnected: false,
+  destinationId: 'sample-destination-id',
+};
+
+beforeEach(() => {
+  // Add a dummy script as it is required by the init script
+  const scriptElement = document.createElement('script');
+  scriptElement.type = 'text/javascript';
+  scriptElement.id = 'dummyScript';
+  const headElements = document.getElementsByTagName('head');
+  headElements[0].insertBefore(scriptElement, headElements[0].firstChild);
+  delete window.Sprig;
+});
+describe('Sprig init tests', () => {
+  test('Testing init call of Sprig with apiKey', () => {
+    const sprig = new Sprig({ apiKey: '12345' }, {}, destinationInfo);
+    sprig.init();
+    expect(typeof window.Sprig).toBe('function');
+  });
+});
+
+describe('Sprig tests', () => {
+  let sprig;
+  beforeEach(() => {
+    sprig = new Sprig({ apiKey: '12345' }, {}, destinationInfo);
+    sprig.init();
+  });
+
+  test('Send identify', () => {
+    sprig.identify({
+      message: {
+        userId: 'user@1',
+        context: {
+          traits: {
+            country: 'USA',
+            email: 'test@rudderlabs.com',
+            source: 'js-sdk',
+          },
+        },
+      },
+    });
+    console.log(window.Sprig._queue[1]);
+    expect(window.Sprig._queue[0][1]).toEqual('user@1');
+    expect(window.Sprig._queue[1][1]).toEqual('test@rudderlabs.com');
+    expect(window.Sprig._queue[2][1]).toEqual({
+      country: 'USA',
+      source: 'js-sdk',
+    });
+  });
+
+  test('Send logIn track event', () => {
+    sprig.track({
+      message: {
+        userId: 'user@1',
+        event: 'signed in',
+        properties: {
+          url: 'http://localhost:3004',
+        },
+      },
+    });
+    console.log(window.Sprig._queue[1]);
+    expect(window.Sprig._queue[0][1]).toEqual({
+      eventName: 'signed in',
+      properties: { url: 'http://localhost:3004' },
+      userId: 'user@1',
+    });
+  });
+});
