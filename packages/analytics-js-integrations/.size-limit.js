4a9cd845e3c54d408bdc0a8658c2510eef445b22
mihir-4116 <mihirb@rudderstack.com>
2023-11-10 17:02:00 +0530

    fix: size-limit issue

--- packages/analytics-js-integrations/.size-limit.js
+++ packages/analytics-js-integrations/.size-limit.js
@@ -7,6 +7,6 @@ module.exports = [
     name: 'All Integrations (legacy build) - CDN',
     path: 'dist/cdn/legacy/js-integrations/*.min.js',
     gzip: true,
-    limit: '312 KiB',
+    limit: '315 KiB',
   },
 ];
