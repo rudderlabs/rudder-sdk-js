2fb7d5c98cb7a73d692b1d56bd55dd4e6cc08b2b
Mihir Bhalala <77438541+mihir-4116@users.noreply.github.com>
2023-11-10 12:23:01 +0530

    feat: onboard sprig destination (#1491)
    * feat: onboard sprig destination
    
    * chore: added sprig tests
    
    * chore: added sprig test cases

---
+++ packages/analytics-js-common/src/constants/integrations/Sprig/constants.ts
@@ -0,0 +1,12 @@
+const DIR_NAME = 'Sprig';
+const NAME = 'SPRIG';
+const DISPLAY_NAME = 'Sprig';
+
+const DISPLAY_NAME_TO_DIR_NAME_MAP = { [DISPLAY_NAME]: DIR_NAME };
+const CNameMapping = {
+  [NAME]: NAME,
+  Sprig: NAME,
+  sprig: NAME,
+};
+
+export { NAME, CNameMapping, DISPLAY_NAME_TO_DIR_NAME_MAP, DISPLAY_NAME, DIR_NAME };
