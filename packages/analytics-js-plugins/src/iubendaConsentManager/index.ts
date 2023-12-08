b10f22f979648a7fcf99ef9eeb46c23afe42cc3f
Mohamed Khattab <muhammadkhattab292@outlook.com>
2023-12-08 12:04:20 +0200

    implement plugin methods

--- packages/analytics-js-plugins/src/iubendaConsentManager/index.ts
+++ packages/analytics-js-plugins/src/iubendaConsentManager/index.ts
@@ -8,7 +8,8 @@ import type { IStoreManager } from '@rudderstack/analytics-js-common/types/Store
 import type { IErrorHandler } from '@rudderstack/analytics-js-common/types/ErrorHandler';
 import type { PluginName } from '@rudderstack/analytics-js-common/types/PluginsManager';
 import { IUBENDA_CONSENT_MANAGER_PLUGIN } from './constants';
-import { DESTINATION_CONSENT_STATUS_ERROR } from './logMessages';
+import { checks, storages, string } from '../shared-chunks/common';
+import { DESTINATION_CONSENT_STATUS_ERROR, IUBENDA_ACCESS_ERROR } from './logMessages';
 
 const pluginName: PluginName = 'IubendaConsentManager';
 
@@ -28,7 +29,39 @@ const IubendaConsentManager = (): ExtensionPlugin => ({
       storeManager?: IStoreManager,
       logger?: ILogger,
     ): void {
-      // Nothing to update. Already provided by the user
+      // eslint-disable-next-line no-underscore-dangle
+      if (!(globalThis as any)._iub) {
+        logger?.error(IUBENDA_ACCESS_ERROR(IUBENDA_CONSENT_MANAGER_PLUGIN));
+        state.consents.initialized.value = false;
+        return;
+      }
+
+      // eslint-disable-next-line no-underscore-dangle
+      if (!(globalThis as any)._iub?.cs?.consent?.purposes) {
+        state.consents.initialized.value = false;
+        return;
+      }
+
+      try {
+        const allowedConsentIds: string[] = [];
+        const deniedConsentIds: string[] = [];
+
+        // eslint-disable-next-line no-underscore-dangle
+        Object.entries((globalThis as any)._iub.cs.api.getPreferences()).forEach(e => {
+          const purposeCode = e[0];
+          const isConsented = e[1];
+          if (isConsented) {
+            allowedConsentIds.push(purposeCode);
+          } else {
+            deniedConsentIds.push(purposeCode);
+          }
+        })
+
+        state.consents.initialized.value = true;
+        state.consents.data.value = { allowedConsentIds, deniedConsentIds };
+      } catch (err) {
+        logger?.error(IUBENDA_ACCESS_ERROR(IUBENDA_CONSENT_MANAGER_PLUGIN), err);
+      }
     },
 
     isDestinationConsented(
@@ -40,39 +73,45 @@ const IubendaConsentManager = (): ExtensionPlugin => ({
       if (!state.consents.initialized.value) {
         return true;
       }
-
       const allowedConsentIds = state.consents.data.value.allowedConsentIds as string[];
+      const matchPredicate = (consent: string) => allowedConsentIds.includes(consent);
 
       try {
-        const { consentManagement } = destConfig;
+        const { iubendaConsentPurposes, consentManagement } = destConfig;
 
         // If the destination does not have consent management config, events should be sent.
-        if (!consentManagement) {
-          return true;
+        if (consentManagement) {
+          // Get the corresponding consents for the destination
+          const cmpConfig = consentManagement.find(c => c.provider === state.consents.provider.value);
+
+          // If there are no consents configured for the destination for the current provider, events should be sent.
+          if (!cmpConfig?.consents) {
+            return true;
+          }
+
+          const configuredConsents = cmpConfig.consents.map(c => c.consent.trim()).filter(n => n);
+          const resolutionStrategy =
+            cmpConfig.resolutionStrategy ?? state.consents.resolutionStrategy.value;
+  
+          // match the configured consents with user provided consents as per
+          // the configured resolution strategy
+          switch (resolutionStrategy) {
+            case 'or':
+              return configuredConsents.some(matchPredicate) || configuredConsents.length === 0;
+            case 'and':
+            default:
+              return configuredConsents.every(matchPredicate);
+          }
         }
 
-        // Get the corresponding consents for the destination
-        const cmpConfig = consentManagement.find(c => c.provider === state.consents.provider.value);
+        if (iubendaConsentPurposes) {
+          const configuredConsents = iubendaConsentPurposes.map(p => p.purpose.trim()).filter(n => n);
 
-        // If there are no consents configured for the destination for the current provider, events should be sent.
-        if (!cmpConfig?.consents) {
-          return true;
+          // Check if any of the destination's mapped ketch purposes are consented by the user in the browser.
+          return configuredConsents.some(matchPredicate) || configuredConsents.length === 0;
         }
 
-        const configuredConsents = cmpConfig.consents.map(c => c.consent.trim()).filter(n => n);
-        const resolutionStrategy =
-          cmpConfig.resolutionStrategy ?? state.consents.resolutionStrategy.value;
-
-        // match the configured consents with user provided consents as per
-        // the configured resolution strategy
-        const matchPredicate = (consent: string) => allowedConsentIds.includes(consent);
-        switch (resolutionStrategy) {
-          case 'or':
-            return configuredConsents.some(matchPredicate) || configuredConsents.length === 0;
-          case 'and':
-          default:
-            return configuredConsents.every(matchPredicate);
-        }
+        return true;
       } catch (err) {
         errorHandler?.onError(err, IUBENDA_CONSENT_MANAGER_PLUGIN, DESTINATION_CONSENT_STATUS_ERROR);
         return true;
--- packages/analytics-js-plugins/src/iubendaConsentManager/logMessages.ts
+++ packages/analytics-js-plugins/src/iubendaConsentManager/logMessages.ts
@@ -1,3 +1,8 @@
+import { LOG_CONTEXT_SEPARATOR } from '@rudderstack/analytics-js-common/constants/logMessages';
+
+const IUBENDA_ACCESS_ERROR = (context: string): string =>
+  `${context}${LOG_CONTEXT_SEPARATOR}Failed to access Iubenda SDK resources. Please ensure that the Iubenda SDK is loaded successfully before RudderStack SDK.`;
+
 const DESTINATION_CONSENT_STATUS_ERROR = `Failed to determine the consent status for the destination. Please check the destination configuration and try again.`;
 
-export { DESTINATION_CONSENT_STATUS_ERROR };
+export { IUBENDA_ACCESS_ERROR, DESTINATION_CONSENT_STATUS_ERROR };
