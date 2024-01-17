6deb94ce2e367750ed8406732f979a6f5082e24e
Mihir Bhalala <77438541+mihir-4116@users.noreply.github.com>
2024-01-17 14:02:52 +0530

    fix: ga4 page call mappings (#1579)
    * fix: ga4 page call mappings
    
    * chore: code review changes
    
    * chore: code review changes
    
    * chore: code review changes

--- packages/analytics-js-integrations/__tests__/integrations/GA4/browser.test.js
+++ packages/analytics-js-integrations/__tests__/integrations/GA4/browser.test.js
@@ -16,7 +16,7 @@ describe('Google Analytics 4 init tests', () => {
   test('Testing init call of Google Analytics 4 with MeasurementId', () => {
     const ga4 = new GA4(
       { measurementId: 'G-123456', debugView: true },
-      { getUserId: () => '1234', getUserTraits: () => { } },
+      { getUserId: () => '1234', getUserTraits: () => {} },
       destinationInfo,
     );
     ga4.init();
@@ -29,7 +29,7 @@ describe('Google Analytics 4 Track events tests', () => {
   beforeEach(() => {
     ga4 = new GA4(
       { measurementId: 'G-123456', extendPageViewParams: true, capturePageView: 'rs' },
-      { getUserId: () => '1234', getUserTraits: () => { } },
+      { getUserId: () => '1234', getUserTraits: () => {} },
       destinationInfo,
     );
     ga4.init();
@@ -52,20 +52,16 @@ describe('Google Analytics 4 Track events tests', () => {
 });
 
 describe('Google Analytics 4 Page events tests', () => {
-  let ga4;
-  beforeEach(() => {
-    ga4 = new GA4(
-      { measurementId: 'G-123456', extendPageViewParams: true, capturePageView: 'rs', piiPropertiesToIgnore: [{ piiProperty: '' }] },
-      { getUserId: () => '1234', getUserTraits: () => { } },
-      destinationInfo,
-    );
-    ga4.init();
-    window.gtag = jest.fn();
-  });
-
   pageEvents.forEach(event => {
-    test(`Page : ${event.description}`, () => {
-      const { input, output } = event;
+    let ga4;
+    const { description, input, output } = event;
+    beforeEach(() => {
+      ga4 = new GA4(input.config, { getUserId: () => '1234', getUserTraits: () => {} }, destinationInfo);
+      ga4.init();
+      window.gtag = jest.fn();
+    });
+
+    test(`Page : ${description}`, () => {
       try {
         ga4.page(input);
         // verifying events
@@ -76,14 +72,19 @@ describe('Google Analytics 4 Page events tests', () => {
       }
     });
   });
-})
+});
 
 describe('Google Analytics 4 Group events tests', () => {
   let ga4;
   beforeEach(() => {
     ga4 = new GA4(
-      { measurementId: 'G-123456', extendPageViewParams: true, capturePageView: 'rs', piiPropertiesToIgnore: [{ piiProperty: '' }] },
-      { getUserId: () => '1234', getUserTraits: () => { } },
+      {
+        measurementId: 'G-123456',
+        extendPageViewParams: true,
+        capturePageView: 'rs',
+        piiPropertiesToIgnore: [{ piiProperty: '' }],
+      },
+      { getUserId: () => '1234', getUserTraits: () => {} },
       destinationInfo,
     );
     ga4.init();
@@ -103,15 +104,20 @@ describe('Google Analytics 4 Group events tests', () => {
       }
     });
   });
-})
+});
 
 describe('Google Analytics 4 Identify events tests with no piiPropertiesToIgnore', () => {
   let ga4;
   // Config 1 : default piiPropertiesToIgnore value
   beforeEach(() => {
     ga4 = new GA4(
-      { measurementId: 'G-123456', extendPageViewParams: true, capturePageView: 'rs', piiPropertiesToIgnore: [{ piiProperty: '' }] },
-      { getUserId: () => '1234', getUserTraits: () => { } },
+      {
+        measurementId: 'G-123456',
+        extendPageViewParams: true,
+        capturePageView: 'rs',
+        piiPropertiesToIgnore: [{ piiProperty: '' }],
+      },
+      { getUserId: () => '1234', getUserTraits: () => {} },
       destinationInfo,
     );
     ga4.init();
@@ -131,7 +137,7 @@ describe('Google Analytics 4 Identify events tests with no piiPropertiesToIgnore
       }
     });
   });
-})
+});
 
 describe('Google Analytics 4 Identify events tests with undefined piiPropertiesToIgnore', () => {
   let ga4;
@@ -139,7 +145,7 @@ describe('Google Analytics 4 Identify events tests with undefined piiPropertiesT
   beforeEach(() => {
     ga4 = new GA4(
       { measurementId: 'G-123456', extendPageViewParams: true, capturePageView: 'rs' },
-      { getUserId: () => '1234', getUserTraits: () => { } },
+      { getUserId: () => '1234', getUserTraits: () => {} },
       destinationInfo,
     );
     ga4.init();
@@ -159,15 +165,25 @@ describe('Google Analytics 4 Identify events tests with undefined piiPropertiesT
       }
     });
   });
-})
+});
 
 describe('Google Analytics 4 Identify events tests with piiPropertiesToIgnore', () => {
   let ga4;
   // Config 3 : valid piiPropertiesToIgnore values
   beforeEach(() => {
     ga4 = new GA4(
-      { measurementId: 'G-123456', extendPageViewParams: true, capturePageView: 'rs', piiPropertiesToIgnore: [{ piiProperty: 'email' }, { piiProperty: 'phone' }, { piiProperty: 'card_number' }, { piiProperty: 'age' }] },
-      { getUserId: () => '1234', getUserTraits: () => { } },
+      {
+        measurementId: 'G-123456',
+        extendPageViewParams: true,
+        capturePageView: 'rs',
+        piiPropertiesToIgnore: [
+          { piiProperty: 'email' },
+          { piiProperty: 'phone' },
+          { piiProperty: 'card_number' },
+          { piiProperty: 'age' },
+        ],
+      },
+      { getUserId: () => '1234', getUserTraits: () => {} },
       destinationInfo,
     );
     ga4.init();
@@ -180,8 +196,12 @@ describe('Google Analytics 4 Identify events tests with piiPropertiesToIgnore', 
       try {
         ga4.identify(input);
         expect(window.gtag.mock.calls[0][2]).toEqual({
-          source: 'RudderStack', userInterest: 'high', age: null,
-          card_number: null, email: null, phone: null
+          source: 'RudderStack',
+          userInterest: 'high',
+          age: null,
+          card_number: null,
+          email: null,
+          phone: null,
         });
         expect(window.gtag.mock.calls[1][2]).toEqual(output.userId);
       } catch (error) {
@@ -189,4 +209,4 @@ describe('Google Analytics 4 Identify events tests with piiPropertiesToIgnore', 
       }
     });
   });
-})
+});
