a2f00972a9493b24269df77a701fc9873213f0b0
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2024-02-02 13:19:15 +0530

    Update packages/analytics-js-integrations/src/integrations/Appcues/browser.js
    Co-authored-by: Moumita <36885121+MoumitaM@users.noreply.github.com>

--- packages/analytics-js-integrations/src/integrations/Appcues/browser.js
+++ packages/analytics-js-integrations/src/integrations/Appcues/browser.js
@@ -57,7 +57,7 @@ class Appcues {
     const { userId } = rudderElement.message;
     // iterate through traits and flatten any properties that are objects or arrays
     Object.keys(traits).forEach(key => {
-      if (typeof traits[key] === 'object' && typeof traits[key] !== null) {
+      if ( traits[key] && typeof traits[key] === 'object' ) {
         Object.keys(traits[key]).forEach(subKey => {
           traits[`${key}.${subKey}`] = traits[key][subKey];
         });
