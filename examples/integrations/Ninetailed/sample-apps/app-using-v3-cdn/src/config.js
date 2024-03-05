56eb22a3f7540a7652920aa67dd29f8c0f9b332d
Anant Jain <62471433+anantjain45823@users.noreply.github.com>
2024-03-05 14:56:57 +0530

    chore: add ninetailed example apps (#1637)
    * chore: add ninetailed example apps
    
    * fix: move integrations folder in examples

---
+++ examples/integrations/Ninetailed/sample-apps/app-using-v3-cdn/src/config.js
@@ -0,0 +1,8 @@
+const DATAPLANE_URL='http://localhost:8080'
+const WRITE_KEY='2F4kphApmhq8LvdG4GUhYwsMH4P'
+const CONFIG_URL='https://api.dev.rudderlabs.com'
+module.exports = {
+    DATAPLANE_URL,
+    WRITE_KEY,
+    CONFIG_URL
+}
