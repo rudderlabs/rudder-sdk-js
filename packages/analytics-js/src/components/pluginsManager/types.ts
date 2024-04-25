c57cf820346a7fede352f2f346db37ad51413cf8
Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
2024-04-25 21:19:13 +0530

    feat: warn users on missing plugins (#1691)
    * feat: warn users on missing plugins
    
    * feat: structure the log messages
    
    * feat: add data plane events state
    
    * refactor: merge plugin filtering and configuration checking logic
    
    * refactor: clean up
    
    * test: add tests for updateDataPlaneEventsStateFromLoadOptions
    
    * fix: use types in signal creation
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix: add missing import
    
    * fix: add tests and refactor code
    
    * test: update descriptions as per coderabbitai suggestions
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/analytics-js/src/components/pluginsManager/types.ts
+++ packages/analytics-js/src/components/pluginsManager/types.ts
@@ -2,3 +2,12 @@ import type { ExtensionPlugin } from '@rudderstack/analytics-js-common/types/Plu
 import type { PluginName } from '@rudderstack/analytics-js-common/types/PluginsManager';
 
 export type PluginMap<T = ExtensionPlugin> = Record<PluginName, () => T>;
+
+export type PluginsGroup = {
+  configurationStatus: () => boolean;
+  configurationStatusStr: string;
+  activePluginName?: PluginName;
+  supportedPlugins: PluginName[];
+  basePlugins?: PluginName[];
+  shouldAddMissingPlugins?: boolean;
+};
