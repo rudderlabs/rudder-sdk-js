d2a3002dc1c5cc17d650ea5c0d9f2091978cf2c9
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-06-04 17:06:06 +0530

    fix: polyfill load in legacy browsers

--- packages/analytics-js-common/src/utilities/url.ts
+++ packages/analytics-js-common/src/utilities/url.ts
@@ -1,5 +1,5 @@
 import { URL_PATTERN } from '../constants/urls';
-import { isString } from './checks';
+import { isFunction, isString } from './checks';
 
 const removeDuplicateSlashes = (str: string): string => str.replace(/\/{2,}/g, '/');
 
@@ -9,15 +9,19 @@ const removeDuplicateSlashes = (str: string): string => str.replace(/\/{2,}/g, '
  * @returns true if `url` is valid and false otherwise
  */
 const isValidURL = (url: string | undefined): url is string => {
+  if (!isString(url)) {
+    return false;
+  }
+
   try {
-    if (!isString(url)) {
-      return false;
+    // If URL is supported by the browser, we can use it to validate the URL
+    // Otherwise, we can at least check if the URL matches the pattern
+    if (isFunction(globalThis.URL)) {
+      // eslint-disable-next-line no-new
+      new URL(url);
     }
-    // eslint-disable-next-line no-new
-    new URL(url);
     return URL_PATTERN.test(url);
   } catch (e) {
-    console.log(e);
     return false;
   }
 };
