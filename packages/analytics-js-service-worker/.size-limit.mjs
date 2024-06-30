f4b488dd435ffca4aec719b31dab9a63025b888e
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-06-30 23:47:14 +0530

    fix: package lint issues

---
+++ packages/analytics-js-service-worker/.size-limit.mjs
@@ -0,0 +1,16 @@
+/**
+ * DO NOT EDIT the size limits configured in this file.
+ * Should you update them, please contact CODEOWNERS.
+ */
+export default [
+  {
+    name: 'Service Worker Module ESM - NPM',
+    path: 'dist/npm/esm/index.mjs',
+    limit: '24 KiB',
+  },
+  {
+    name: 'Service Worker Module CJS - NPM',
+    path: 'dist/npm/esm/index.mjs',
+    limit: '24 KiB',
+  },
+];
