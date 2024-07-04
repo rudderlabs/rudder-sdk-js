75aa117911b1811b21576c95d2692d7f8580176c
Moumita <36885121+MoumitaM@users.noreply.github.com>
2024-07-04 13:19:20 +0530

    feat: update itp implementation based on load options (#1777)
    * feat: update itp implementation based on load options
    
    * chore: remove unnecessary logs
    
    * chore: refactor if condition
    
    Co-authored-by: Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
    
    * chore: review comment addressed
    
    * chore: update log message
    
    Co-authored-by: Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
    
    * chore: review comment address
    
    * chore: variable name update
    
    ---------
    
    Co-authored-by: Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>

--- packages/analytics-js/__tests__/components/configManager/validate.test.ts
+++ packages/analytics-js/__tests__/components/configManager/validate.test.ts
@@ -2,6 +2,7 @@ import {
   validateLoadArgs,
   getTopDomainUrl,
   getDataServiceUrl,
+  isWebpageTopLevelDomain,
 } from '../../../src/components/configManager/util/validate';
 
 describe('Config manager util - validate load arguments', () => {
@@ -46,12 +47,27 @@ describe('Config manager util - validate load arguments', () => {
   });
   describe('getDataServiceUrl', () => {
     it('should return dataServiceUrl', () => {
-      const dataServiceUrl = getDataServiceUrl('endpoint');
+      const dataServiceUrl = getDataServiceUrl('endpoint', false);
       expect(dataServiceUrl).toBe('https://test-host.com/endpoint');
     });
     it('should prepare the dataServiceUrl with endpoint without leading slash', () => {
-      const dataServiceUrl = getDataServiceUrl('/endpoint');
+      const dataServiceUrl = getDataServiceUrl('/endpoint', false);
       expect(dataServiceUrl).toBe('https://test-host.com/endpoint');
+    });
+    it('should return dataServiceUrl with exact domain', () => {
+      const dataServiceUrl = getDataServiceUrl('endpoint', true);
+      expect(dataServiceUrl).toBe('https://www.test-host.com/endpoint');
+    });
+  });
+
+  describe('isWebpageTopLevelDomain', () => {
+    it('should return true for top level domain', () => {
+      const isTopLevel = isWebpageTopLevelDomain('test-host.com');
+      expect(isTopLevel).toBe(true);
+    });
+    it('should return false for subdomain', () => {
+      const isTopLevel = isWebpageTopLevelDomain('sub.test-host.com');
+      expect(isTopLevel).toBe(false);
     });
   });
 });
