68b4293a9817b3a113af5f4ad8bfcfcf1cc81e88
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-09-18 11:14:18 +0530

    fix: add explicit checking for setting user and group traits

--- packages/analytics-js/.size-limit.mjs
+++ packages/analytics-js/.size-limit.mjs
@@ -77,7 +77,7 @@ export default [
     name: 'Core (Bundled) - Modern - NPM (CJS)',
     path: 'dist/npm/modern/bundled/cjs/index.cjs',
     import: '*',
-    limit: '39 KiB',
+    limit: '39.5 KiB',
   },
   {
     name: 'Core (Bundled) - Modern - NPM (UMD)',
