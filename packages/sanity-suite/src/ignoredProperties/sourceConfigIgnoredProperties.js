9a904e3962b00bf39ee835894e61723b79619499
Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
2024-09-20 13:12:27 +0530

    chore: fix sanity suite issues (#1862)

--- packages/sanity-suite/src/ignoredProperties/sourceConfigIgnoredProperties.js
+++ packages/sanity-suite/src/ignoredProperties/sourceConfigIgnoredProperties.js
@@ -35,14 +35,18 @@ const sourceConfigIgnoredProperties = [
     key: `source.destinations.6.updatedAt`,
     type: 'string',
   },
-{
+  {
+    key: `source.destinations.7.updatedAt`,
+    type: 'string',
+  },
+  {
     key: `source.destinations.8.updatedAt`,
     type: 'string',
   },
   {
     key: `source.destinations.9.updatedAt`,
     type: 'string',
-  }
+  },
 ];
 
 export { sourceConfigIgnoredProperties };
