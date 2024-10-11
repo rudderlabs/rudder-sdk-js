fe31ee6d7ae0e2ff01764642208a065d73e27189
Mohamed Khattab <muhammadkhattab292@outlook.com>
2024-10-11 11:22:29 +0300

    remove unused logMessage

--- packages/analytics-js-plugins/src/iubendaConsentManager/logMessages.ts
+++ packages/analytics-js-plugins/src/iubendaConsentManager/logMessages.ts
@@ -1,8 +1,5 @@
 import { LOG_CONTEXT_SEPARATOR } from '@rudderstack/analytics-js-common/constants/logMessages';
 
-const IUBENDA_ACCESS_ERROR = (context: string): string =>
-  `${context}${LOG_CONTEXT_SEPARATOR}Failed to access Iubenda SDK resources. Please ensure that the Iubenda SDK is loaded successfully before RudderStack SDK.`;
-
 const IUBENDA_CONSENT_COOKIE_READ_ERROR = (context: string): string =>
   `${context}${LOG_CONTEXT_SEPARATOR}Failed to read the consent cookie.`;
 
@@ -11,4 +8,4 @@ const IUBENDA_CONSENT_COOKIE_PARSE_ERROR = (context: string): string =>
 
 const DESTINATION_CONSENT_STATUS_ERROR = `Failed to determine the consent status for the destination. Please check the destination configuration and try again.`;
 
-export { IUBENDA_ACCESS_ERROR, IUBENDA_CONSENT_COOKIE_READ_ERROR, IUBENDA_CONSENT_COOKIE_PARSE_ERROR, DESTINATION_CONSENT_STATUS_ERROR };
+export { IUBENDA_CONSENT_COOKIE_READ_ERROR, IUBENDA_CONSENT_COOKIE_PARSE_ERROR, DESTINATION_CONSENT_STATUS_ERROR };
