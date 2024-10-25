3d8c8bc195421cabe91ae139a6ef951bb36f8cda
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2024-10-25 09:51:10 +0530

    fix: fix build size

--- packages/analytics-js/.size-limit.mjs
+++ packages/analytics-js/.size-limit.mjs
@@ -59,7 +59,7 @@ export default [
     name: 'Core (Bundled) - Legacy - NPM (CJS)',
     path: 'dist/npm/legacy/bundled/cjs/index.cjs',
     import: '*',
-    limit: '48.5 KiB',
+    limit: '49 KiB',
   },
   {
     name: 'Core (Bundled) - Legacy - NPM (UMD)',
