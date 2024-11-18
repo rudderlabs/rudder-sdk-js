295793a2cc60172b001c3fb1bc2624bb19fa8546
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-11-18 20:15:40 +0530

    feat: error handle public apis

--- packages/analytics-js/.size-limit.mjs
+++ packages/analytics-js/.size-limit.mjs
@@ -7,7 +7,7 @@ export default [
     name: 'Core - Legacy - NPM (ESM)',
     path: 'dist/npm/legacy/esm/index.mjs',
     import: '*',
-    limit: '48.5 KiB',
+    limit: '49 KiB',
   },
   {
     name: 'Core - Legacy - NPM (CJS)',
@@ -19,7 +19,7 @@ export default [
     name: 'Core - Legacy - NPM (UMD)',
     path: 'dist/npm/legacy/umd/index.js',
     import: '*',
-    limit: '48.5 KiB',
+    limit: '49 KiB',
   },
   {
     name: 'Core - Legacy - CDN',
@@ -47,13 +47,13 @@ export default [
   {
     name: 'Core - Modern - CDN',
     path: 'dist/cdn/modern/iife/rsa.min.js',
-    limit: '25 KiB',
+    limit: '25.5 KiB',
   },
   {
     name: 'Core (Bundled) - Legacy - NPM (ESM)',
     path: 'dist/npm/legacy/bundled/esm/index.mjs',
     import: '*',
-    limit: '48.5 KiB',
+    limit: '49 KiB',
   },
   {
     name: 'Core (Bundled) - Legacy - NPM (CJS)',
@@ -65,7 +65,7 @@ export default [
     name: 'Core (Bundled) - Legacy - NPM (UMD)',
     path: 'dist/npm/legacy/bundled/umd/index.js',
     import: '*',
-    limit: '48.5 KiB',
+    limit: '49 KiB',
   },
   {
     name: 'Core (Bundled) - Modern - NPM (ESM)',
@@ -77,7 +77,7 @@ export default [
     name: 'Core (Bundled) - Modern - NPM (CJS)',
     path: 'dist/npm/modern/bundled/cjs/index.cjs',
     import: '*',
-    limit: '40 KiB',
+    limit: '40.5 KiB',
   },
   {
     name: 'Core (Bundled) - Modern - NPM (UMD)',
@@ -89,7 +89,7 @@ export default [
     name: 'Core (Content Script) - Legacy - NPM (ESM)',
     path: 'dist/npm/legacy/content-script/esm/index.mjs',
     import: '*',
-    limit: '48 KiB',
+    limit: '48.5 KiB',
   },
   {
     name: 'Core (Content Script) - Legacy - NPM (CJS)',
@@ -101,7 +101,7 @@ export default [
     name: 'Core (Content Script) - Legacy - NPM (UMD)',
     path: 'dist/npm/legacy/content-script/umd/index.js',
     import: '*',
-    limit: '48 KiB',
+    limit: '48.5 KiB',
   },
   {
     name: 'Core (Content Script) - Modern - NPM (ESM)',
@@ -119,6 +119,6 @@ export default [
     name: 'Core (Content Script) - Modern - NPM (UMD)',
     path: 'dist/npm/modern/content-script/umd/index.js',
     import: '*',
-    limit: '39 KiB',
+    limit: '39.5 KiB',
   },
 ];
