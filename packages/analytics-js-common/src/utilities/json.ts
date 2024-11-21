4b9e0ccd85ef33c00e41072a10a7e27b479c3c43
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-11-22 00:58:42 +0530

    fix: use utility in a different method

--- packages/analytics-js-common/src/utilities/json.ts
+++ packages/analytics-js-common/src/utilities/json.ts
@@ -1,12 +1,10 @@
 import type { ILogger } from '../types/Logger';
 import type { Nullable } from '../types/Nullable';
-import { isBigInt, isNull, isNullOrUndefined } from './checks';
+import { isNull, isNullOrUndefined } from './checks';
 import {
-  BAD_DATA_WARNING,
   CIRCULAR_REFERENCE_WARNING,
   JSON_STRINGIFY_WARNING,
 } from '../constants/logMessages';
-import { isObjectLiteralAndNotNull } from './object';
 
 const JSON_STRINGIFY = 'JSONStringify';
 
@@ -71,8 +69,6 @@ const stringifyWithoutCircular = <T = Record<string, any> | any[] | number | str
   }
 };
 
-const JSON_UTIL = 'JSON';
-
 /**
  * Utility method for JSON stringify object excluding null values & circular references
  *
@@ -93,68 +89,4 @@ const stringifyData = <T = Record<string, any> | any[] | number | string>(
     return value;
   });
 
-const getReplacer = (logger?: ILogger): ((key: string, value: any) => any) => {
-  const ancestors: any[] = []; // Array to track ancestor objects
-
-  // Using a regular function to use `this` for the parent context
-  return function replacer(key, value): any {
-    if (isBigInt(value)) {
-      logger?.warn(BAD_DATA_WARNING(JSON_UTIL, key));
-      return '[BigInt]'; // Replace BigInt values
-    }
-
-    // `this` is the object that value is contained in, i.e., its direct parent.
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore-next-line
-    while (ancestors.length > 0 && ancestors[ancestors.length - 1] !== this) {
-      ancestors.pop(); // Remove ancestors that are no longer part of the chain
-    }
-
-    // Check for circular references (if the value is already in the ancestors)
-    if (ancestors.includes(value)) {
-      logger?.warn(BAD_DATA_WARNING(JSON_UTIL, key));
-      return '[Circular Reference]';
-    }
-
-    // Add current value to ancestors
-    ancestors.push(value);
-
-    return value;
-  };
-};
-
-const traverseWithThis = (obj: any, replacer: (key: string, value: any) => any): any => {
-  // Create a new result object or array
-  const result = Array.isArray(obj) ? [] : {};
-
-  // Traverse object properties or array elements
-  // eslint-disable-next-line no-restricted-syntax
-  for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key)) {
-      const value = obj[key];
-
-      // Recursively apply the replacer and traversal
-      const sanitizedValue = replacer.call(obj, key, value);
-
-      // If the value is an object or array, continue traversal
-      if (isObjectLiteralAndNotNull(sanitizedValue) || Array.isArray(sanitizedValue)) {
-        (result as any)[key] = traverseWithThis(sanitizedValue, replacer);
-      } else {
-        (result as any)[key] = sanitizedValue;
-      }
-    }
-  }
-
-  return result;
-};
-
-/**
- * Recursively traverses an object similar to JSON.stringify,
- * sanitizing BigInts and circular references
- * @param value Input object
- * @param logger Logger instance
- * @returns Sanitized value
- */
-const getSanitizedValue = <T = any>(value: T, logger?: ILogger): T => value;
-
-export { stringifyWithoutCircular, getSanitizedValue, stringifyData };
+export { stringifyWithoutCircular, stringifyData };
