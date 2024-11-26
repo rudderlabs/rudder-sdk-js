e45d58f5ba01fface948a482f2f866b21aaa3eb5
davidbielik <davidbielik@users.noreply.github.com>
2024-11-26 14:25:42 -0500

    Update packages/analytics-js-integrations/src/integrations/Braze/browser.js

--- packages/analytics-js-integrations/src/integrations/Braze/browser.js
+++ packages/analytics-js-integrations/src/integrations/Braze/browser.js
@@ -58,7 +58,6 @@ class Braze {
       allowUserSuppliedJavascript: this.allowUserSuppliedJavascript,
     });
     window.braze.addSdkMetadata([window.braze.BrazeSdkMetadata.CDN, 'rud']);
-
     window.braze.automaticallyShowInAppMessages();
     const { userId } = this.analytics;
     // send userId if you have it https://js.appboycdn.com/web-sdk/latest/doc/module-appboy.html#.changeUser
