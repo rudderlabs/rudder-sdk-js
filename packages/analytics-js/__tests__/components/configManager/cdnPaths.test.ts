4e41fe0c5e402e6c760d95c565acfacfd8fc1013
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-12-06 20:45:02 +0530

    chore: include url in cdn path error messages

--- packages/analytics-js/__tests__/components/configManager/cdnPaths.test.ts
+++ packages/analytics-js/__tests__/components/configManager/cdnPaths.test.ts
@@ -1,10 +1,9 @@
-import { CDN_INT_DIR } from '@rudderstack/analytics-js-common/constants/urls';
 import {
   getIntegrationsCDNPath,
   getPluginsCDNPath,
 } from '../../../src/components/configManager/util/cdnPaths';
-import { getSDKUrl } from '../../../src/components/configManager/util/commonUtil';
-import { DEST_SDK_BASE_URL, SDK_CDN_BASE_URL } from '../../../src/constants/urls';
+
+const getSDKUrlMock = jest.fn();
 
 jest.mock('../../../src/components/configManager/util/commonUtil.ts', () => {
   const originalModule = jest.requireActual(
@@ -14,62 +13,75 @@ jest.mock('../../../src/components/configManager/util/commonUtil.ts', () => {
   return {
     __esModule: true,
     ...originalModule,
-    getSDKUrl: jest.fn(),
+    getSDKUrl: (...args: any[]) => getSDKUrlMock(...args),
   };
 });
 
 describe('CDN path utilities', () => {
   describe('getIntegrationsCDNPath', () => {
-    const dummyCustomURL = 'https://www.dummy.url/integrations';
+    const dummyCustomURL = 'https://www.dummy.url/integrations/';
     const dummyScriptURL = 'https://www.dummy.url/fromScript/v3/modern/rsa.min.js';
     const dummyVersion = '3.x.x';
 
     beforeEach(() => {
-      getSDKUrl.mockImplementation(() => dummyScriptURL);
+      getSDKUrlMock.mockImplementation(() => dummyScriptURL);
     });
 
     afterEach(() => {
       jest.resetAllMocks();
     });
 
-    it('should return custom url if valid url is provided', () => {
+    it('should return custom URL if it is valid', () => {
       const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, false, dummyCustomURL);
-      expect(integrationsCDNPath).toBe(dummyCustomURL);
+      expect(integrationsCDNPath).toBe('https://www.dummy.url/integrations');
     });
 
-    it('should throw error if invalid custom url is provided', () => {
+    it('should throw error if invalid custom URL is provided', () => {
       const integrationsCDNPath = () => getIntegrationsCDNPath(dummyVersion, false, '/');
       expect(integrationsCDNPath).toThrow(
-        'Failed to load the SDK as the base URL for integrations is not valid.',
+        'Failed to load the SDK as the base URL "/" for integrations is not valid.',
       );
     });
 
     it('should return script src path if script src exists and integrations version is not locked', () => {
-      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, false, undefined);
+      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, false);
       expect(integrationsCDNPath).toBe(
         'https://www.dummy.url/fromScript/v3/modern/js-integrations',
       );
     });
 
     it('should return script src path with versioned folder if script src exists and integrations version is locked', () => {
-      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, true, undefined);
+      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, true);
       expect(integrationsCDNPath).toBe(
         'https://www.dummy.url/fromScript/3.x.x/modern/js-integrations',
       );
     });
 
     it('should return default path if no script src exists and integrations version is not locked', () => {
-      getSDKUrl.mockImplementation(() => undefined);
+      getSDKUrlMock.mockImplementation(() => undefined);
 
-      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, false, undefined);
+      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, false);
       expect(integrationsCDNPath).toBe('https://cdn.rudderlabs.com/v3/modern/js-integrations');
     });
 
     it('should return default path with versioned folder if no script src exists and integrations version is locked', () => {
-      getSDKUrl.mockImplementation(() => undefined);
+      getSDKUrlMock.mockImplementation(() => undefined);
 
-      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, true, undefined);
-      expect(integrationsCDNPath).toBe(`${SDK_CDN_BASE_URL}/${dummyVersion}/modern/${CDN_INT_DIR}`);
+      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, true);
+      expect(integrationsCDNPath).toBe(
+        `https://cdn.rudderlabs.com/${dummyVersion}/modern/js-integrations`,
+      );
+    });
+
+    it('should return that is not version locked when the script source is not as per the convention', () => {
+      getSDKUrlMock.mockImplementation(
+        () => 'https://www.dummy.url/fromScript/v3/custom/rsa.min.js',
+      );
+
+      const integrationsCDNPath = getIntegrationsCDNPath(dummyVersion, true);
+      expect(integrationsCDNPath).toBe(
+        `https://www.dummy.url/fromScript/v3/custom/js-integrations`,
+      );
     });
   });
 
@@ -79,7 +91,7 @@ describe('CDN path utilities', () => {
     const dummyVersion = '3.x.x';
 
     beforeEach(() => {
-      getSDKUrl.mockImplementation(() => dummyScriptURL);
+      getSDKUrlMock.mockImplementation(() => dummyScriptURL);
     });
 
     afterEach(() => {
@@ -91,10 +103,10 @@ describe('CDN path utilities', () => {
       expect(pluginsCDNPath).toBe('https://www.dummy.url/plugins');
     });
 
-    it('should throw error if invalid custom url is provided', () => {
+    it('should throw error if invalid custom URL is provided', () => {
       const pluginsCDNPath = () => getPluginsCDNPath(dummyVersion, false, 'htp:/some.broken.url');
       expect(pluginsCDNPath).toThrow(
-        'Failed to load the SDK as the base URL for plugins is not valid.',
+        'Failed to load the SDK as the base URL "htp:/some.broken.url" for plugins is not valid.',
       );
     });
 
@@ -109,17 +121,26 @@ describe('CDN path utilities', () => {
     });
 
     it('should return default path if no script src exists and plugins version is not locked', () => {
-      getSDKUrl.mockImplementation(() => undefined);
+      getSDKUrlMock.mockImplementation(() => undefined);
 
-      const pluginsCDNPath = getPluginsCDNPath(dummyVersion, false, undefined);
+      const pluginsCDNPath = getPluginsCDNPath(dummyVersion, false);
       expect(pluginsCDNPath).toBe('https://cdn.rudderlabs.com/v3/modern/plugins');
     });
 
     it('should return default path if no script src exists but plugins version is locked', () => {
-      getSDKUrl.mockImplementation(() => undefined);
+      getSDKUrlMock.mockImplementation(() => undefined);
 
-      const pluginsCDNPath = getPluginsCDNPath(dummyVersion, true, undefined);
+      const pluginsCDNPath = getPluginsCDNPath(dummyVersion, true);
       expect(pluginsCDNPath).toBe('https://cdn.rudderlabs.com/3.x.x/modern/plugins');
+    });
+
+    it('should return that is not version locked when the script source is not as per the convention', () => {
+      getSDKUrlMock.mockImplementation(
+        () => 'https://www.dummy.url/fromScript/v3/custom/rsa.min.js',
+      );
+
+      const pluginsCDNPath = getPluginsCDNPath(dummyVersion, true);
+      expect(pluginsCDNPath).toBe(`https://www.dummy.url/fromScript/v3/custom/plugins`);
     });
   });
 });
