c928e6e0d3ea6780b68975ff5f3c91d35f83c7df
Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
2025-01-07 21:58:36 +0530

    chore: fix sonarqube scan (#1995)

--- scripts/fix-reports-path-in-github-runner.sh
+++ scripts/fix-reports-path-in-github-runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Path variables
-defaultPrefixToReplace="/github/workspace"
-defaultAbsolutePathPrefix="home/runner/work/rudder-sdk-js/rudder-sdk-js"
-selfHostedAbsolutePathPrefix="runner/_work/rudder-sdk-js/rudder-sdk-js"
+defaultPrefixToReplace=""
+defaultAbsolutePathPrefix="home/runner/work/rudder-sdk-js/rudder-sdk-js/"
+selfHostedAbsolutePathPrefix="runner/_work/rudder-sdk-js/rudder-sdk-js/"
 absolutePathPrefix="$selfHostedAbsolutePathPrefix"
 # List of package folders
 projectFolderNames=("analytics-js" "analytics-js-common" "analytics-js-integrations" "analytics-js-plugins" "analytics-js-service-worker" "analytics-v1.1" "analytics-js-cookies")
