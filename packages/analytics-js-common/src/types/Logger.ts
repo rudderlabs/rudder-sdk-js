6a1433f3702c316d2b8d8cf7ba28c2d42755fb3d
Moumita <36885121+MoumitaM@users.noreply.github.com>
2025-06-18 11:56:42 +0530

    feat: custom integration interface and types (#2295)
    * feat(analytics-js-common): customintegration interface and types
    
    * chore: addressed review comments
    
    * chore: removed unused import
    
    * chore: updated customintegration type

--- packages/analytics-js-common/src/types/Logger.ts
+++ packages/analytics-js-common/src/types/Logger.ts
@@ -17,3 +17,5 @@ export interface ILogger {
 }
 
 export type LogLevel = 'LOG' | 'INFO' | 'DEBUG' | 'WARN' | 'ERROR' | 'NONE';
+
+export type RSALogger = Pick<ILogger, 'log' | 'info' | 'debug' | 'warn' | 'error'>;
