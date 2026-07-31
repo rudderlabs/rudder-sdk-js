f4c17702df739d2235ab1afb270ea9953a8ccfea
dciccale <539546+dciccale@users.noreply.github.com>
2026-07-31 21:57:42 +0200

    test: sdk-5057 reuse shared logger mock

--- packages/analytics-js/__tests__/components/customContext/utilities.test.ts
+++ packages/analytics-js/__tests__/components/customContext/utilities.test.ts
@@ -1,27 +1,14 @@
-import type { ILogger } from '@rudderstack/analytics-js-common/types/Logger';
+import { Logger } from '@rudderstack/analytics-js-common/__mocks__/Logger';
 import {
   filterReservedCustomContextKeys,
   prepareCustomContextUpdate,
 } from '../../../src/components/customContext';
 
-class MockLogger implements ILogger {
-  warn = jest.fn();
-  log = jest.fn();
-  error = jest.fn();
-  info = jest.fn();
-  debug = jest.fn();
-  minLogLevel = 0;
-  scope = 'test scope';
-  setMinLogLevel = jest.fn();
-  setScope = jest.fn();
-  logProvider = console;
-}
-
 describe('custom context utilities', () => {
-  let logger: MockLogger;
+  let logger: Logger;
 
   beforeEach(() => {
-    logger = new MockLogger();
+    logger = new Logger();
   });
 
   it('copies retained keys without invoking the __proto__ setter', () => {
