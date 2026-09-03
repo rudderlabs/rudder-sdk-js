79bdbae243b0bbd4fbb03791adb123b483ac3742
dciccale <539546+dciccale@users.noreply.github.com>
2026-09-03 09:34:08 +0200

    fix: sdk-2454 buffer initial browser events

--- examples/nextjs/full-stack/sample-app/src/pages/_app.tsx
+++ examples/nextjs/full-stack/sample-app/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import { useEffect } from 'react';
+import { getBrowserAnalytics } from '@/lib/rudderstack-client';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    void getBrowserAnalytics();
+  }, []);
+
   return <Component {...pageProps} />;
 }
